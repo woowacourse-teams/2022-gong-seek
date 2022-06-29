668869c83ec727deb2f4285c482508136594658d
이한울 <dkfghg3@naver.com>
2022-06-29 21:10:59 +0900

    chore: 프로젝트 초기 설정

---
+++ backend/src/main/java/com/woowacourse/gongseek/GongseekApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.gongseek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongseekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongseekApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/woowacourse/gongseek/GongseekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.gongseek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongseekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
