d4fa1ebe443986bb7ee446b4731a15d01555d6a8
brorae <jhr3239@gmail.com>
2022-07-04 20:43:29 +0900

    refactor: 매직넘버 상수화

--- backend/src/main/java/com/woowacourse/gongseek/question/domain/Question.java
+++ backend/src/main/java/com/woowacourse/gongseek/question/domain/Question.java
@@ -7,6 +7,10 @@ import javax.persistence.Id;
 @Entity
 public class Question {
 
+    private static final int MIN_TITLE_LENGTH = 0;
+    private static final int MAX_TITLE_LENGTH = 500;
+    private static final int MAX_CONTENT_LENGTH = 1000;
+
     @Id
     @GeneratedValue
     private Long id;
@@ -26,13 +30,13 @@ public class Question {
     }
 
     public void validateTitleLength(String title) {
-        if (title.trim().length() <= 0 || title.length() > 500) {
+        if (title.trim().length() <= MIN_TITLE_LENGTH || title.length() > MAX_TITLE_LENGTH) {
             throw new IllegalArgumentException("타이틀의 길이는 0 이상 500 이하여야합니다.");
         }
     }
 
     public void validateContentLength(String content) {
-        if (content.length() > 1000) {
+        if (content.length() > MAX_CONTENT_LENGTH) {
             throw new IllegalArgumentException("컨텐트의 길이는 1000 이하여야합니다.");
         }
     }
