840d4bc20a02bc50eb69651da2e35f939735328c
brorae <jhr3239@gmail.com>
2022-07-04 20:41:38 +0900

    style: 코드 포맷 변경

--- backend/src/main/java/com/woowacourse/gongseek/GongseekApplication.java
+++ backend/src/main/java/com/woowacourse/gongseek/GongseekApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GongseekApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GongseekApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GongseekApplication.class, args);
+    }
 
 }
