0426dd3ec6ebb881da3f8eda197d49b5ab5bbfa0
JASUN LEE <liswktjs@gmail.com>
2022-07-04 20:13:17 +0900

    chore: storybook decorators 설정

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,9 +1,33 @@
+import { ThemeProvider, Global } from '@emotion/react';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import { BrowserRouter } from 'react-router-dom';
+import { theme } from '@/styles/Theme';
+import { reset } from '@/styles/reset';
+
+const queryClient = new QueryClient();
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
+
+export const decorators = [
+	(Story) => (
+		<ThemeProvider theme={theme}>
+			<Global styles={reset} />
+			<QueryClientProvider client={queryClient}>
+				<RecoilRoot>
+					<BrowserRouter>
+						<Story />
+					</BrowserRouter>
+				</RecoilRoot>
+			</QueryClientProvider>
+		</ThemeProvider>
+	),
+];
