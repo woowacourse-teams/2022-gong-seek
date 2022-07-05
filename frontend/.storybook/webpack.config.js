11d3e12818a5eb26217886058d45a8579c8d8e0e
JASUN LEE <liswktjs@gmail.com>
2022-07-05 16:00:00 +0900

    chore: emotion 관련 바벨 설정
    emotion의 css props을 사용하기 위해서 바벨 플러그인 설치 및 설정
    
    Co-authored-by: Jungmin Hwang <hwangstar156@users.noreply.github.com>

--- frontend/.storybook/webpack.config.js
+++ frontend/.storybook/webpack.config.js
@@ -1,9 +1,18 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = ({ config }) => {
-  config.resolve.alias = {
-    "@": path.resolve(__dirname, "../src"),
-  };
+	config.resolve.alias = {
+		'@': path.resolve(__dirname, '../src'),
+	};
 
-  return config;
+	config.module.rules.push({
+		test: /\.(ts|tsx)$/,
+		loader: require.resolve('babel-loader'),
+		options: {
+			plugins: [require.resolve('@emotion/babel-plugin')],
+		},
+	});
+
+	config.resolve.extensions.push('.ts', '.tsx');
+	return config;
 };
