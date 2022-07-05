28c05f719b6504a7223e851f972eca19e462b66c
JASUN LEE <liswktjs@gmail.com>
2022-07-05 21:12:17 +0900

    fix: devSever추가 리액트 라우터 돔에서 발생하는 history 에러 해결

--- frontend/webpack/webpack.common.js
+++ frontend/webpack/webpack.common.js
@@ -1,47 +1,49 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js",
-  },
-  mode: "development",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "/src"),
-    },
-    modules: ["node_modules"],
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
-  },
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.(css)$/,
-        exclude: /node_modules/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpeg|jpg)$/,
-        exclude: /node_modules/,
-        use: "file-loader",
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-    new CleanWebpackPlugin(),
-  ],
+	entry: './src/index.tsx',
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'bundle.js',
+	},
+	devServer: {
+		historyApiFallback: true,
+	},
+	mode: 'development',
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, '/src'),
+		},
+		modules: ['node_modules'],
+		extensions: ['.js', '.jsx', '.ts', '.tsx'],
+	},
+	devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx|ts|tsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpeg|jpg)$/,
+				exclude: /node_modules/,
+				use: 'file-loader',
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+		}),
+		new CleanWebpackPlugin(),
+	],
 };
