6cb6a82c88d416067f45cfd927266a619b35bff5
brorae <jhr3239@gmail.com>
2022-07-05 17:26:09 +0900

    refactor: 게시글 등록 API 명세 변경

--- backend/src/test/java/com/woowacourse/gongseek/article/presentation/ArticleControllerTest.java
+++ backend/src/test/java/com/woowacourse/gongseek/article/presentation/ArticleControllerTest.java
@@ -1,4 +1,4 @@
-package com.woowacourse.gongseek.question.presentation;
+package com.woowacourse.gongseek.article.presentation;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -6,13 +6,16 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.headerWit
 import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.woowacourse.gongseek.article.presentation.dto.ArticleRequest;
 import com.woowacourse.gongseek.config.RestDocsConfig;
-import com.woowacourse.gongseek.question.application.QuestionService;
-import com.woowacourse.gongseek.question.presentation.dto.QuestionResponse;
+import com.woowacourse.gongseek.article.application.ArticleService;
+import com.woowacourse.gongseek.article.presentation.dto.ArticleIdResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,17 +25,18 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 @DisplayName("질문 게시판 문서화")
 @AutoConfigureRestDocs
-@WebMvcTest(QuestionController.class)
+@WebMvcTest(ArticleController.class)
 @Import(RestDocsConfig.class)
-class QuestionControllerTest {
+class ArticleControllerTest {
 
     @MockBean
-    private QuestionService questionService;
+    private ArticleService articleService;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -42,19 +46,27 @@ class QuestionControllerTest {
 
     @Test
     void 질문_게시물_생성_API_문서화() throws Exception {
-        QuestionResponse response = new QuestionResponse(1L, "제목입니당", "내용입니당");
-        given(questionService.save(any())).willReturn(response);
+        ArticleIdResponse response = new ArticleIdResponse(1L);
+        ArticleRequest request = new ArticleRequest("title", "content", "question");
+        given(articleService.save(any())).willReturn(response);
 
-        ResultActions results = mockMvc.perform(post("/api/articles/questions")
-                .content(objectMapper.writeValueAsString(response))
+        ResultActions results = mockMvc.perform(post("/api/articles")
+                .content(objectMapper.writeValueAsString(request))
                 .contentType(MediaType.APPLICATION_JSON)
                 .characterEncoding("UTF-8"));
 
         results.andExpect(status().isCreated())
                 .andDo(print())
                 .andDo(document("question-create",
-                        responseHeaders(
-                                headerWithName(HttpHeaders.LOCATION).description("생성된 게시물의 url")
-                        )));
+                                requestFields(
+                                        fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
+                                        fieldWithPath("content").type(JsonFieldType.STRING).description("내용"),
+                                        fieldWithPath("category").type(JsonFieldType.STRING).description("카테고리")
+                                ),
+                                responseHeaders(
+                                        headerWithName(HttpHeaders.LOCATION).description("생성된 게시물의 url")
+                                )
+                        )
+                );
     }
 }
