c56326672f883da5f28f1e18e51c18d18aa72e23
brorae <jhr3239@gmail.com>
2022-07-05 17:24:53 +0900

    test: 토론 글 등록 인수 테스트 추가

--- backend/src/test/java/com/woowacourse/gongseek/acceptance/ArticleAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/gongseek/acceptance/ArticleAcceptanceTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.woowacourse.gongseek.auth.presentation.dto.TokenResponse;
-import com.woowacourse.gongseek.question.presentation.dto.QuestionRequest;
+import com.woowacourse.gongseek.article.presentation.dto.ArticleRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
@@ -15,7 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 @Disabled
-public class QuestionAcceptanceTest extends AcceptanceTest {
+public class ArticleAcceptanceTest extends AcceptanceTest {
 
     @Test
     void 유저가_깃허브로_로그인을_하고_질문을_등록할_수_있다() {
@@ -36,9 +36,41 @@ public class QuestionAcceptanceTest extends AcceptanceTest {
                 .given().log().all()
                 .header(HttpHeaders.AUTHORIZATION, tokenResponse.getAccessToken())
                 .accept(MediaType.APPLICATION_JSON_VALUE)
-                .body(new QuestionRequest("title", "content"))
+                .body(new ArticleRequest("title", "content", "question"))
                 .when()
-                .post("/api/articles/questions")
+                .post("/api/articles")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertAll(
+                () -> assertThat(response.header(HttpHeaders.LOCATION)).isNotBlank(),
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value())
+        );
+    }
+
+    @Test
+    void 유저가_깃허브로_로그인을_하고_토론을_등록할_수_있다() {
+        // given
+        TokenResponse tokenResponse = RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .body("code")
+                .when()
+                .post("/api/auth/token")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(TokenResponse.class);
+
+        // when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .header(HttpHeaders.AUTHORIZATION, tokenResponse.getAccessToken())
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .body(new ArticleRequest("title", "content", "discussion"))
+                .when()
+                .post("/api/articles")
                 .then().log().all()
                 .extract();
 
