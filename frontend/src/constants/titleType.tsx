e200a09934f03da01fa5a547d9d46d0677be62d0
JASUN LEE <liswktjs@gmail.com>
2022-07-05 13:48:34 +0900

    feat: title 스타일링 mobile, desktop 환경에 따른 글자 스타일링 Co-authored-by: Jungmin Hwang <hwangstar156@users.noreply.github.com>

---
+++ frontend/src/constants/titleType.tsx
@@ -0,0 +1,26 @@
+import { theme } from '@/styles/Theme';
+import { css } from '@emotion/react';
+
+export const mobileTitlePrimary = css({
+	color: '#000000',
+	fontWeight: 600,
+	fontSize: `${theme.fonts.SIZE_022}`,
+});
+
+export const mobileTitleSecondary = css({
+	color: '#000000',
+	fontWeight: 500,
+	fontSize: `${theme.fonts.SIZE_020}`,
+});
+
+export const desktopTitlePrimary = css({
+	color: '#000000',
+	fontWeight: 600,
+	fontSize: `${theme.fonts.SIZE_024}`,
+});
+
+export const desktopTitleSecondary = css({
+	color: '#000000',
+	fontWeight: 500,
+	fontSize: `${theme.fonts.SIZE_022}`,
+});
