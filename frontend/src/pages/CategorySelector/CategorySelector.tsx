ec9c8cea0351eef0f817f2cd7ecc5c00bfaa43ae
JASUN LEE <60773373+liswktjs@users.noreply.github.com>
2022-07-06 20:11:29 +0900

    게시글 작성하기 로직 구현  (#40)
    * chore: navigate할때에 depth 가 2이상일때를 대처
    기존에 두개 이상의 depth가 들어간 path로 navigate할 때에 에러가 발생하여, 해당 부분을 가능하게 하기 위해 웹팩 설정 수정
    
    Co-authored-by: Jungmin Hwang <hwangstar156@users.noreply.github.com>
    
    * feat: 임시 home 컴포넌트 생성 및, 링크 연결
    
    로직 테스트를 위한 home링크 연결 및 praram 연결
    
    * fix: 로그인 cors origin에러 수정하기 위해서 header 변경
    
    * feat: 카테고리 선택 페이지 연결
    각 카테고리마다, 링크 연결 + 파람을 연결해주기 위해서 param값 지정
    
    * feat: 글 쓰기 비동기 통신 로직 구현
    
    글쓰기 Post 요청 연결
    
    Co-authored-by: Jungmin Hwang <hwangstar156@users.noreply.github.com>

--- frontend/src/pages/CategorySelector/CategorySelector.tsx
+++ frontend/src/pages/CategorySelector/CategorySelector.tsx
@@ -2,6 +2,7 @@ import PageLayout from '@/components/layout/PageLayout/PageLayout';
 import { mobileTitleSecondary } from '@/constants/titleType';
 import styled from '@emotion/styled';
 import { CATEGORY_TYPE } from '@/constants/categoryType';
+import { Link } from 'react-router-dom';
 
 const Container = styled.div`
 	display: flex;
@@ -33,17 +34,25 @@ const CategoryButton = styled.button<{ categoryType: 'error' | 'discussion' }>`
 	}
 `;
 
+const StyledLink = styled(Link)`
+	text-decoration: none;
+`;
+
 const CategorySelector = () => (
 	<Container>
 		<PageLayout width="80%" height="16rem" flexDirection="column" justifyContent="space-around">
 			<h2 css={mobileTitleSecondary}>글쓰기 카테고리 선택</h2>
 			<CategoryButtonBox>
-				<CategoryButton type="button" categoryType="error">
-					에러
-				</CategoryButton>
-				<CategoryButton type="button" categoryType="discussion">
-					토론
-				</CategoryButton>
+				<StyledLink to="/article/error">
+					<CategoryButton type="button" categoryType="error">
+						에러
+					</CategoryButton>
+				</StyledLink>
+				<StyledLink to="/article/discussion">
+					<CategoryButton type="button" categoryType="discussion">
+						토론
+					</CategoryButton>
+				</StyledLink>
 			</CategoryButtonBox>
 		</PageLayout>
 	</Container>
--- frontend/src/pages/WritingArticles/index.tsx
+++ frontend/src/pages/WritingArticles/index.tsx
@@ -1,7 +1,94 @@
+import { postWritingArticle } from '@/api/article';
 import PageLayout from '@/components/layout/PageLayout/PageLayout';
 import styled from '@emotion/styled';
+import { AxiosResponse, AxiosError } from 'axios';
+import { useRef, useState } from 'react';
 import { IoIosArrowDown } from 'react-icons/io';
+import { useMutation } from 'react-query';
+import { useNavigate, useParams } from 'react-router-dom';
 import ToastUiEditor from './ToastUiEditor/ToastUiEditor';
+import { Editor } from '@toast-ui/react-editor';
+
+const WritingArticles = () => {
+	const { category } = useParams();
+
+	const { data, mutate, isError, isLoading, isSuccess, error } = useMutation<
+		AxiosResponse<{ id: string }>,
+		AxiosError,
+		{ title: string; category: string; content: string }
+	>(postWritingArticle);
+
+	const [title, setTitle] = useState('');
+	const [categoryOption, setCategoryOption] = useState(category ? category : '');
+	const content = useRef<Editor | null>(null);
+
+	const navigate = useNavigate();
+
+	const handleSubmitButtonClick = () => {
+		if (content.current === null) {
+			return;
+		}
+		mutate({
+			title,
+			category: categoryOption,
+			content: content.current.getInstance().getMarkdown(),
+		});
+	};
+
+	if (isLoading) return <div>글 전송중 </div>;
+
+	if (isError) {
+		if (error instanceof Error) {
+			return <div>{error.message}</div>;
+		}
+		return null;
+	}
+
+	if (isSuccess) {
+		navigate(`/article/${category}?id=${data.data.id}`);
+	}
+	return (
+		<Container>
+			<SelectorBox>
+				<PageLayout width="100%" height="fit-content">
+					<TitleInput
+						type="text"
+						placeholder="제목을 입력해주세요"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+				</PageLayout>
+				<PageLayout width="100%" height="fit-content">
+					<CategorySelectorBox>
+						<CategorySelector
+							name="writing"
+							required
+							value={categoryOption}
+							onChange={(e) => setCategoryOption(e.target.value)}
+						>
+							<option value="" disabled>
+								카테고리를 선택해주세요
+							</option>
+							<option value="error">에러</option>
+							<option value="discussion">토론</option>
+						</CategorySelector>
+						<SelectorButton />
+					</CategorySelectorBox>
+				</PageLayout>
+				<PageLayout width="100%" height="fit-content">
+					<HashTagInput type="text" placeholder="해쉬태그를 입력해주세요" />
+				</PageLayout>
+			</SelectorBox>
+
+			<Content>
+				<ToastUiEditor ref={content} />
+			</Content>
+			<SubmitButton type="button" onClick={handleSubmitButtonClick}>
+				등록하기
+			</SubmitButton>
+		</Container>
+	);
+};
 
 const Container = styled.div`
 	width: 100%;
@@ -98,35 +185,5 @@ const SubmitButton = styled.button`
 		background-color: ${({ theme }) => theme.colors.PURPLE_400};
 	}
 `;
-
-const WritingArticles = () => (
-	<Container>
-		<SelectorBox>
-			<PageLayout width="100%" height="fit-content">
-				<TitleInput type="text" placeholder="제목을 입력해주세요" />
-			</PageLayout>
-			<PageLayout width="100%" height="fit-content">
-				<CategorySelectorBox>
-					<CategorySelector name="writing" required defaultValue="">
-						<option value="" disabled>
-							카테고리를 선택해주세요
-						</option>
-						<option value="에러">에러</option>
-						<option value="토론">토론</option>
-					</CategorySelector>
-					<SelectorButton />
-				</CategorySelectorBox>
-			</PageLayout>
-			<PageLayout width="100%" height="fit-content">
-				<HashTagInput type="text" placeholder="해쉬태그를 입력해주세요" />
-			</PageLayout>
-		</SelectorBox>
-
-		<Content>
-			<ToastUiEditor />
-		</Content>
-		<SubmitButton type="button">등록하기</SubmitButton>
-	</Container>
-);
 
 export default WritingArticles;
