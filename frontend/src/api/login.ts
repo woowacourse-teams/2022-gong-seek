ab326ed35682e7f903d44378e89da6fa43729b45
JASUN LEE <liswktjs@gmail.com>
2022-07-06 20:06:22 +0900

    fix: 로그인 cors origin에러 수정하기 위해서 header 변경

--- frontend/src/api/login.ts
+++ frontend/src/api/login.ts
@@ -1,10 +1,21 @@
 import axios from 'axios';
 
 export const getGithubURL = async () => {
-	const response = await axios.get<{ url: string }>('/api/auth/github');
+	const response = await axios.get<{ url: string }>('http://192.168.0.155:8080/api/auth/github', {
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+		},
+	});
 
 	return response.data.url;
 };
 
 export const postLogin = (code: string) =>
-	axios.post<{ accesstoken: string }>('api/auth/token', { code });
+	axios.post<{ accesstoken: string }>('http://192.168.0.155:8080/api/auth/token', {
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+		},
+		body: {
+			code,
+		},
+	});
