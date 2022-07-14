f58b5eeb56ea364037fc7be15f17ea433c53eb34
이한울 <dkfghg3@naver.com>
2022-07-14 21:12:40 +0900

    test: 게시글 등록, 로그인 인수테스트 추가

--- backend/src/test/java/com/woowacourse/gongseek/acceptance/ArticleAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/gongseek/acceptance/ArticleAcceptanceTest.java
@@ -1,32 +1,31 @@
 package com.woowacourse.gongseek.acceptance;
 
+import static com.woowacourse.gongseek.auth.support.GithubClientFixtures.주디;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.woowacourse.gongseek.article.presentation.dto.ArticleRequest;
+import com.woowacourse.gongseek.auth.presentation.dto.OAuthCodeRequest;
 import com.woowacourse.gongseek.auth.presentation.dto.TokenResponse;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 @SuppressWarnings("NonAsciiCharacters")
-@Disabled
 public class ArticleAcceptanceTest extends AcceptanceTest {
 
     @Test
-    void 유저가_깃허브로_로그인을_하고_질문을_등록할_수_있다() {
+    void 유저가_깃허브로_로그인을_하고_게시글을_등록할_수_있다() {
         // given
         TokenResponse tokenResponse = RestAssured
                 .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .body("code")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new OAuthCodeRequest(주디.getCode()))
                 .when()
-                .post("/api/auth/token")
+                .post("/api/auth/fake/token")
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value())
                 .extract()
@@ -35,8 +34,8 @@ public class ArticleAcceptanceTest extends AcceptanceTest {
         // when
         ExtractableResponse<Response> response = RestAssured
                 .given().log().all()
-                .header(HttpHeaders.AUTHORIZATION, tokenResponse.getAccessToken())
-                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + tokenResponse.getAccessToken())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(new ArticleRequest("title", "content", "question"))
                 .when()
                 .post("/api/articles")
@@ -44,42 +43,20 @@ public class ArticleAcceptanceTest extends AcceptanceTest {
                 .extract();
 
         // then
-        assertAll(
-                () -> assertThat(response.header(HttpHeaders.LOCATION)).isNotBlank(),
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value())
-        );
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
     @Test
-    void 유저가_깃허브로_로그인을_하고_토론을_등록할_수_있다() {
-        // given
-        TokenResponse tokenResponse = RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .body("code")
-                .when()
-                .post("/api/auth/token")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .extract()
-                .as(TokenResponse.class);
-
-        // when
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .header(HttpHeaders.AUTHORIZATION, tokenResponse.getAccessToken())
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .body(new ArticleRequest("title", "content", "discussion"))
+    void 유저가_로그인을_하지않고_게시글을_등록할_수_없다() {
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer ")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new ArticleRequest("title", "content", "question"))
                 .when()
                 .post("/api/articles")
                 .then().log().all()
                 .extract();
 
-        // then
-        assertAll(
-                () -> assertThat(response.header(HttpHeaders.LOCATION)).isNotBlank(),
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value())
-        );
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
-
--- backend/src/test/java/com/woowacourse/gongseek/auth/support/GithubClientFixtures.java
+++ backend/src/test/java/com/woowacourse/gongseek/auth/support/GithubClientFixtures.java
@@ -1,51 +1,34 @@
 package com.woowacourse.gongseek.auth.support;
 
+import com.woowacourse.gongseek.auth.presentation.dto.GithubProfileResponse;
 import java.util.Arrays;
 
 public enum GithubClientFixtures {
 
-    주디("1", "주디", "jurlring", "https://avatars.githubusercontent.com/u/78091011?v=4", "accessToken1"),
-    슬로("2", "슬로", "hanull", "https://avatars.githubusercontent.com/u/46413629?v=4", "accessToken2");
+    주디("1", "주디", "jurlring", "https://avatars.githubusercontent.com/u/78091011?v=4"),
+    슬로("2", "슬로", "hanull", "https://avatars.githubusercontent.com/u/46413629?v=4");
 
     private final String code;
     private final String name;
     private final String githubId;
     private final String avatarUrl;
-    private final String accessToken;
 
-    GithubClientFixtures(String code, String name, String githubId, String avatarUrl, String accessToken) {
+    GithubClientFixtures(String code, String name, String githubId, String avatarUrl) {
         this.code = code;
         this.name = name;
         this.githubId = githubId;
         this.avatarUrl = avatarUrl;
-        this.accessToken = accessToken;
     }
 
-    public static String generateAccessToken(String code) {
-        return Arrays.stream(values())
+    public static GithubProfileResponse getGithubProfile(String code) {
+        GithubClientFixtures client = Arrays.stream(values())
                 .filter(value -> value.code.equals(code))
                 .findFirst()
-                .orElseThrow(() -> new IllegalStateException("로그인이 실패했습니다."))
-                .getAccessToken();
+                .orElseThrow(() -> new IllegalStateException("로그인이 실패했습니다."));
+        return new GithubProfileResponse(client.githubId, client.name, client.avatarUrl);
     }
 
     public String getCode() {
         return code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getGithubId() {
-        return githubId;
-    }
-
-    public String getAvatarUrl() {
-        return avatarUrl;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
     }
 }
