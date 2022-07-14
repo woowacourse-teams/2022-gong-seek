0fb2f9ec53340644ed663ec24f8a74723c5ea4e6
JASUN LEE <liswktjs@gmail.com>
2022-07-14 11:53:16 +0900

    feat: 댓글 컴포넌트 구현

---
+++ frontend/src/components/common/Comment/Comment.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, Story } from '@storybook/react';
+import Comment, { CommentPropType } from '@/components/common/Comment/Comment';
+
+export default {
+	title: 'common/Common',
+	component: Comment,
+	decorators: [
+		(Story) => (
+			<div style={{ width: '320px' }}>
+				<Story />
+			</div>
+		),
+	],
+} as Meta;
+
+const Template: Story<CommentPropType> = (args) => <Comment {...args} />;
+
+export const DefaultComment = Template.bind({});
+
+DefaultComment.args = {
+	id: '1',
+	author: {
+		name: '자스민',
+		avatar: 'http://openimage.interpark.com/goods_image_big/0/3/2/7/8317700327e_l.jpg',
+	},
+	content:
+		'댓글 예시를 적는 곳입니다. 댓글 예시입니다. 2줄 이상일때에 어떻게 처리할지 다루기 위해서 입력하는 곳입니다. 안녕하세요 공식을 방문해주셔서 감사합니다. 실험 테스트 중입니다. 에헿에에에헹헤에에헤엫ㅇ헿',
+	createAt: '2022.07.08:19:03',
+	isAuthor: true,
+};
---
+++ frontend/src/components/common/Comment/Comment.tsx
@@ -0,0 +1,43 @@
+import * as S from '@/components/common/Comment/Comment.style';
+
+export interface CommentPropType {
+	id: string;
+	author: {
+		name: string;
+		avatar: string;
+	};
+	content: string;
+	createAt: string;
+	isAuthor: boolean;
+}
+
+const Comment = ({ id, author, content, createAt, isAuthor }: CommentPropType) => {
+	const onUpdateButtonClick = () => {
+		// 댓글 수정 포털 열기
+	};
+	const onDeleteButtonClick = () => {
+		// 댓글 삭제 비동기 요청 보내기
+	};
+	return (
+		<S.Container>
+			<S.CommentHeader>
+				<S.CommentInfo>
+					<S.UserProfile src={author.avatar} />
+					<S.CommentInfoSub>
+						<S.UserName>{author.name}</S.UserName>
+						<S.CreateTime>{createAt}</S.CreateTime>
+					</S.CommentInfoSub>
+				</S.CommentInfo>
+				{isAuthor && (
+					<S.CommentAuthBox>
+						<S.Button onClick={onUpdateButtonClick}>수정</S.Button>
+						<S.Button onClick={onDeleteButtonClick}>삭제</S.Button>
+					</S.CommentAuthBox>
+				)}
+			</S.CommentHeader>
+			<S.CommentContent>{content}</S.CommentContent>
+		</S.Container>
+	);
+};
+
+export default Comment;
