84e3010cd4588e0ec4256cb1b16f46048ba5ed68
brorae <jhr3239@gmail.com>
2022-07-14 14:18:51 +0900

    refactor: 테스트 DB 설정 yml 파일로 이동

--- backend/src/test/java/com/woowacourse/gongseek/member/domain/repository/MemberRepositoryTest.java
+++ backend/src/test/java/com/woowacourse/gongseek/member/domain/repository/MemberRepositoryTest.java
@@ -5,12 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.woowacourse.gongseek.member.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @SuppressWarnings("NonAsciiCharacters")
-@AutoConfigureTestDatabase(replace = Replace.NONE)
 @DataJpaTest
 public class MemberRepositoryTest {
 
