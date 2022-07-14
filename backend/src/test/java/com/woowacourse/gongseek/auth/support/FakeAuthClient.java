fcad60676a8036a8bf07d47d30f6348de390c987
이한울 <dkfghg3@naver.com>
2022-07-15 03:06:22 +0900

    refactor: 상수명 변경

--- backend/src/test/java/com/woowacourse/gongseek/auth/support/FakeAuthClient.java
+++ backend/src/test/java/com/woowacourse/gongseek/auth/support/FakeAuthClient.java
@@ -7,12 +7,12 @@ public class FakeAuthClient implements OAuthClient {
 
     private static final String BASE_URL = "https://github.com";
     private static final String LOGIN_URL_SUFFIX = "/login/oauth/authorize?client_id=%s&redirect_uri=%s";
-    private static final String clientId = "clientId";
+    private static final String CLIENT_ID = "clientId";
     private static final String REDIRECT_URL = "http://localhost:8080/callback";
 
     @Override
     public String getRedirectUrl() {
-        return String.format(BASE_URL + LOGIN_URL_SUFFIX, clientId, REDIRECT_URL);
+        return String.format(BASE_URL + LOGIN_URL_SUFFIX, CLIENT_ID, REDIRECT_URL);
     }
 
     @Override
