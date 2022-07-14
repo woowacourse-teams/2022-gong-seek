626f237423cc134a0f912a304242b09e7a4ce9cb
JASUN LEE <liswktjs@gmail.com>
2022-07-14 14:28:24 +0900

    feat: toastUiViewer 구현
    toastEditor에서 입력된 값이 보여질 ToastUiViewer구현

---
+++ frontend/src/components/common/ArticleContent/ToastUiViewer/ToastUiViewer.stories.tsx
@@ -0,0 +1,15 @@
+import ToastUiViewer, {
+	ToastUiContentProps,
+} from '@/components/common/ArticleContent/ToastUiViewer/ToastUiViewer';
+import { Meta, Story } from '@storybook/react';
+
+export default {
+	title: 'common/ArticleContent/ToastUiViewer',
+	component: ToastUiViewer,
+} as Meta;
+
+const Template: Story<ToastUiContentProps> = (args) => <ToastUiViewer {...args} />;
+export const DefaultViewer = Template.bind({});
+DefaultViewer.args = {
+	initContent: '<h2>제목</h2>',
+};
