3c64efc208e3088fd197e1e31fb85c363af267c3
이한울 <dkfghg3@naver.com>
2022-07-15 16:30:21 +0900

    feat: 댓글 예외 처리 기능 구현

--- backend/src/main/java/com/woowacourse/gongseek/comment/domain/Comment.java
+++ backend/src/main/java/com/woowacourse/gongseek/comment/domain/Comment.java
@@ -4,6 +4,7 @@ package com.woowacourse.gongseek.comment.domain;
 import com.woowacourse.gongseek.article.domain.Article;
 import com.woowacourse.gongseek.member.domain.Member;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.FetchType;
@@ -24,6 +25,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Entity
 public class Comment {
 
+    private static final int MAX_CONTENT_LENGTH = 10000;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -42,8 +45,15 @@ public class Comment {
     private LocalDateTime createdAt;
 
     public Comment(String content, Member member, Article article) {
+        validateContentLength(content);
         this.content = content;
         this.member = member;
         this.article = article;
+    }
+
+    private void validateContentLength(String content) {
+        if (Objects.isNull(content) || content.isBlank() || content.length() > MAX_CONTENT_LENGTH) {
+            throw new IllegalArgumentException("댓글의 길이는 1~10000이여야 합니다.");
+        }
     }
 }
--- backend/src/test/java/com/woowacourse/gongseek/comment/domain/CommentTest.java
+++ backend/src/test/java/com/woowacourse/gongseek/comment/domain/CommentTest.java
@@ -5,20 +5,35 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import com.woowacourse.gongseek.article.domain.Article;
 import com.woowacourse.gongseek.article.domain.Category;
 import com.woowacourse.gongseek.member.domain.Member;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CommentTest {
 
+    private final Member member = new Member("jurl", "jurlring", "url");
+    private final Article article = new Article("title", "content", Category.DISCUSSION, member);
+
+    @Test
+    void 댓글이_null_이면_예외를_발생한다() {
+        assertThatThrownBy(() -> new Comment(null, member, article))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("댓글의 길이는 1~10000이여야 합니다.");
+    }
+
     @ParameterizedTest
     @ValueSource(strings = {"", " "})
     void 댓글이_비어있는_경우_예외를_발생한다(String content) {
-        Member member = new Member("jurl", "jurlring", "url");
-        Article article = new Article("title", "content", Category.DISCUSSION, member);
-
         assertThatThrownBy(() -> new Comment(content, member, article))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("댓글의 길이는 1~10000이여야 합니다.");
     }
 
+    @Test
+    void 댓글이_10000자를_초과하면_예외를_발생한다() {
+        String content = "a".repeat(10001);
+        assertThatThrownBy(() -> new Comment(content, member, article))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("댓글의 길이는 1~10000이여야 합니다.");
+    }
 }
