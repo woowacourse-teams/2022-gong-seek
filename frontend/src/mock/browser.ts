1ae60ae406ba4d41bdbf6d9db07d4668db113ba7
Hwang Jung Min <hwangstar156@gmail.com>
2022-07-15 10:25:06 +0900

    feat: 투표에 대한 msw 추가

--- frontend/src/mock/browser.ts
+++ frontend/src/mock/browser.ts
@@ -1,5 +1,7 @@
 import { setupWorker } from 'msw';
 
 import { LoginHandler } from '@/mock/index';
+import { VoteHandler } from './vote';
+import { ArticleHandler } from './article';
 
-export const worker = setupWorker(...LoginHandler);
+export const worker = setupWorker(...LoginHandler, ...VoteHandler, ...ArticleHandler);
