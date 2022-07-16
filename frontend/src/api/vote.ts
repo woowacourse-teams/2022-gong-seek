3db9554ab46aa2fa781658ffaf03d091a86aae7f
Hwang Jung Min <hwangstar156@gmail.com>
2022-07-16 17:18:13 +0900

    feat:api에서 articleId voteId 동적으로 주도록 변경

--- frontend/src/api/vote.ts
+++ frontend/src/api/vote.ts
@@ -5,34 +5,48 @@ export interface VoteItems {
 	count: number;
 }
 
-export const getVoteItems = async () => {
+export const getVoteItems = async (articleId: string) => {
 	const accessToken = localStorage.getItem('accessToken');
 
-	const { data } = await axios.get<VoteItems[]>('http://192.168.0.155:8080/api/articles/1/votes', {
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			Authorization: `Bearer ${accessToken}`,
+	const { data } = await axios.get<VoteItems[]>(
+		`http://192.168.0.155:8080/api/articles/${articleId}/votes`,
+		{
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				Authorization: `Bearer ${accessToken}`,
+			},
 		},
-	});
+	);
+	console.log(data);
 
 	return data;
 };
 
-export const registerVoteItems = () => {
+export const registerVoteItems = ({
+	articleId,
+	options,
+}: {
+	articleId: string;
+	options: string[];
+}) => {
 	const accessToken = localStorage.getItem('accessToken');
 
-	return axios.post<{ articleId: string }>('http://192.168.0.155:8080/api/articles/1/votes', {
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			Authorization: `Bearer ${accessToken}`,
+	return axios.post<{ articleId: string }>(
+		`http://192.168.0.155:8080/api/articles/${articleId}/votes`,
+		{ options },
+		{
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				Authorization: `Bearer ${accessToken}`,
+			},
 		},
-	});
+	);
 };
 
-export const checkVoteItems = (voteId: string) => {
+export const checkVoteItems = ({ articleId, voteId }: { articleId: string; voteId: string }) => {
 	const accessToken = localStorage.getItem('accessToken');
 
-	return axios.post(`http://192.168.0.155:8080/api/articles/1/votes/${voteId}`, {
+	return axios.post(`http://192.168.0.155:8080/api/articles/${articleId}/votes/${voteId}`, {
 		headers: {
 			'Access-Control-Allow-Origin': '*',
 			Authorization: `Bearer ${accessToken}`,
