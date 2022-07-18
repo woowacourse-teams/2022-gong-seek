6621e61b4a35e8e9d19801d6e56cf1dcbb833d3a
Hwang Jung Min <hwangstar156@gmail.com>
2022-07-18 14:43:31 +0900

    feat: 피드백 수장

--- frontend/src/api/comments.ts
+++ frontend/src/api/comments.ts
@@ -23,8 +23,16 @@ export const postComments = ({ content }: { content: string }) => {
 };
 
 export const getComments = async () => {
+	const accessToken = localStorage.getItem('accessToken');
+
 	const response = await axios.get<{ comments: CommentsResponse[] }>(
 		'http://192.168.0.155:8080/api/articles/:articleId/comments',
+		{
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				Authorization: `Bearer ${accessToken}`,
+			},
+		},
 	);
 
 	return response.data.comments;
