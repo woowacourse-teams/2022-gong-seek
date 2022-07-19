d0af4dd436d1538129098afc804a1e05ac31c900
이한울 <dkfghg3@naver.com>
2022-07-19 11:45:47 +0900

    refactor: 메서드명 변경

--- backend/src/main/java/com/woowacourse/gongseek/comment/domain/repository/CommentRepository.java
+++ backend/src/main/java/com/woowacourse/gongseek/comment/domain/repository/CommentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findCommentsByArticleId(Long articleId);
+    List<Comment> findAllByArticleId(Long articleId);
 }
