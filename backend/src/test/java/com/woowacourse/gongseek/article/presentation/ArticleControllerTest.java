16d79ce6189d02e7517090f931a0cdbed3667845
Gyuchool <balance0591@naver.com>
2022-07-19 15:16:53 +0900

    test: 게시물 수정 API 문서화

--- backend/src/test/java/com/woowacourse/gongseek/article/presentation/ArticleControllerTest.java
+++ backend/src/test/java/com/woowacourse/gongseek/article/presentation/ArticleControllerTest.java
@@ -2,9 +2,12 @@ package com.woowacourse.gongseek.article.presentation;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
@@ -13,9 +16,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.woowacourse.gongseek.article.application.ArticleService;
+import com.woowacourse.gongseek.article.domain.Category;
 import com.woowacourse.gongseek.article.presentation.dto.ArticleIdResponse;
 import com.woowacourse.gongseek.article.presentation.dto.ArticleRequest;
 import com.woowacourse.gongseek.article.presentation.dto.ArticleResponse;
+import com.woowacourse.gongseek.article.presentation.dto.ArticleUpdateRequest;
+import com.woowacourse.gongseek.article.presentation.dto.ArticleUpdateResponse;
 import com.woowacourse.gongseek.auth.infra.JwtTokenProvider;
 import com.woowacourse.gongseek.config.RestDocsConfig;
 import com.woowacourse.gongseek.member.presentation.dto.AuthorDto;
@@ -63,13 +69,16 @@ class ArticleControllerTest {
 
         ResultActions results = mockMvc.perform(post("/api/articles")
                 .header(HttpHeaders.AUTHORIZATION, "Bearer token")
-                .content(objectMapper.writeValueAsString(request))
                 .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request))
                 .characterEncoding("UTF-8"));
 
         results.andExpect(status().isCreated())
                 .andDo(print())
                 .andDo(document("article-create",
+                                requestHeaders(
+                                        headerWithName(HttpHeaders.AUTHORIZATION).description("Bearer + 토큰")
+                                ),
                                 requestFields(
                                         fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
                                         fieldWithPath("content").type(JsonFieldType.STRING).description("내용"),
@@ -95,22 +104,23 @@ class ArticleControllerTest {
         given(articleService.findOne(any(), any())).willReturn(response);
 
         ResultActions results = mockMvc.perform(get("/api/articles/{id}", 1L)
-                        .param("category", "question")
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer token")
-                        .characterEncoding("UTF-8"));
+                .header(HttpHeaders.AUTHORIZATION, "Bearer token")
+                .characterEncoding("UTF-8"));
 
         results.andExpect(status().isOk())
                 .andDo(print())
                 .andDo(document("article-find-one-login",
+                                requestHeaders(
+                                        headerWithName(HttpHeaders.AUTHORIZATION).description("Bearer + 토큰")
+                                ),
                                 responseFields(
                                         fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
-                                        fieldWithPath("author").type(JsonFieldType.OBJECT).description("작성자"),
                                         fieldWithPath("author.name").type(JsonFieldType.STRING).description("이름"),
                                         fieldWithPath("author.avatarUrl").type(JsonFieldType.STRING).description("프로필"),
                                         fieldWithPath("content").type(JsonFieldType.STRING).description("내용"),
                                         fieldWithPath("views").type(JsonFieldType.NUMBER).description("조회수"),
-                                        fieldWithPath("isAuthor").type(JsonFieldType.BOOLEAN).description("작성자"),
-                                        fieldWithPath("createdAt").type(JsonFieldType.VARIES).description("카테고리")
+                                        fieldWithPath("isAuthor").type(JsonFieldType.BOOLEAN).description("작성자이면 true"),
+                                        fieldWithPath("createdAt").type(JsonFieldType.STRING).description("생성 날짜")
                                 )
                         )
                 );
@@ -129,7 +139,6 @@ class ArticleControllerTest {
         given(articleService.findOne(any(), any())).willReturn(response);
 
         ResultActions results = mockMvc.perform(get("/api/articles/{id}", 1L)
-                .param("category", "question")
                 .characterEncoding("UTF-8"));
 
         results.andExpect(status().isOk())
@@ -137,15 +146,47 @@ class ArticleControllerTest {
                 .andDo(document("article-find-one-not-login",
                                 responseFields(
                                         fieldWithPath("title").type(JsonFieldType.STRING).description("제목"),
-                                        fieldWithPath("author").type(JsonFieldType.OBJECT).description("작성자"),
                                         fieldWithPath("author.name").type(JsonFieldType.STRING).description("이름"),
                                         fieldWithPath("author.avatarUrl").type(JsonFieldType.STRING).description("프로필"),
                                         fieldWithPath("content").type(JsonFieldType.STRING).description("내용"),
                                         fieldWithPath("views").type(JsonFieldType.NUMBER).description("조회수"),
-                                        fieldWithPath("isAuthor").type(JsonFieldType.BOOLEAN).description("작성자"),
-                                        fieldWithPath("createdAt").type(JsonFieldType.VARIES).description("카테고리")
+                                        fieldWithPath("isAuthor").type(JsonFieldType.BOOLEAN).description("작성자이면 true"),
+                                        fieldWithPath("createdAt").type(JsonFieldType.STRING).description("생성 날짜")
                                 )
                         )
                 );
+    }
+
+    @Test
+    void 게시물_수정_API_문서화() throws Exception {
+        ArticleUpdateRequest request = new ArticleUpdateRequest("제목 바꿀께요", "내용 수정합니다~~~");
+        ArticleUpdateResponse articleUpdateResponse = new ArticleUpdateResponse(1L, Category.QUESTION);
+        given(jwtTokenProvider.validateToken(any())).willReturn(true);
+        given(jwtTokenProvider.getPayload(any())).willReturn("1");
+        given(articleService.update(any(), any(), any())).willReturn(articleUpdateResponse);
+
+        ResultActions results = mockMvc.perform(put("/api/articles/{id}", 1L)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer token")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request))
+                .characterEncoding("UTF-8"));
+
+        results.andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("article-update",
+                                requestHeaders(
+                                        headerWithName(HttpHeaders.AUTHORIZATION).description("Bearer + 토큰")
+                                ),
+                                requestFields(
+                                        fieldWithPath("title").type(JsonFieldType.STRING).description("수정할 게시물 제목"),
+                                        fieldWithPath("content").type(JsonFieldType.STRING).description("수정할 게시물 내용")
+                                ),
+                                responseFields(
+                                        fieldWithPath("id").type(JsonFieldType.NUMBER).description("게시물 식별자"),
+                                        fieldWithPath("category").type(JsonFieldType.STRING).description("카테고리")
+                                )
+                        )
+                );
+
     }
 }
