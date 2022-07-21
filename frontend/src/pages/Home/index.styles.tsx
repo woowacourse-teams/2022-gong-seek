2746d10987211848df587d8155fb6dc24d76f4a3
Hwang Jung Min <hwangstar156@gmail.com>
2022-07-21 11:51:27 +0900

    refactor: emotion styles파일로 분리

---
+++ frontend/src/pages/Home/index.styles.tsx
@@ -0,0 +1,52 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+export const CategoryTitleContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-top: ${({ theme }) => theme.size.SIZE_050};
+`;
+
+export const CategoryTitleBox = styled.div`
+	display: flex;
+	gap: ${({ theme }) => theme.size.SIZE_012};
+	align-items: center;
+`;
+
+export const CategoryTitle = styled.div<{ isActive: boolean }>`
+	font-size: ${({ theme }) => theme.size.SIZE_018};
+	font-weight: 600;
+	color: ${({ theme }) => theme.colors.BLACK_600};
+	opacity: 0.3;
+	cursor: pointer;
+
+	${({ isActive, theme }) =>
+		isActive &&
+		css`
+			color: ${theme.colors.PURPLE_500};
+			text-decoration: underline;
+			opacity: 1;
+		`}
+`;
+
+export const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+export const PopularArticleTitle = styled.h2`
+	font-size: ${({ theme }) => theme.size.SIZE_012};
+	font-weight: 600;
+	color: ${({ theme }) => theme.colors.BLACK_600};
+	margin-bottom: ${({ theme }) => theme.size.SIZE_030};
+`;
+
+export const ArticleItemList = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: ${({ theme }) => theme.size.SIZE_018};
+	width: 100%;
+	margin-top: ${({ theme }) => theme.size.SIZE_024};
+`;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,92 +1,42 @@
 import PopularArticle from '@/pages/Home//PopularArticle/PopularArticle';
 import { useState } from 'react';
-import styled from '@emotion/styled';
-import { css } from '@emotion/react';
+
 import ArticleItem from '@/components/common/ArticleItem/ArticleItem';
 import SortDropdown from '@/pages/CategoryArticles/SortDropdown/SortDropDown';
-
-const CategoryTitleContainer = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	margin-top: ${({ theme }) => theme.size.SIZE_050};
-`;
-
-const CategoryTitleBox = styled.div`
-	display: flex;
-	gap: ${({ theme }) => theme.size.SIZE_012};
-	align-items: center;
-`;
-
-const CategoryTitle = styled.div<{ isActive: boolean }>`
-	font-size: ${({ theme }) => theme.size.SIZE_018};
-	font-weight: 600;
-	color: ${({ theme }) => theme.colors.BLACK_600};
-	opacity: 0.3;
-	cursor: pointer;
-
-	${({ isActive, theme }) =>
-		isActive &&
-		css`
-			color: ${theme.colors.PURPLE_500};
-			text-decoration: underline;
-			opacity: 1;
-		`}
-`;
-
-const Container = styled.div`
-	display: flex;
-	flex-direction: column;
-`;
-
-const PopularArticleTitle = styled.h2`
-	font-size: ${({ theme }) => theme.size.SIZE_012};
-	font-weight: 600;
-	color: ${({ theme }) => theme.colors.BLACK_600};
-	margin-bottom: ${({ theme }) => theme.size.SIZE_030};
-`;
-
-const ArticleItemList = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	gap: ${({ theme }) => theme.size.SIZE_018};
-	width: 100%;
-	margin-top: ${({ theme }) => theme.size.SIZE_024};
-`;
+import * as S from '@/pages/Home/index.styles';
 
 const Home = () => {
 	const [currentCategory, setCurrentCategory] = useState('question');
 
 	return (
-		<Container>
-			<PopularArticleTitle>오늘의 인기글</PopularArticleTitle>
+		<S.Container>
+			<S.PopularArticleTitle>오늘의 인기글</S.PopularArticleTitle>
 			<PopularArticle />
-			<CategoryTitleContainer>
-				<CategoryTitleBox>
-					<CategoryTitle
+			<S.CategoryTitleContainer>
+				<S.CategoryTitleBox>
+					<S.CategoryTitle
 						isActive={currentCategory === 'question'}
 						onClick={() => setCurrentCategory('question')}
 					>
 						에러
-					</CategoryTitle>
-					<CategoryTitle
+					</S.CategoryTitle>
+					<S.CategoryTitle
 						isActive={currentCategory === 'discussion'}
 						onClick={() => setCurrentCategory('discussion')}
 					>
 						토론
-					</CategoryTitle>
-				</CategoryTitleBox>
+					</S.CategoryTitle>
+				</S.CategoryTitleBox>
 				<SortDropdown />
-			</CategoryTitleContainer>
-			<ArticleItemList>
+			</S.CategoryTitleContainer>
+			<S.ArticleItemList>
 				<ArticleItem />
 				<ArticleItem />
 				<ArticleItem />
 				<ArticleItem />
 				<ArticleItem />
-			</ArticleItemList>
-		</Container>
+			</S.ArticleItemList>
+		</S.Container>
 	);
 };
 
