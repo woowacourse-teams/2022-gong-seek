76b87dc0011cadcd04b36b672460f238e2a085d0
jurlring <78091011+jurlring@users.noreply.github.com>
2022-07-21 15:02:53 +0900

    docs: 더미 데이터 생성

---
+++ backend/src/test/resources/dummy.sql
@@ -0,0 +1,93 @@
+insert into member (name, github_id, avatar_url)
+values ('name', 'github_id', 'avatar_url');
+
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'all', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'discussion', 1, '2022-07-18T12:34:12', 1);
+insert into article (title, content, category, member_id, created_at, views)
+values ('title', 'content', 'question', 1, '2022-07-18T12:34:12', 1);
