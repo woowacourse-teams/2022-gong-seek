f4bac506267064227df137402fb5011f322239e8
JASUN LEE <liswktjs@gmail.com>
2022-07-25 20:51:58 +0900

    fix: section 태그에 대응하기 위한 h2 태그를 추가함

--- frontend/src/components/common/Loading/Loading.tsx
+++ frontend/src/components/common/Loading/Loading.tsx
@@ -2,6 +2,7 @@ import * as S from '@/components/common/Loading/Loading.style';
 
 const Loading = () => (
 	<S.Container aria-label="현재 로딩 중 화면입니다">
+		<h2 hidden>로딩 중 화면입니다</h2>
 		<S.FirstSpinner />
 		<S.SecondSpinner />
 	</S.Container>
