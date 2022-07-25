db4e32db5250892ac4ddf8f7ba3816e1f28e2320
Hwang Jung Min <hwangstar156@gmail.com>
2022-07-25 18:31:52 +0900

    fix: msw 안되는 에러 해결

--- frontend/src/mock/article.ts
+++ frontend/src/mock/article.ts
@@ -2,6 +2,7 @@ import { rest } from 'msw';
 import { WritingArticles } from '@/api/article';
 import type { PathParams } from 'msw';
 import mockData from '@/mock/data/detailArticle.json';
+import { HOME_URL } from '@/constants/url';
 
 interface WritingArticlesWithId extends WritingArticles {
 	id: number;
@@ -13,7 +14,7 @@ const mockArticle = data ? (JSON.parse(data) as WritingArticlesWithId[]) : [];
 
 export const ArticleHandler = [
 	rest.post<{ title: string; content: string; category: string }, never, { id: number }>(
-		'http://192.168.0.155:8080/api/articles',
+		`${HOME_URL}/api/articles`,
 		(req, res, ctx) => {
 			const { title, content, category } = req.body;
 
@@ -26,7 +27,7 @@ export const ArticleHandler = [
 		},
 	),
 
-	rest.get('http://192.168.0.155:8080/api/articles/:id', (req, res, ctx) => {
+	rest.get(`${HOME_URL}/api/articles/:id`, (req, res, ctx) => {
 		const { id } = req.params;
 
 		if (typeof id !== 'string') {
@@ -56,7 +57,7 @@ export const ArticleHandler = [
 		);
 	}),
 
-	rest.get('http://192.168.0.155:8080/api/articles', (req, res, ctx) => {
+	rest.get(`${HOME_URL}/api/articles`, (req, res, ctx) => {
 		const page = req.url.searchParams.get('page');
 		const size = req.url.searchParams.get('size');
 
@@ -75,7 +76,7 @@ export const ArticleHandler = [
 		}));
 
 		if (page === null || size === null) {
-			return;
+			return res(ctx.status(200), ctx.json({ articles: responseArticles }));
 		}
 
 		const articlesPage = responseArticles.filter(
@@ -94,7 +95,7 @@ export const ArticleHandler = [
 	}),
 
 	rest.put<{ title: string; content: string }, PathParams, { id: string }>(
-		'http://192.168.0.155:8080/api/articles/:id',
+		`${HOME_URL}/api/articles/:id`,
 		(req, res, ctx) => {
 			const { title, content } = req.body;
 			const { id } = req.params;
@@ -117,7 +118,7 @@ export const ArticleHandler = [
 		},
 	),
 
-	rest.delete<never, PathParams>('http://192.168.0.155:8080/api/articles/:id', (req, res, ctx) => {
+	rest.delete<never, PathParams>(`${HOME_URL}/api/articles/:id`, (req, res, ctx) => {
 		const { id } = req.params;
 
 		if (typeof id !== 'string') {
