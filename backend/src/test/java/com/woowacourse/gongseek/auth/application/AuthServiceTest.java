9f8fcfc083bddf87bd71974a5f2595e48297d069
jurlring <78091011+jurlring@users.noreply.github.com>
2022-07-26 13:46:10 +0900

    refactor: 상수 및 매서드 분리

--- backend/src/test/java/com/woowacourse/gongseek/auth/application/AuthServiceTest.java
+++ backend/src/test/java/com/woowacourse/gongseek/auth/application/AuthServiceTest.java
@@ -1,6 +1,5 @@
 package com.woowacourse.gongseek.auth.application;
 
-import static com.woowacourse.gongseek.auth.support.GithubClientFixtures.주디;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
@@ -13,8 +12,9 @@ import com.woowacourse.gongseek.auth.presentation.dto.GithubProfileResponse;
 import com.woowacourse.gongseek.auth.presentation.dto.OAuthCodeRequest;
 import com.woowacourse.gongseek.auth.presentation.dto.OAuthLoginUrlResponse;
 import com.woowacourse.gongseek.auth.presentation.dto.TokenResponse;
+import com.woowacourse.gongseek.auth.support.GithubClientFixtures;
+import com.woowacourse.gongseek.member.domain.Member;
 import com.woowacourse.gongseek.member.domain.repository.MemberRepository;
-import java.net.URISyntaxException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -51,8 +51,10 @@ class AuthServiceTest {
     }
 
     @Test
-    void 엑세스토큰을_발급한다() throws JsonProcessingException, URISyntaxException {
-        mockGithubServer(주디.getGithubId(), 주디.getName(), 주디.getAvatarUrl());
+    void 엑세스토큰을_발급한다() {
+        GithubClientFixtures 주디 = GithubClientFixtures.주디;
+        mockGithubServer(주디);
+
         TokenResponse response = authService.generateAccessToken(new OAuthCodeRequest(주디.getCode()));
         mockServer.verify();
 
@@ -60,35 +62,42 @@ class AuthServiceTest {
     }
 
     @Test
-    void 기존_유저의_정보가_바뀌면_로그인을_했을때_업데이트되고_엑세스토큰을_발급한다() throws URISyntaxException, JsonProcessingException {
-        mockGithubServer(주디.getGithubId(), 주디.getName(), 주디.getAvatarUrl());
-        authService.generateAccessToken(new OAuthCodeRequest(주디.getCode()));
-        mockServer.verify();
+    void 기존_유저의_정보가_바뀌면_로그인을_했을때_업데이트되고_엑세스토큰을_발급한다() {
+        GithubClientFixtures 주디 = GithubClientFixtures.주디;
+        Member member = new Member(주디.getGithubId(), 주디.getName(), "previous avatar url");
+        memberRepository.save(member);
 
-        mockGithubServer(주디.getGithubId(), 주디.getName(), "update email");
+        mockGithubServer(주디);
         TokenResponse response = authService.generateAccessToken(new OAuthCodeRequest(주디.getCode()));
         mockServer.verify();
 
+        Member actual = memberRepository.findByGithubId(주디.getGithubId()).get();
         assertAll(
-                () -> assertThat(memberRepository.findByGithubId(주디.getGithubId()).get().getAvatarUrl()).isEqualTo(
-                        "update email"),
+                () -> assertThat(actual.getAvatarUrl()).isEqualTo(주디.getAvatarUrl()),
                 () -> assertThat(response).isNotNull()
         );
     }
 
-    private void mockGithubServer(String githubId, String name, String avatarUrl)
-            throws JsonProcessingException, URISyntaxException {
+    private void mockGithubServer(GithubClientFixtures client) {
         mockServer = MockRestServiceServer.createServer(restTemplate);
 
-        mockServer.expect(requestTo("https://github.com/login/oauth/access_token"))
-                .andRespond(withSuccess(
-                        objectMapper.writeValueAsString(new TokenResponse("accessToken")),
-                        MediaType.APPLICATION_JSON));
+        try {
+            mockServer.expect(requestTo("https://github.com/login/oauth/access_token")).andRespond(
+                    withSuccess(objectMapper.writeValueAsString(new TokenResponse("accessToken")),
+                            MediaType.APPLICATION_JSON));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new IllegalStateException("로그인이 실패했습니다.");
+        }
 
-        mockServer.expect(requestTo("https://api.github.com/user"))
-                .andExpect(method(HttpMethod.GET))
-                .andRespond(withSuccess(
-                        objectMapper.writeValueAsString(new GithubProfileResponse(githubId, name, avatarUrl)),
-                        MediaType.APPLICATION_JSON));
+        try {
+            mockServer.expect(requestTo("https://api.github.com/user")).andExpect(method(HttpMethod.GET)).andRespond(
+                    withSuccess(objectMapper.writeValueAsString(
+                                    new GithubProfileResponse(client.getGithubId(), client.getName(), client.getAvatarUrl())),
+                            MediaType.APPLICATION_JSON));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new IllegalStateException("로그인이 실패했습니다.");
+        }
     }
 }
