60cc7c7c4b424e9e0a5c08efc9c23d2202ccc908
JASUN LEE <liswktjs@gmail.com>
2022-07-26 15:50:01 +0900

    feat: header에 nav 추가
    데스트탑에서 nav bar로 보여주도록 새로운 nav 컴포넌트 추가 및 연결

--- frontend/src/components/layout/Header/Header.styles.tsx
+++ frontend/src/components/layout/Header/Header.styles.tsx
@@ -2,11 +2,22 @@ import { Link } from 'react-router-dom';
 
 import styled from '@emotion/styled';
 
-export const HeaderSection = styled.header`
+export const Container = styled.div`
 	position: sticky;
 	display: flex;
 
 	top: 0;
+
+	flex-direction: column;
+
+	width: 100%;
+	height: fit-content;
+
+	border-bottom: ${({ theme }) => theme.size.SIZE_001} solid ${({ theme }) => theme.colors.GRAY_500};
+`;
+
+export const HeaderSection = styled.header`
+	display: flex;
 
 	flex-direction: row;
 	align-items: center;
@@ -17,7 +28,13 @@ export const HeaderSection = styled.header`
 
 	background-color: ${({ theme }) => theme.colors.WHITE};
 
-	padding-top: 1rem;
+	padding: 1rem 0;
+
+	@media (min-width: ${({ theme }) => theme.breakpoints.DESKTOP}) {
+		width: calc(100% - ${({ theme }) => theme.size.SIZE_160} * 2);
+		padding: 1rem ${({ theme }) => theme.size.SIZE_160};
+		justify-content: space-between;
+	}
 `;
 
 export const LogoLink = styled.h1`
@@ -29,8 +46,51 @@ export const LogoLink = styled.h1`
 
 export const SearchBarBox = styled.div`
 	width: 60%;
+
+	@media (min-width: ${({ theme }) => theme.breakpoints.DESKTOP}) {
+		width: 40%;
+	}
 `;
 
 export const StyledLink = styled(Link)`
 	text-decoration: none;
+`;
+
+export const NavBar = styled.nav`
+	display: none;
+	visibility: hidden;
+
+	padding-left: ${({ theme }) => theme.size.SIZE_160};
+
+	@media (min-width: ${({ theme }) => theme.breakpoints.DESKTOP}) {
+		display: flex;
+		visibility: visible;
+
+		gap: ${({ theme }) => theme.size.SIZE_024};
+	}
+`;
+
+export const NavBarItem = styled(Link)`
+	text-decoration: none;
+
+	color: ${({ theme }) => theme.colors.BLACK_600};
+
+	padding: ${({ theme }) => theme.size.SIZE_016} 0;
+
+	&:hover,
+	&:active {
+		color: ${({ theme }) => theme.colors.PURPLE_500};
+	}
+`;
+
+export const LogOutItem = styled.button`
+	border: none;
+
+	background-color: transparent;
+	color: ${({ theme }) => theme.colors.BLACK_600};
+
+	&:hover,
+	&:active {
+		color: ${({ theme }) => theme.colors.PURPLE_500};
+	}
 `;
--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -1,15 +1,37 @@
 import SearchBar from '@/components/common/SearchBar/SearchBar';
 import * as S from '@/components/layout/Header/Header.styles';
+import { getUserIsLogin } from '@/store/userState';
+import { useRecoilValue } from 'recoil';
 
-const Header = () => (
-	<S.HeaderSection>
-		<S.StyledLink to="/">
-			<S.LogoLink>공식</S.LogoLink>
-		</S.StyledLink>
-		<S.SearchBarBox>
-			<SearchBar />
-		</S.SearchBarBox>
-	</S.HeaderSection>
-);
+const Header = () => {
+	const isLogin = useRecoilValue(getUserIsLogin);
+
+	return (
+		<S.Container>
+			<S.HeaderSection>
+				<S.StyledLink to="/">
+					<S.LogoLink>공식</S.LogoLink>
+				</S.StyledLink>
+				<S.SearchBarBox>
+					<SearchBar />
+				</S.SearchBarBox>
+			</S.HeaderSection>
+			<S.NavBar>
+				{isLogin && (
+					<>
+						<S.NavBarItem to="">마이페이지</S.NavBarItem>
+						<S.LogOutItem>로그아웃</S.LogOutItem>
+					</>
+				)}
+				<S.NavBarItem to="/login">로그인</S.NavBarItem>
+
+				<S.NavBarItem to="/category">글 쓰러 가기</S.NavBarItem>
+				<S.NavBarItem to="/articles/question">에러 카테고리</S.NavBarItem>
+				<S.NavBarItem to="/articles/discussion">토론 카테고리</S.NavBarItem>
+				<S.NavBarItem to="/">문의하기</S.NavBarItem>
+			</S.NavBar>
+		</S.Container>
+	);
+};
 
 export default Header;
