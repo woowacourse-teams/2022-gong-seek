2a76c02ed2650a30fbf7d543df9746107bbdc9aa
JASUN LEE <liswktjs@gmail.com>
2022-07-29 11:49:48 +0900

    refactor: category 페이지에서의 비동기 통신 로직을 커스텀 훅으로 분리
    무한스크롤 구현 및 useInfiniteQuery 부분을 커스텀 훅으로 분리

---
+++ frontend/src/pages/CategoryArticles/hooks/useGetCategoryArticles.tsx
@@ -0,0 +1,40 @@
+import { getAllArticle } from '@/api/article';
+import { infiniteArticleResponse } from '@/types/articleResponse';
+import { useEffect, useState } from 'react';
+import { useInfiniteQuery } from 'react-query';
+
+const useGetCategoryArticles = (category: string) => {
+	const [sortIndex, setSortIndex] = useState('최신순');
+
+	const { data, isLoading, isError, error, refetch, fetchNextPage } = useInfiniteQuery<
+		infiniteArticleResponse,
+		Error
+	>(['articles', category], () => getAllArticle({ category, sort: sortIndex }), {
+		getNextPageParam: (lastPage) => {
+			const { hasNext, cursorId, cursorViews } = lastPage;
+			if (hasNext) {
+				return {
+					category: category,
+					sort: sortIndex,
+					cursorId,
+					cursorViews,
+				};
+			}
+			return undefined;
+		},
+	});
+
+	useEffect(() => {
+		refetch();
+	}, [sortIndex]);
+
+	useEffect(() => {
+		if (isError) {
+			throw new Error(error.message);
+		}
+	}, [isError]);
+
+	return { data, fetchNextPage, sortIndex, setSortIndex, isLoading };
+};
+
+export default useGetCategoryArticles;
