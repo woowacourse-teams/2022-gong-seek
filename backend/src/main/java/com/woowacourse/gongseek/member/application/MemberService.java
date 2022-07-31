ab1bedf1ba90260c4902c186f129b35a9145625d
brorae <jhr3239@gmail.com>
2022-07-31 23:27:52 +0900

    refactor: transactional read only 추가

--- backend/src/main/java/com/woowacourse/gongseek/member/application/MemberService.java
+++ backend/src/main/java/com/woowacourse/gongseek/member/application/MemberService.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 @Service
 public class MemberService {
 
