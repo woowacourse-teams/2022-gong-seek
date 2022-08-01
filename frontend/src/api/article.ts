ff7cd6f1a37053ad613a7bf31a60d0ddb0b78f02
JASUN LEE <liswktjs@gmail.com>
2022-08-01 22:44:27 +0900

    fix: api 명세 변경으로 인한 수정
    pageSize로 수정됨

--- frontend/src/api/article.ts
+++ frontend/src/api/article.ts
@@ -29,7 +29,7 @@ export interface PopularArticles {
 
 export const getPopularArticles = async () => {
 	const result = await axios.get<PopularArticles>(
-		`${HOME_URL}/api/articles?category=all&sort=views&size=10`,
+		`${HOME_URL}/api/articles?category=all&sort=views&pageSize=10`,
 	);
 	return result.data;
 };
@@ -58,7 +58,7 @@ export const getAllArticle = async ({
 }) => {
 	const currentSort = sort === '최신순' ? 'latest' : 'views';
 	const { data } = await axios.get<AllArticleResponse>(
-		`${HOME_URL}/api/articles?category=${category}&sort=${currentSort}&cursorId=${cursorId}&cursorViews=${cursorViews}&size=5`,
+		`${HOME_URL}/api/articles?category=${category}&sort=${currentSort}&cursorId=${cursorId}&cursorViews=${cursorViews}&pageSize=5`,
 	);
 
 	return {
