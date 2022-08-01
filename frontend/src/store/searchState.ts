785fd98e8655d80be0515c7023e2f41cd6f5cac5
JASUN LEE <liswktjs@gmail.com>
2022-08-01 22:46:54 +0900

    feat: 검색 상태에 대해서 관리하기 위한 recoil store 생성
    검색창으로 이동했을 때에, header 스타일링을 다르게하고 검색 내용을 상태로 관리하기위한 reocil 선언

---
+++ frontend/src/store/searchState.ts
@@ -0,0 +1,10 @@
+import {atom } from 'recoil';
+
+export const searchState = atom({
+    key: 'searchState',
+    default: {
+        isSearchOpen: false,
+        isSearching: false,
+        target: '',
+    }
+})
