b9d236176818131224c2f1be905bb38a80e9d460
JASUN LEE <liswktjs@gmail.com>
2022-08-01 22:45:45 +0900

    feat: search 비동기 통신 구현
    무한 스크롤 방식을 사용하기 때문에, useinfiniteQuery를 사용하고 이를 customhook으로 분리
    서버에서 오는 응답값에 대한 타입 지정

---
+++ frontend/src/api/search.ts
@@ -0,0 +1,13 @@
+import { HOME_URL } from "@/constants/url";
+import { SearchResultType } from "@/types/searchResponse";
+import axios from "axios";
+
+export const getSearchResult = async ({target, cursorId = ''} : { target: string; cursorId: string; }) => {
+    const { data } = await axios.get<SearchResultType>(`${HOME_URL}/api/articles/search?searchText=${target}&cursorId=${cursorId}&pageSize=5`);
+    
+    return {
+        articles: data.articles,
+        hasNext: data.hasNext,
+        cursorId: '',
+    }
+}
