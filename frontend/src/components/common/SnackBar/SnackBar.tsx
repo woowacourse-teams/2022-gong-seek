a545a956763435a11e7a329da9d62341dd6387e5
JASUN LEE <liswktjs@gmail.com>
2022-08-02 01:12:09 +0900

    refactor: Portal 선언부를 분리
    DOM 찾는 비용을 줄이기 위해서 Portal 선언부 분리

--- frontend/src/components/common/SnackBar/SnackBar.tsx
+++ frontend/src/components/common/SnackBar/SnackBar.tsx
@@ -4,24 +4,35 @@ import { useRecoilValue } from 'recoil';
 import { snackBarState } from '@/store/snackBarState';
 
 import * as S from '@/components/common/SnackBar/SnackBar.styles';
+import { ReactNode } from 'react';
 
-
-const SnackBar = () => {
+const SnackBarPortal = ({children} : {children: ReactNode}) => {
     const snackBar = document.getElementById('snack-bar');
-    const {isOpen, message} = useRecoilValue(snackBarState);
 
     if(!snackBar){
         throw new Error('스낵바를 찾지 못하였습니다');
     }
+
+    return ReactDOM.createPortal(
+        <div>{children}</div>, snackBar
+    )
+}
+
+const SnackBar = () => {
+    const {isOpen, message} = useRecoilValue(snackBarState);
+
+   return  (
+    <SnackBarPortal>
+        {isOpen &&  
+        <S.Container>
+            <h2 hidden>안내 메세지가 나오는 곳입니다</h2>
+            <S.MessageBox>
+                {message}
+            </S.MessageBox>
+        </S.Container>}
+    </SnackBarPortal>
    
-   return isOpen ? ReactDOM.createPortal(
-    <S.Container>
-        <h2 hidden>안내 메세지가 나오는 곳입니다</h2>
-        <S.MessageBox>
-            {message}
-        </S.MessageBox>
-    </S.Container>
-   ,snackBar): null;
+   );
 };
 
 export default SnackBar;
