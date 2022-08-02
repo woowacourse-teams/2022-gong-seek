15dbadd4b6b731cdca997e5b36fe36a597539ae2
Hwang Jung Min <hwangstar156@gmail.com>
2022-08-02 20:23:10 +0900

    feat: VoteDeadlineGenerator 페이지 생성
    날짜와 시간을 받아서 같이 보내주는 페이지 구현

---
+++ frontend/src/pages/VoteDeadlineGenerator/index.tsx
@@ -0,0 +1,56 @@
+import { AxiosError, AxiosResponse } from 'axios';
+import React, { useEffect, useRef } from 'react';
+import { useMutation } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+
+import { registerVoteItems } from '@/api/vote';
+import Loading from '@/components/common/Loading/Loading';
+import useLocationState from '@/hooks/useLocationState';
+import * as S from '@/pages/VoteDeadlineGenerator/index.styles';
+
+const VoteDeadlineGenerator = () => {
+	const dateRef = useRef<HTMLInputElement>(null);
+	const timeRef = useRef<HTMLInputElement>(null);
+	const { articleId, options } = useLocationState<{ articleId: string; options: string[] }>();
+	const navigate = useNavigate();
+	const { isLoading, mutate, isError, data, isSuccess } = useMutation<
+		AxiosResponse<{ articleId: string }>,
+		AxiosError,
+		{ articleId: string; options: string[]; expiryDate: string }
+	>(registerVoteItems);
+
+	useEffect(() => {
+		if (isSuccess) {
+			alert('등록이 완료되었습니다.');
+			navigate(`/articles/discussion/${articleId}`);
+		}
+	}, [isSuccess]);
+
+	const handleSubmitVoteDeadlineForm = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+
+		mutate({
+			articleId,
+			options,
+			expiryDate: `${dateRef.current?.value}T${timeRef.current?.value}`,
+		});
+	};
+
+	if (isLoading) return <Loading />;
+
+	return (
+		<S.Container onSubmit={handleSubmitVoteDeadlineForm}>
+			<S.VoteDeadlineLabel>마감 기한을 설정해주세요.</S.VoteDeadlineLabel>
+			<S.VoteDeadlineInputBox>
+				<S.DeadlineInput type="date" required min="2022-08-02" ref={dateRef} />
+				<S.ValidateMessage></S.ValidateMessage>
+				<br />
+				<S.DeadlineInput type="time" required ref={timeRef} />
+				<S.ValidateMessage></S.ValidateMessage>
+			</S.VoteDeadlineInputBox>
+			<S.SubmitButton>등록하기</S.SubmitButton>
+		</S.Container>
+	);
+};
+
+export default VoteDeadlineGenerator;
