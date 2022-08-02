70fda4e0e19a5f7b39b633b430827e196ae40be0
이한울 <dkfghg3@naver.com>
2022-08-02 16:24:25 +0900

    refactor: 테이블명 스네이크 형식으로 변경하는 로직 추가

--- backend/src/test/java/com/woowacourse/gongseek/common/DatabaseCleaner.java
+++ backend/src/test/java/com/woowacourse/gongseek/common/DatabaseCleaner.java
@@ -21,13 +21,24 @@ public class DatabaseCleaner implements InitializingBean {
     public void afterPropertiesSet() {
         tableNames = entityManager.getMetamodel().getEntities().stream()
                 .filter(entityType -> entityType.getJavaType().getAnnotation(Entity.class) != null)
-                .map(entityType -> entityType.getName().toLowerCase())
+                .map(entityType -> changeNaming(entityType.getName()))
                 .collect(Collectors.toList());
+    }
+
+    private String changeNaming(String entityName) {
+        StringBuilder tableName = new StringBuilder();
+        for (int index = 0; index < entityName.length(); index++) {
+            char ch = entityName.charAt(index);
+            if (index > 0 && Character.isUpperCase(ch)) {
+                tableName.append("_");
+            }
+            tableName.append(Character.toLowerCase(ch));
+        }
+        return tableName.toString();
     }
 
     @Transactional
     public void tableClear() {
-
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
 
         for (String tableName : tableNames) {
