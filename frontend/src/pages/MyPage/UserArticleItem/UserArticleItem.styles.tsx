699283ea3c12872fa78e2b940f4077e2cf0e026b
JASUN LEE <liswktjs@gmail.com>
2022-08-02 16:25:31 +0900

    feat: 유저가 쓴 글 보여줄 컴포넌트 구현
    유저가 쓴 글을 간단하게 보여줄 컴포넌트 구현,
    해당 컴포넌트를 클릭시 상세페이지로 이동하도록 구현
    *추후에 시간 바꾸는 것 완성되면 수정 예정

---
+++ frontend/src/pages/MyPage/UserArticleItem/UserArticleItem.styles.tsx
@@ -0,0 +1,77 @@
+import styled from "@emotion/styled";
+
+import { BiCommentDetail } from "react-icons/bi";
+
+export const Container = styled.div`
+    display: flex;
+
+    flex-direction: column;
+
+    width: 100%;
+    height: ${({theme}) => theme.size.SIZE_126};
+
+    border: ${({theme}) => theme.size.SIZE_001} solid ${({theme}) => theme.colors.GRAY_500};
+    border-radius: ${({theme}) => theme.size.SIZE_004};
+
+    background-color: ${({theme}) => theme.colors.GRAY_100};
+
+    padding: ${({theme}) => theme.size.SIZE_006};
+
+    gap: ${({theme}) => theme.size.SIZE_010};
+`
+
+export const CategoryName = styled.div<{isQuestion: boolean}>`
+    width: fit-content;
+    height: fit-content;
+
+    color: ${({theme})=> theme.colors.WHITE};
+
+    border: ${({theme}) => theme.size.SIZE_001} solid ${({theme}) => theme.colors.GRAY_500};
+    border-radius: ${({theme}) => theme.size.SIZE_010};
+
+    background-color: ${(props) => (props.isQuestion ? '#FF0063' : '#3AB0FF')};
+
+    padding: ${({theme}) => theme.size.SIZE_004};
+`
+
+export const ArticleTitle = styled.div`
+    height: ${({theme}) => theme.size.SIZE_050};
+    font-size: ${({theme}) => theme.size.SIZE_016};
+
+    overflow: hidden;
+    word-break: break-all;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+
+    padding: ${({theme}) => theme.size.SIZE_002};
+`;
+
+export const ArticleSubInfo = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const ArticleTime = styled.div`
+    font-size: ${({theme}) => theme.size.SIZE_012};
+    color: ${({theme})=>theme.colors.BLACK_400};
+`;
+
+export const ArticleRightBox = styled.div`
+    display: flex;
+    gap: ${({theme}) => theme.size.SIZE_004};
+`
+
+export const CommentBox = styled.div`
+    display: flex;
+
+    gap: ${({theme}) => theme.size.SIZE_006};
+`;
+export const CommentIcon = styled(BiCommentDetail)``;
+export const CommentCount = styled.div`
+    font-size: ${({theme}) => theme.size.SIZE_014};
+`;
+
+
+export const Views = styled.span`
+    font-size: ${({theme}) => theme.size.SIZE_014};
+`;
