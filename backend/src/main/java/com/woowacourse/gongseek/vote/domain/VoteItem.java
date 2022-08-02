c6982cdf580b31d607584442719dcd72fbb8bafb
이한울 <dkfghg3@naver.com>
2022-08-02 16:26:11 +0900

    refactor: id 전략 추가

--- backend/src/main/java/com/woowacourse/gongseek/vote/domain/VoteItem.java
+++ backend/src/main/java/com/woowacourse/gongseek/vote/domain/VoteItem.java
@@ -4,20 +4,23 @@ import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 public class VoteItem {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Embedded
