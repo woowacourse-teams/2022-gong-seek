d7b63070592099f66655c4a606571e35cff3ab98
이한울 <dkfghg3@naver.com>
2022-08-02 16:26:43 +0900

    refactor: 기본 생성자 접근 제한자 변경

--- backend/src/main/java/com/woowacourse/gongseek/vote/presentation/dto/VoteResponse.java
+++ backend/src/main/java/com/woowacourse/gongseek/vote/presentation/dto/VoteResponse.java
@@ -1,10 +1,11 @@
 package com.woowacourse.gongseek.vote.presentation.dto;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 @Getter
 public class VoteResponse {
