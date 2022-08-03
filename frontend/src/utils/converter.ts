7f188d427d3ae843286492c238d6a4d24456224b
Hwang Jung Min <hwangstar156@gmail.com>
2022-08-03 11:28:17 +0900

    feat: yyyy-mm-ddThh:mm:ss 형식으로 들어오는 dataFormat 변경하는 함수 구현
    h년전, m달전, d일전, h시간전, m분전, 방금전이 나올수 있도록 구현

--- frontend/src/utils/converter.ts
+++ frontend/src/utils/converter.ts
@@ -41,3 +41,52 @@ export const convertIdxToArticleColorKey = (idx: number) => {
 			return 'ARTICLE_001';
 	}
 };
+
+const convertIdxToTimeKey = (idx: number) => {
+	switch (idx) {
+		case 0:
+			return '년';
+		case 1:
+			return '달';
+		case 2:
+			return '일';
+		case 3:
+			return '시간';
+		case 4:
+			return '분';
+		default:
+			return '방금';
+	}
+};
+
+const timePicker = (timeArray: number[]) => {
+	for (let i = 0; i < timeArray.length - 1; i++) {
+		if (timeArray[i] > 0) {
+			return { value: timeArray[i], key: convertIdxToTimeKey(i) };
+		}
+	}
+
+	return { key: convertIdxToTimeKey(timeArray.length - 1) };
+};
+
+export const dateTimeConverter = (dateTime: string) => {
+	const [date, time] = dateTime.split('T');
+
+	const [year, month, day] = date.split('-').map((e) => Number(e));
+	const [hour, minute, second] = time.split(':').map((e) => Number(e));
+
+	const createdAt = new Date(year, month - 1, day, hour, minute, second);
+
+	const timeGap = (Date.now() - createdAt.getTime()) / 1000;
+
+	const yearGap = Math.floor(timeGap / (3600 * 24 * 30 * 12));
+	const monthGap = Math.floor((timeGap / (3600 * 24 * 30)) % 12);
+	const dayGap = Math.floor((timeGap / (3600 * 24)) % 30);
+	const hourGap = Math.floor((timeGap / 3600) % 24);
+	const minuteGap = Math.floor((timeGap / 60) % 60);
+	const secondGap = Math.floor(timeGap % 60);
+
+	const selectedTime = timePicker([yearGap, monthGap, dayGap, hourGap, minuteGap, secondGap]);
+
+	return `${selectedTime.value ? selectedTime.value : ''}${selectedTime.key}전`;
+};
