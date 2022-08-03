1d732384d79bfddccb3d267cad31acd3ab3aef72
JASUN LEE <liswktjs@gmail.com>
2022-08-03 17:50:19 +0900

    test: 인기 게시글을 조회하는 커스텀 훅에 대해서 테스트 한다
    prettier로 인한 코드 포맷팅

--- frontend/src/pages/CategoryArticles/hooks/useGetCategoryArticles.tsx
+++ frontend/src/pages/CategoryArticles/hooks/useGetCategoryArticles.tsx
@@ -1,7 +1,8 @@
-import { getAllArticle } from '@/api/article';
-import { infiniteArticleResponse } from '@/types/articleResponse';
 import { useEffect, useState } from 'react';
 import { useInfiniteQuery } from 'react-query';
+
+import { getAllArticle } from '@/api/article';
+import { infiniteArticleResponse } from '@/types/articleResponse';
 
 const useGetCategoryArticles = (category: string) => {
 	const [sortIndex, setSortIndex] = useState('최신순');
@@ -9,20 +10,31 @@ const useGetCategoryArticles = (category: string) => {
 	const { data, isLoading, isError, isSuccess, error, refetch, fetchNextPage } = useInfiniteQuery<
 		infiniteArticleResponse,
 		Error
-	>(['articles', category], () => getAllArticle({ category, sort: sortIndex }), {
-		getNextPageParam: (lastPage) => {
-			const { hasNext, cursorId, cursorViews } = lastPage;
-			if (hasNext) {
-				return {
-					category: category,
-					sort: sortIndex,
-					cursorId,
-					cursorViews,
-				};
-			}
-			return undefined;
+	>(
+		['articles', category],
+		({
+			pageParam = {
+				category,
+				sort: sortIndex,
+				cursorId: '',
+				cursorViews: '',
+			},
+		}) => getAllArticle(pageParam),
+		{
+			getNextPageParam: (lastPage) => {
+				const { hasNext, cursorId, cursorViews } = lastPage;
+				if (hasNext) {
+					return {
+						category: category,
+						sort: sortIndex,
+						cursorId,
+						cursorViews,
+					};
+				}
+				return;
+			},
 		},
-	});
+	);
 
 	useEffect(() => {
 		refetch();
@@ -34,7 +46,7 @@ const useGetCategoryArticles = (category: string) => {
 		}
 	}, [isError]);
 
-	return { data, fetchNextPage, sortIndex, setSortIndex, isLoading , isSuccess};
+	return { data, fetchNextPage, sortIndex, setSortIndex, isLoading, isSuccess };
 };
 
 export default useGetCategoryArticles;
