3046c742b7af87a1bd54b1ee0f9c29cae1171c41
JASUN LEE <60773373+liswktjs@users.noreply.github.com>
2022-08-03 14:45:03 +0900

    feat: 마이페이지 관련 api mock 구현

--- frontend/src/mock/index.ts
+++ frontend/src/mock/index.ts
@@ -1,6 +1,7 @@
 import { ArticleHandler } from '@/mock/article';
 import { CommentHandler } from '@/mock/comment';
 import { LoginHandler } from '@/mock/login';
+import { MyPageHandler } from '@/mock/myPage';
 import { VoteHandler } from '@/mock/vote';
 
-export { LoginHandler, ArticleHandler, VoteHandler, CommentHandler };
+export { LoginHandler, ArticleHandler, VoteHandler, CommentHandler, MyPageHandler };
