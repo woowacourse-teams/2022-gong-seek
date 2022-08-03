f53d71de8c1e60211f7848cdf9ca07a71f6b04f4
Hwang Jung Min <hwangstar156@gmail.com>
2022-08-03 18:43:51 +0900

    feat: ErrorBoundary 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
+import ErrorBoundary from './components/helper/ErrorBoundary';
 import { Routes, Route } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
 import SnackBar from '@/components/common/SnackBar/SnackBar';
+import PrivateRouter from '@/components/helper/PrivateRouter';
+import PublicRouter from '@/components/helper/PublicRouter';
 import Header from '@/components/layout/Header/Header';
 import TabBar from '@/components/layout/TabBar/TabBar';
-import PrivateRouter from '@/components/router/PrivateRouter';
-import PublicRouter from '@/components/router/PublicRouter';
 import CategoryArticles from '@/pages/CategoryArticles/CategoryArticles';
 import CategorySelector from '@/pages/CategorySelector/CategorySelector';
 import DiscussionDetail from '@/pages/DiscussionDetail';
@@ -45,29 +46,31 @@ const App = () => {
 	return (
 		<Layout>
 			<Header />
-			<Content>
-				<Routes>
-					<Route path="/callback" element={<LoginController />} />
-					<Route path="/category" element={<CategorySelector />} />
-					<Route element={<PrivateRouter isAuthenticated={isLogin} />}>
-						<Route path="/article/:category" element={<WritingArticles />} />
-						<Route path="/votes/:articleId" element={<VoteGenerator />} />
-						<Route path="/my-page" element={<MyPage />} />
-						<Route path="/votes-deadline" element={<VoteDeadlineGenerator />} />
-					</Route>
-					<Route element={<PublicRouter isAuthenticated={isLogin} />}>
-						<Route path="/login" element={<Login />} />
-					</Route>
-					<Route path="/articles/question/:id" element={<ErrorDetail />} />
-					<Route path="/articles/:category" element={<CategoryArticles />} />
-					<Route path="/articles/modify/:category/:id" element={<UpdateWriting />} />
-					<Route path="/search-result" element={<Search />} />
-					<Route path="/articles/discussion/:id" element={<DiscussionDetail />} />
-					<Route path="/articles/discussion/:id" element={<DiscussionDetail />} />
-					<Route path="/" element={<Home />} />
-					<Route path="/*" element={<NotFound />} />
-				</Routes>
-			</Content>
+			<ErrorBoundary enable={false}>
+				<Content>
+					<Routes>
+						<Route path="/callback" element={<LoginController />} />
+						<Route path="/category" element={<CategorySelector />} />
+						<Route element={<PrivateRouter isAuthenticated={isLogin} />}>
+							<Route path="/article/:category" element={<WritingArticles />} />
+							<Route path="/votes/:articleId" element={<VoteGenerator />} />
+							<Route path="/my-page" element={<MyPage />} />
+							<Route path="/votes-deadline" element={<VoteDeadlineGenerator />} />
+						</Route>
+						<Route element={<PublicRouter isAuthenticated={isLogin} />}>
+							<Route path="/login" element={<Login />} />
+						</Route>
+						<Route path="/articles/question/:id" element={<ErrorDetail />} />
+						<Route path="/articles/:category" element={<CategoryArticles />} />
+						<Route path="/articles/modify/:category/:id" element={<UpdateWriting />} />
+						<Route path="/search-result" element={<Search />} />
+						<Route path="/articles/discussion/:id" element={<DiscussionDetail />} />
+						<Route path="/articles/discussion/:id" element={<DiscussionDetail />} />
+						<Route path="/" element={<Home />} />
+						<Route path="/*" element={<NotFound />} />
+					</Routes>
+				</Content>
+			</ErrorBoundary>
 			<TabBar />
 			<SnackBar />
 		</Layout>
