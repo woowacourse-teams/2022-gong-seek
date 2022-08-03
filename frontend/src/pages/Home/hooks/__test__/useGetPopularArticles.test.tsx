752d8ec7136a9f890fd7575b355938c690b77b1c
JASUN LEE <liswktjs@gmail.com>
2022-08-03 17:48:16 +0900

    feat: 인기 게시글을 가져오는 customhook에 대해서 테스트 한다

--- frontend/src/pages/Home/hooks/__test__/useGetPopularArticles.test.tsx
+++ frontend/src/pages/Home/hooks/__test__/useGetPopularArticles.test.tsx
@@ -1,40 +1,37 @@
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { renderHook } from '@testing-library/react-hooks';
-import React from 'react';
 import useGetPopularArticles from '../useGetPopularArticles';
-import { setupServer } from 'msw/node'
+import { setupServer } from 'msw/node';
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
 
 import { ArticleHandler } from '@/mock/article';
-import { BrowserRouter } from 'react-router-dom';
+import { renderHook } from '@testing-library/react-hooks';
 
 const server = setupServer(...ArticleHandler);
 
 describe('useGetPopularArticles 테스트', () => {
-    beforeAll(() => server.listen())
-    afterEach(() => server.resetHandlers())
-    afterAll(() => server.close())
+	beforeAll(() => server.listen());
+	afterEach(() => server.resetHandlers());
+	afterAll(() => server.close());
 
-    test('인기 게시글들에 대해서 조회 할 수 있다', async () => {
+	test('인기 게시글들에 대해서 조회 할 경우, 10개의 응답이 내려온다', async () => {
+		const queryClient = new QueryClient();
+		const wrapper = ({ children }: { children: React.ReactNode }) => (
+			<React.StrictMode>
+				<QueryClientProvider client={queryClient}>
+					<BrowserRouter>{children}</BrowserRouter>
+				</QueryClientProvider>
+			</React.StrictMode>
+		);
 
-        const queryClient = new QueryClient();
-        const wrapper = ({ children }: { children: React.ReactNode }) => (
-            <React.StrictMode>
-                <QueryClientProvider client={queryClient}>
-                    <BrowserRouter>
-                        {children}
-                    </BrowserRouter>
-                </QueryClientProvider>
-            </React.StrictMode>
-        );
-    
-        const { result, waitFor } = renderHook(() => useGetPopularArticles(), { wrapper });
-        await waitFor(() => result.current.isSuccess, {interval: 100});
-        if(typeof result.current.data === 'undefined'){
-            return;
-        }
-        
-        const {data} = result.current;
-        const {articles} =data;
-        expect(articles.length).toEqual(10);
-    })  
-})
+		const { result, waitFor } = renderHook(() => useGetPopularArticles(), { wrapper });
+		await waitFor(() => result.current.isSuccess, { interval: 100 });
+		if (typeof result.current.data === 'undefined') {
+			return;
+		}
+
+		const { data } = result.current;
+		const { articles } = data;
+		expect(articles.length).toEqual(10);
+	});
+});
