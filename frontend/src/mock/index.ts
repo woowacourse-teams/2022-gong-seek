0de40af1889346ad0265b24487bfd1e71a7c6f64
Hwang Jung Min <hwangstar156@gmail.com>
2022-08-05 00:18:53 +0900

    feat: mock 주석처리

--- frontend/src/mock/index.ts
+++ frontend/src/mock/index.ts
@@ -3,6 +3,7 @@ import { CommentHandler } from '@/mock/comment';
 import { LoginHandler } from '@/mock/login';
 import { MyPageHandler } from '@/mock/myPage';
 import { SearchHandler } from '@/mock/search';
-import { VoteHandler } from '@/mock/vote';
 
-export { LoginHandler, ArticleHandler, VoteHandler, CommentHandler, MyPageHandler, SearchHandler };
+// import { VoteHandler } from '@/mock/vote';
+
+export { LoginHandler, ArticleHandler, CommentHandler, MyPageHandler, SearchHandler };
