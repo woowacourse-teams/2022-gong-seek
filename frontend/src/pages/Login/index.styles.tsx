58969449053a5a28ff5b5e9c6d5c3cc93f78658c
Jungmin Hwang <85891751+hwangstar156@users.noreply.github.com>
2022-08-04 22:21:06 +0900

    깨진 UI 수정 (#240)
    * feat: 검색결과 UI 수정
    
    flex-direction: column을 이용하여 해결
    
    * feat: TabBar position sticky -> fixed로 고치고 height 삭제

--- frontend/src/pages/Login/index.styles.tsx
+++ frontend/src/pages/Login/index.styles.tsx
@@ -7,5 +7,4 @@ export const Container = styled.div`
 	align-items: center;
 
 	width: 100%;
-	height: calc(100vh - 15rem);
 `;
--- frontend/src/pages/Search/SearchResult/SearchResult.styles.tsx
+++ frontend/src/pages/Search/SearchResult/SearchResult.styles.tsx
@@ -1,25 +1,25 @@
-import styled from "@emotion/styled";
+import styled from '@emotion/styled';
 
 export const Container = styled.section`
-    display: flex;
-    flex-direction: column;
+	display: flex;
+	flex-direction: column;
 
-    width: 100vw;
+	width: 100vw;
 
-    padding: ${({theme}) => theme.size.SIZE_010};
+	padding: ${({ theme }) => theme.size.SIZE_010};
 
-    gap: ${({theme}) => theme.size.SIZE_020};
-`
-
-export const Title = styled.h2`
-    font-size: ${({theme}) => theme.size.SIZE_018};
-    
-`
-export const SearchResultBox = styled.div`
-    display: flex;
-
-    width: 100%;
-
-    justify-content: center;
+	gap: ${({ theme }) => theme.size.SIZE_020};
 `;
 
+export const Title = styled.h2`
+	font-size: ${({ theme }) => theme.size.SIZE_018};
+`;
+export const SearchResultBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+
+	justify-content: center;
+	align-items: center;
+	gap: ${({ theme }) => theme.size.SIZE_020};
+`;
