24123c00c9bf881301e3f2e493a7d87c55887be5
Hwang Jung Min <hwangstar156@gmail.com>
2022-08-04 21:10:41 +0900

    feat: Home에 대한 반응형 UI 추가

--- frontend/src/pages/Home/index.styles.tsx
+++ frontend/src/pages/Home/index.styles.tsx
@@ -63,4 +63,14 @@ export const ArticleItemList = styled.div`
 	width: 100%;
 
 	margin-top: ${({ theme }) => theme.size.SIZE_024};
+
+	@media (min-width: ${({ theme }) => theme.breakpoints.DESKTOP}) {
+		display: grid;
+		width: 90%;
+		grid-template-columns: 1fr 1fr 1fr 1fr;
+		place-items: center;
+		margin: 0 auto;
+		gap: ${({ theme }) => theme.size.SIZE_022};
+		margin-top: ${({ theme }) => theme.size.SIZE_040};
+	}
 `;
