89cd037bba07e98f97697f61cbd3f4006350df9b
JASUN LEE <liswktjs@gmail.com>
2022-08-06 20:54:36 +0900

    fix: nav 바에서 있는 로그아웃을 클릭했을 때에 로그아웃이 가능하도록 한다
    데스크탑에서의 nav Bar에서 로그아웃을 눌렀을 때에 로그아웃이 되도록 한다

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -10,6 +10,13 @@ import { getUserIsLogin } from '@/store/userState';
 const Header = () => {
 	const isLogin = useRecoilValue(getUserIsLogin);
 	const { isSearchOpen } = useRecoilValue(searchState);
+
+	const onLogOutClick = () => {
+		window.confirm('정말로 로그아웃을 하시겠습니까?');
+		localStorage.removeItem('accessToken');
+		window.location.href = '/';
+	};
+
 	if (isSearchOpen) {
 		return (
 			<S.Container>
@@ -42,7 +49,7 @@ const Header = () => {
 				{isLogin ? (
 					<>
 						<S.NavBarItem to="/my-page">마이페이지</S.NavBarItem>
-						<S.LogOutItem>로그아웃</S.LogOutItem>
+						<S.LogOutItem onClick={onLogOutClick}>로그아웃</S.LogOutItem>
 					</>
 				) : (
 					<S.NavBarItem to="/login">로그인</S.NavBarItem>
