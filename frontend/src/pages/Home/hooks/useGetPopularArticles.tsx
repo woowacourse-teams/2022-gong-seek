7772c9ce066abf63d6c479ab78ab02b76b73c6fc
JASUN LEE <liswktjs@gmail.com>
2022-08-06 23:37:17 +0900

    fix: 불필요한 코드 제거
    더이상 커스텀 훅 내에서 사용하지 않는 부분들을 삭제

--- frontend/src/pages/Home/hooks/useGetPopularArticles.tsx
+++ frontend/src/pages/Home/hooks/useGetPopularArticles.tsx
@@ -1,14 +1,12 @@
 import { AxiosError } from 'axios';
 import { useEffect, useRef, useState } from 'react';
 import { useQuery } from 'react-query';
-import { useNavigate } from 'react-router-dom';
 
 import { getPopularArticles, PopularArticles } from '@/api/article';
 import CustomError from '@/components/helper/CustomError';
 import * as S from '@/pages/Home/PopularArticle/PopularArticle.styles';
 
 const useGetPopularArticles = () => {
-	const navigate = useNavigate();
 	const [currentIndex, setCurrentIndex] = useState(0);
 	const [indexLimit, setIndexLimit] = useState(0);
 	const mainArticleContent = useRef<HTMLDivElement>(null);
@@ -49,14 +47,6 @@ const useGetPopularArticles = () => {
 		mainArticleContent.current?.animate(S.showPopularSlider, S.animationTiming);
 	};
 
-	const navigateDetailPage = () => {
-		if (isSuccess) {
-			navigate(
-				`/articles/${data.articles[currentIndex].category}/${data.articles[currentIndex].id}`,
-			);
-		}
-	};
-
 	return {
 		data,
 		isLoading,
@@ -65,7 +55,6 @@ const useGetPopularArticles = () => {
 		currentIndex,
 		handleLeftSlideEvent,
 		handleRightSlideEvent,
-		navigateDetailPage,
 		mainArticleContent,
 	};
 };
