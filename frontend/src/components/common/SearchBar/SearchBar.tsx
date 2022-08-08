9d5ebd4e07b9f90a1429bfd22ee8ee68ae561967
JASUN LEE <60773373+liswktjs@users.noreply.github.com>
2022-08-08 14:50:33 +0900

    fix: 검색버튼을 눌렀을 때에, 안내메세지를 전달한다 (#245)
    2글자 미만으로 검색어를 입력하였을 때에, 유저에게 스낵바로 이를 알린다

--- frontend/src/components/common/SearchBar/SearchBar.tsx
+++ frontend/src/components/common/SearchBar/SearchBar.tsx
@@ -2,17 +2,24 @@ import { useState } from 'react';
 import { useRecoilState } from 'recoil';
 
 import * as S from '@/components/common/SearchBar/SearchBar.styles';
+import useSnackBar from '@/hooks/useSnackBar';
 import { searchState } from '@/store/searchState';
+import { validatedSearchInput } from '@/utils/validateInput';
 
 const SearchBar = ({ isValid }: { isValid: boolean }) => {
 	const [searchInput, setSearchInput] = useState('');
 	const [searchInputState, setSearchInputState] = useRecoilState(searchState);
+	const { showSnackBar } = useSnackBar();
 
 	const onChangeInputValue = (e: { target: HTMLInputElement }) => {
 		setSearchInput(e.target.value);
 	};
 
 	const onSubmitSearchTarget = () => {
+		if (!validatedSearchInput(searchInput)) {
+			showSnackBar('검색어는 최소 2글자 이상이여야 합니다!');
+			return;
+		}
 		setSearchInputState({
 			isSearching: true,
 			target: searchInput,
