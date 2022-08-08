8e65cf79d9ae2ed66d1527b269c45d800fb3d37f
JASUN LEE <liswktjs@gmail.com>
2022-08-08 21:51:45 +0900

    feat: 수정하기에 hashTag 기능 추가하기
    recoil에서 관리하는 상태에 hashTag 추가

--- frontend/src/hooks/useGetDetailArticle.tsx
+++ frontend/src/hooks/useGetDetailArticle.tsx
@@ -23,7 +23,7 @@ const useGetDetailArticle = (id: string) => {
 
 	useEffect(() => {
 		if (isSuccess) {
-			setTempArticle({ title: data.title, content: data.content });
+			setTempArticle({ title: data.title, content: data.content, hashTag: data.hashtag });
 		}
 	}, [isSuccess]);
 
