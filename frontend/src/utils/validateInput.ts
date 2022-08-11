966f96f31b2c8fd85b671ac03b32c715dcdf931d
JASUN LEE <liswktjs@gmail.com>
2022-08-11 14:29:54 +0900

    feat: title에 대한 유효성 평가를 하는 함수 생성

--- frontend/src/utils/validateInput.ts
+++ frontend/src/utils/validateInput.ts
@@ -1,1 +1,4 @@
 export const validatedSearchInput = (target: string) => target.length >= 2;
+
+export const validatedTitleInput = (titleInput: string) =>
+	titleInput.length >= 1 && titleInput.length <= 500;
