dbc396d9a3fb2d2b096e4958e92b4447d539097d
이한울 <dkfghg3@naver.com>
2022-08-11 11:28:32 +0900

    refactor: 게시글 카테고리 없는 예외 상속 클래스 변경

--- backend/src/main/java/com/woowacourse/gongseek/article/exception/ArticleCategoryNotFoundException.java
+++ backend/src/main/java/com/woowacourse/gongseek/article/exception/ArticleCategoryNotFoundException.java
@@ -1,7 +1,7 @@
 package com.woowacourse.gongseek.article.exception;
 
-import com.woowacourse.gongseek.common.exception.BadRequestException;
+import com.woowacourse.gongseek.common.exception.NotFoundException;
 
-public class ArticleCategoryNotFoundException extends BadRequestException {
+public class ArticleCategoryNotFoundException extends NotFoundException {
 
 }
