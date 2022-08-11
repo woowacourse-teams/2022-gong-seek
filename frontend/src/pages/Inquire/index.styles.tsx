5a6b04df818b1406560c356005925971bf4e661b
Hwang Jung Min <hwangstar156@gmail.com>
2022-08-11 23:59:40 +0900

    feat: 모달 수직 중앙 정렬을 위한 height 설정

--- frontend/src/pages/Inquire/index.styles.tsx
+++ frontend/src/pages/Inquire/index.styles.tsx
@@ -7,6 +7,7 @@ export const Container = styled.div`
 	align-items: center;
 
 	width: 100%;
+	height: 100%;
 `;
 
 export const InquireButton = styled.button`
--- frontend/src/pages/Login/index.styles.tsx
+++ frontend/src/pages/Login/index.styles.tsx
@@ -7,4 +7,5 @@ export const Container = styled.div`
 	align-items: center;
 
 	width: 100%;
+	height: 100%;
 `;
