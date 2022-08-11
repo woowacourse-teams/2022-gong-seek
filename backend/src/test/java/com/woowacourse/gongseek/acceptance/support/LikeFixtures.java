66ba11dd4e78409bfd5c7682249fa5221fb0bc26
jurlring <78091011+jurlring@users.noreply.github.com>
2022-08-11 19:53:33 +0900

    test: 파라미터 명 통일

--- backend/src/test/java/com/woowacourse/gongseek/acceptance/support/LikeFixtures.java
+++ backend/src/test/java/com/woowacourse/gongseek/acceptance/support/LikeFixtures.java
@@ -8,28 +8,28 @@ import io.restassured.response.Response;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
+@SuppressWarnings("NonAsciiCharacters")
 public class LikeFixtures {
 
     public static ExtractableResponse<Response> 게시물을_추천한다(TokenResponse accessToken,
-                                                          ArticleIdResponse articleIdResponse) {
-        ExtractableResponse<Response> response = RestAssured
+                                                          ArticleIdResponse article) {
+        return RestAssured
                 .given().log().all()
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken.getAccessToken())
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
-                .post("/api/articles/{articleId}/like", articleIdResponse.getId())
+                .post("/api/articles/{articleId}/like", article.getId())
                 .then().log().all()
                 .extract();
-        return response;
     }
 
-    public static ExtractableResponse<Response> 게시물_추천을_취소한다(TokenResponse 엑세스토큰, ArticleIdResponse 게시물) {
+    public static ExtractableResponse<Response> 게시물_추천을_취소한다(TokenResponse accessToken, ArticleIdResponse article) {
         return RestAssured
                 .given().log().all()
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + 엑세스토큰.getAccessToken())
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken.getAccessToken())
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
-                .delete("/api/articles/{articleId}/unlike", 게시물.getId())
+                .delete("/api/articles/{articleId}/unlike", article.getId())
                 .then().log().all()
                 .extract();
     }
