970dc15e7a0dfbd62bde19c9e60fc5006dfdf62f
Hwang Jung Min <hwangstar156@gmail.com>
2022-08-11 14:00:15 +0900

    feat: UserProfile에 수정할수 있는 UI 추가

--- frontend/src/pages/MyPage/UserProfile/UserProfile.styles.tsx
+++ frontend/src/pages/MyPage/UserProfile/UserProfile.styles.tsx
@@ -1,28 +1,55 @@
-import styled from "@emotion/styled";
+import { FaPencilAlt, FaCheckSquare } from 'react-icons/fa';
+
+import Input from '@/components/common/Input/Input';
+import styled from '@emotion/styled';
 
 export const Container = styled.div`
-    display: flex;
+	display: flex;
 
-    width: 100%;
+	width: 100%;
 
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    
-    gap: ${({theme}) => theme.size.SIZE_020};
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	gap: ${({ theme }) => theme.size.SIZE_020};
 `;
 
 export const UserProfile = styled.img`
-    width: ${({theme}) => theme.size.SIZE_050};
-    height: ${({theme}) => theme.size.SIZE_050};
+	width: ${({ theme }) => theme.size.SIZE_050};
+	height: ${({ theme }) => theme.size.SIZE_050};
 
-    border-radius: 50%;
+	border-radius: 50%;
 
-    object-fit: cover;
-    object-position: center;
-`
+	object-fit: cover;
+	object-position: center;
+`;
 
 export const UserName = styled.div`
-    text-align: center;
-    font-size: ${({theme}) => theme.size.SIZE_014};
-`
+	text-align: center;
+	font-size: ${({ theme }) => theme.size.SIZE_014};
+`;
+
+export const UserNameBox = styled.div`
+	display: flex;
+	gap: ${({ theme }) => theme.size.SIZE_016};
+	cursor: pointer;
+`;
+
+export const EditIcon = styled(FaPencilAlt)`
+	:hover,
+	:active {
+		color: ${({ theme }) => theme.colors.PURPLE_400};
+	}
+`;
+
+export const ConfirmIcon = styled(FaCheckSquare)`
+	:hover,
+	:active {
+		color: ${({ theme }) => theme.colors.PURPLE_400};
+	}
+`;
+
+export const EditUserNameInput = styled(Input)`
+	height: ${({ theme }) => theme.size.SIZE_016};
+`;
--- frontend/src/pages/MyPage/UserProfile/UserProfile.tsx
+++ frontend/src/pages/MyPage/UserProfile/UserProfile.tsx
@@ -1,15 +1,57 @@
+import React, { useEffect, useState } from 'react';
+
+import Loading from '@/components/common/Loading/Loading';
+import { queryClient } from '@/index';
 import * as S from '@/pages/MyPage/UserProfile/UserProfile.styles';
+import usePutUserProfile from '@/pages/MyPage/UserProfile/hooks/usePutUserProfile';
 
 export interface UserProfileProps {
 	name: string;
 	avatarUrl: string;
 }
 
-const UserProfile = ({ name, avatarUrl }: UserProfileProps) => (
-	<S.Container>
-		<S.UserProfile src={avatarUrl} />
-		<S.UserName>{name}</S.UserName>
-	</S.Container>
-);
+const UserProfile = ({ name, avatarUrl }: UserProfileProps) => {
+	const [isEdit, setIsEdit] = useState(false);
+	const [editedName, setEditedName] = useState(name);
+	const { data, isLoading, isSuccess, onClickConfirmButton } = usePutUserProfile();
+	const onClickEditIcon = () => {
+		setIsEdit(true);
+	};
+
+	const onChangeNameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setEditedName(e.target.value);
+	};
+
+	useEffect(() => {
+		if (isSuccess) {
+			queryClient.invalidateQueries('user-info');
+		}
+	});
+
+	if (isLoading) return <Loading />;
+
+	return (
+		<S.Container>
+			<S.UserProfile src={avatarUrl} />
+			<S.UserNameBox>
+				{isEdit ? (
+					<S.EditUserNameInput value={editedName} onChange={onChangeNameInput} />
+				) : (
+					<S.UserName>{name}</S.UserName>
+				)}
+				{isEdit ? (
+					<S.ConfirmIcon
+						size={25}
+						onClick={() => {
+							onClickConfirmButton({ name: editedName });
+						}}
+					/>
+				) : (
+					<S.EditIcon onClick={onClickEditIcon} />
+				)}
+			</S.UserNameBox>
+		</S.Container>
+	);
+};
 
 export default UserProfile;
