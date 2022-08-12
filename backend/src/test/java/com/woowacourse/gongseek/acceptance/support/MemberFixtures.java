f8e4cac2c3221d60802dbe9f0a781caefeb45d62
giron <60054318+Gyuchool@users.noreply.github.com>
2022-08-12 21:37:42 +0900

    회원 정보 수정하기 기능 구현 (#318)
    * test: Member 도메인 테스트 추가
    
    * feat: 도메인에서 member 이름 수정 기능 구현
    
    * feat: 내 정보 이름 수정하기 기능 구현
    
    * refactor: test naming 수정
    
    * docs: 회원 정보 수정 api 문서화 추가
    
    * style: 코드 포멧팅
    
    * refactor: 메서드 위치를 테스트 픽스처로 위치로 수정
    
    * refactor: logback파일 resources 하위에 위치하도록 수정
    
    * refactor: logback 디렉토리 위치 수정

--- backend/src/test/java/com/woowacourse/gongseek/acceptance/support/MemberFixtures.java
+++ backend/src/test/java/com/woowacourse/gongseek/acceptance/support/MemberFixtures.java
@@ -1,10 +1,12 @@
 package com.woowacourse.gongseek.acceptance.support;
 
 import com.woowacourse.gongseek.auth.presentation.dto.AccessTokenResponse;
+import com.woowacourse.gongseek.member.presentation.dto.MemberUpdateRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 public class MemberFixtures {
 
@@ -34,6 +36,18 @@ public class MemberFixtures {
                 .header(HttpHeaders.AUTHORIZATION, "Bearer " + tokenResponse.getAccessToken())
                 .when()
                 .get("/api/members/me/comments")
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> 이름을_수정한다(AccessTokenResponse tokenResponse, String name) {
+        return RestAssured
+                .given().log().all()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + tokenResponse.getAccessToken())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new MemberUpdateRequest(name))
+                .when()
+                .patch("/api/members/me")
                 .then().log().all()
                 .extract();
     }
