07f776cacb9c7b2377d41cd09f7c7ca875ad0a26
Gyuchool <balance0591@naver.com>
2022-08-12 11:31:24 +0900

    test: Member 도메인 테스트 추가

---
+++ backend/src/test/java/com/woowacourse/gongseek/member/domain/MemberTest.java
@@ -0,0 +1,26 @@
+package com.woowacourse.gongseek.member.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MemberTest {
+
+    @Test
+    void 익명의_유저는_깃허브_Id가_암호화된_Id와_같다() {
+        Member member = new Member("홍길동", "123", "avatar.example");
+
+        assertThat(member.isAnonymous("123")).isTrue();
+    }
+
+    @Test
+    void 깃허브_프로필을_수정한다() {
+        String updatedAvatarUrl = "avatar.update";
+        Member member = new Member("홍길동", "123", "avatar.example");
+        member.updateAvatarUrl(updatedAvatarUrl);
+
+        assertThat(member.getAvatarUrl()).isEqualTo(updatedAvatarUrl);
+
+    }
+}
