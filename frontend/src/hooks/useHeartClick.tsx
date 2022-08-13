11fbc6268ddfe2100d5800d57e25a637032a6186
Hwang Jung Min <hwangstar156@gmail.com>
2022-08-13 15:34:23 +0900

    feat: 성공시에 다시 fetch하는 로직 제거

--- frontend/src/hooks/useHeartClick.tsx
+++ frontend/src/hooks/useHeartClick.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import { useMutation } from 'react-query';
 
 import { deleteLikeArticle, postAddLikeArticle } from '@/api/like';
-import { queryClient } from '@/index';
 
 const useHeartClick = ({
 	prevIsLike,
@@ -43,20 +42,22 @@ const useHeartClick = ({
 	}, [postIsError, deleteIsError]);
 
 	useEffect(() => {
-		if (deleteIsSuccess || postIsSuccess) {
-			queryClient.invalidateQueries('all-articles');
+		if (deleteIsSuccess) {
+			setIsLike(false);
+			setLikeCount((prevLikeCount) => prevLikeCount - 1);
+		}
+
+		if (postIsSuccess) {
+			setIsLike(true);
+			setLikeCount((prevLikeCount) => prevLikeCount + 1);
 		}
 	});
 
 	const onLikeButtonClick = () => {
-		setIsLike(true);
-		setLikeCount((prevLikeCount) => prevLikeCount + 1);
 		postMutate(articleId);
 	};
 
 	const onUnlikeButtonClick = () => {
-		setIsLike(false);
-		setLikeCount((prevLikeCount) => prevLikeCount - 1);
 		deleteMutate(articleId);
 	};
 
