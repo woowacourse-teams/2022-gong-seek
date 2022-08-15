2dcc7736620ed92c9d62849f8b954159f3df2ab3
JASUN LEE <liswktjs@gmail.com>
2022-08-15 20:38:02 +0900

    fix: 잘못된 곳에서 import 받고 있는 useEffect에 대한 수정

--- frontend/src/pages/Login/RefreshTokenHandler/RefreshTokenHandler.tsx
+++ frontend/src/pages/Login/RefreshTokenHandler/RefreshTokenHandler.tsx
@@ -1,17 +1,18 @@
 import { AxiosError } from 'axios';
+import { useEffect } from 'react';
 import { useQuery } from 'react-query';
 
 import { getAccessTokenByRefreshToken } from '@/api/login';
 import CustomError from '@/components/helper/CustomError';
 import { ErrorMessage } from '@/constants/ErrorMessage';
 import useSnackBar from '@/hooks/useSnackBar';
-import { useEffect } from '@storybook/addons';
 
 const RefreshTokenHandler = () => {
 	const { data, isSuccess, isError, error } = useQuery<
 		{ accessToken: string },
 		AxiosError<{ errorCode: keyof typeof ErrorMessage; message: string }>
 	>('getBack-accessToken', getAccessTokenByRefreshToken);
+
 	const { showSnackBar } = useSnackBar();
 
 	useEffect(() => {
@@ -33,8 +34,8 @@ const RefreshTokenHandler = () => {
 	useEffect(() => {
 		if (isSuccess) {
 			localStorage.setItem('accessToken', data.accessToken);
-			showSnackBar('재로그인 되었습니다');
 			location.href = '/';
+			showSnackBar('재로그인 되었습니다');
 		}
 	}, [isSuccess]);
 
