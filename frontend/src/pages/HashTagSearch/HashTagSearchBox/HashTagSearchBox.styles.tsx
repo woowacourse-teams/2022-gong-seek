6649a334193d9595f8f9493ec8a9ee89f569fa38
JASUN LEE <liswktjs@gmail.com>
2022-08-15 10:38:04 +0900

    feat: HashTagSearchBox 컴포넌트 구현 및 스타일링

--- frontend/src/pages/HashTagSearch/HashTagSearchBox/HashTagSearchBox.styles.tsx
+++ frontend/src/pages/HashTagSearch/HashTagSearchBox/HashTagSearchBox.styles.tsx
@@ -1,5 +1,59 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.section``;
+export const Container = styled.section`
+	display: flex;
+	flex-direction: column;
 
-export const HashTagItem = styled.div``;
+	width: 100%;
+`;
+
+export const HashTagPreviewBox = styled.div``;
+
+export const HashTagLists = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+
+	width: 100%;
+
+	gap: ${({ theme }) => theme.size.SIZE_004};
+`;
+
+export const HashTagItem = styled.button<{ isChecked: boolean }>`
+	width: fit-content;
+	height: fit-content;
+
+	border: ${({ theme }) => theme.size.SIZE_001} solid;
+	border-color: ${({ theme, isChecked }) =>
+		isChecked ? theme.colors.PURPLE_500 : theme.colors.BLACK_400};
+	border-radius: ${({ theme }) => theme.size.SIZE_004};
+
+	background-color: transparent;
+
+	padding: ${({ theme }) => theme.size.SIZE_004};
+
+	color: ${({ isChecked, theme }) =>
+		isChecked ? theme.colors.PURPLE_500 : theme.colors.BLACK_500};
+
+	&:hover,
+	&:active {
+		cursor: pointer;
+	}
+`;
+
+export const HashTagButton = styled.button`
+	width: fit-content;
+	height: fit-content;
+
+	border: ${({ theme }) => theme.size.SIZE_001} solid ${({ theme }) => theme.colors.BLACK_400};
+	border-radius: ${({ theme }) => theme.size.SIZE_004};
+	padding: ${({ theme }) => theme.size.SIZE_004};
+
+	background-color: transparent;
+
+	&:hover,
+	&:active {
+		cursor: pointer;
+	}
+`;
+
+export const EmptyMsg = styled.div``;
