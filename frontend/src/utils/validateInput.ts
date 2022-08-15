2b0e0a3e5680ef69513a1ac6c08939298d8a2a02
JASUN LEE <liswktjs@gmail.com>
2022-08-15 22:55:32 +0900

    fix: 유실된 해시태그 검증하는 함수 추가

--- frontend/src/utils/validateInput.ts
+++ frontend/src/utils/validateInput.ts
@@ -2,3 +2,6 @@ export const validatedSearchInput = (target: string) => target.length >= 2;
 
 export const validatedTitleInput = (titleInput: string) =>
 	titleInput.length >= 1 && titleInput.length <= 500;
+
+export const validatedHashTagInput = (targetInput: string) =>
+	targetInput.length >= 2 && targetInput.length <= 20;
