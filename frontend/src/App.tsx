531d7b20d770046a9c3bc68b0e380789d9ee0c49
Jungmin Hwang <85891751+hwangstar156@users.noreply.github.com>
2022-08-16 15:03:15 +0900

    글쓰기에서 깨진 ui 수정  (#359)
    * fix: 글쓰기에서 깨진 ui를 수정
    
    * fix: min-height값 설정
    
    Co-authored-by: JASUN LEE <liswktjs@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -34,8 +34,8 @@ const Layout = styled.div`
 
 const Content = styled.main`
 	width: 100%;
+	min-height: calc(100vh - 2 * ${({ theme }) => theme.size.SIZE_126});
 	padding-bottom: 7rem;
-	height: calc(100vh - 2 * ${({ theme }) => theme.size.SIZE_126});
 
 	@media (min-width: ${({ theme }) => theme.breakpoints.DESKTOP}) {
 		width: calc(100% - ${({ theme }) => theme.size.SIZE_160} * 2);
