57464bd82fbe83aa0f1ea016f4c0f2013e29f522
Jungmin Hwang <85891751+hwangstar156@users.noreply.github.com>
2022-08-16 21:18:09 +0900

    마이페이지에 이름 수정 기능을 구현한다. (#292)
    * feat: UserProfile에 수정할수 있는 UI 추가
    
    * feat: 마이페이지 수정에 대한 api 작성
    
    * feat: 마이페이지 수정기능에 대한 mock api 작성
    
    * feat: 마이페이지 수정 기능에 대한 로직 커스텀 훅으로 분리
    
    * feat: checkBox 사이즈 조절
    
    * feat: useEffect의 의존성 배열에 isSuccess 추가
    
    * feat: editInput에 대한 유효메시지 추가
    
    * feat: put -> patch로 api 요청 변경

--- frontend/src/pages/MyPage/UserProfile/UserProfile.tsx
+++ frontend/src/pages/MyPage/UserProfile/UserProfile.tsx
@@ -1,15 +1,66 @@
+import React, { useEffect, useState } from 'react';
+
+import Loading from '@/components/common/Loading/Loading';
+import { queryClient } from '@/index';
 import * as S from '@/pages/MyPage/UserProfile/UserProfile.styles';
+import usePutUserProfile from '@/pages/MyPage/UserProfile/hooks/usePutUserProfile';
+import { validatedEditInput } from '@/utils/validateInput';
 
 export interface UserProfileProps {
 	name: string;
 	avatarUrl: string;
 }
 
-const UserProfile = ({ name, avatarUrl }: UserProfileProps) => (
-	<S.Container>
-		<S.UserProfile src={avatarUrl} />
-		<S.UserName>{name}</S.UserName>
-	</S.Container>
-);
+const UserProfile = ({ name, avatarUrl }: UserProfileProps) => {
+	const [isEdit, setIsEdit] = useState(false);
+	const [editedName, setEditedName] = useState(name);
+	const { data, isLoading, isSuccess, onClickConfirmButton } = usePutUserProfile();
+	const isValidInput = validatedEditInput(editedName);
+
+	const onClickEditIcon = () => {
+		setIsEdit(true);
+	};
+
+	useEffect(() => {
+		if (isSuccess) {
+			queryClient.invalidateQueries('user-info');
+		}
+	}, [isSuccess]);
+
+	if (isLoading) return <Loading />;
+
+	return (
+		<S.Container>
+			<S.UserProfile src={avatarUrl} />
+			<S.UserNameBox>
+				{isEdit ? (
+					<S.EditUserNameBox>
+						<S.EditUserNameInput
+							value={editedName}
+							onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+								setEditedName(e.target.value);
+							}}
+						/>
+						<S.ValidateMessage isValid={isValidInput}>
+							{isValidInput ? '유효한 입력입니다.' : '다시 입력해주세요'}
+						</S.ValidateMessage>
+					</S.EditUserNameBox>
+				) : (
+					<S.UserName>{name}</S.UserName>
+				)}
+				{isEdit ? (
+					<S.ConfirmIcon
+						size={20}
+						onClick={() => {
+							onClickConfirmButton({ name: editedName });
+						}}
+					/>
+				) : (
+					<S.EditIcon onClick={onClickEditIcon} />
+				)}
+			</S.UserNameBox>
+		</S.Container>
+	);
+};
 
 export default UserProfile;
---
+++ frontend/src/pages/MyPage/UserProfile/hooks/usePutUserProfile.tsx
@@ -0,0 +1,35 @@
+import { AxiosError, AxiosResponse } from 'axios';
+import { useEffect } from 'react';
+import { useMutation } from 'react-query';
+
+import { editUserInfo } from '@/api/myPage';
+import CustomError from '@/components/helper/CustomError';
+import { ErrorMessage } from '@/constants/ErrorMessage';
+
+const usePutUserProfile = () => {
+	const { data, isSuccess, isLoading, isError, error, mutate } = useMutation<
+		AxiosResponse<{ name: string }>,
+		AxiosError<{ errorCode: keyof typeof ErrorMessage; message: string }>,
+		{ name: string }
+	>(editUserInfo);
+
+	useEffect(() => {
+		if (isError) {
+			if (!error.response) {
+				return;
+			}
+			throw new CustomError(
+				error.response.data.errorCode,
+				ErrorMessage[error.response.data.errorCode],
+			);
+		}
+	}, [isError]);
+
+	const onClickConfirmButton = ({ name }: { name: string }) => {
+		mutate({ name });
+	};
+
+	return { onClickConfirmButton, isLoading, isSuccess, data };
+};
+
+export default usePutUserProfile;
