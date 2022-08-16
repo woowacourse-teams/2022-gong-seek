1302b4ab1115339c0348770a920995f7d6e365d5
윤주리 <78091011+jurlring@users.noreply.github.com>
2022-08-16 20:36:43 +0900

    로그 설정 수정 (#375)
    * refactor: 로그 설정 수정
    
    * docs: 불필요한 설정 제거

--- backend/src/main/java/com/woowacourse/gongseek/config/WebConfig.java
+++ backend/src/main/java/com/woowacourse/gongseek/config/WebConfig.java
@@ -1,5 +1,7 @@
 package com.woowacourse.gongseek.config;
 
+import ch.qos.logback.access.servlet.TeeFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -23,5 +25,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public FilterRegistrationBean<TeeFilter> requestLoggingFilter() {
+        return new FilterRegistrationBean<>(new TeeFilter());
     }
 }
