3c187336be1fb841fc6232da0bc041e5b62ac5ec
JASUN LEE <liswktjs@gmail.com>
2022-08-17 11:50:15 +0900

    fix: 변경된 검색 api 반영 및 경우에 따른 함수 분리

--- frontend/src/api/search.ts
+++ frontend/src/api/search.ts
@@ -3,37 +3,19 @@ import axios from 'axios';
 import { HOME_URL } from '@/constants/url';
 import { SearchResultType } from '@/types/searchResponse';
 
-export const getSearchResult = async ({
+export const getUserSearchResult = async ({
+	accessToken,
 	target,
+	cursorId,
 	searchIndex,
-	cursorId = '',
 }: {
+	accessToken: string | null;
 	target: string;
-	searchIndex: string;
 	cursorId: string;
+	searchIndex: string;
 }) => {
-	const accessToken = localStorage.getItem('accessToken');
-	if (searchIndex === '유저') {
-		const { data } = await axios.get<SearchResultType>(
-			`${HOME_URL}/api/articles/searchAuthor?author=${target}&cursorId=${cursorId}&pageSize=5`,
-			{
-				headers: {
-					'Access-Control-Allow-Origin': '*',
-					Authorization: `Bearer ${accessToken}`,
-				},
-			},
-		);
-
-		return {
-			articles: data.articles,
-			hasNext: data.hasNext,
-			cursorId: String(data.articles[data.articles.length - 1].id),
-			target: target,
-			searchIndex,
-		};
-	}
 	const { data } = await axios.get<SearchResultType>(
-		`${HOME_URL}/api/articles/searchText?text=${target}&cursorId=${cursorId}&pageSize=5`,
+		`${HOME_URL}/api/articles/author?author=${target}&cursorId=${cursorId}&pageSize=5`,
 		{
 			headers: {
 				'Access-Control-Allow-Origin': '*',
@@ -49,4 +31,52 @@ export const getSearchResult = async ({
 		target: target,
 		searchIndex,
 	};
+};
+
+export const getArticleSearchResult = async ({
+	accessToken,
+	target,
+	cursorId,
+	searchIndex,
+}: {
+	accessToken: string | null;
+	target: string;
+	cursorId: string;
+	searchIndex: string;
+}) => {
+	const { data } = await axios.get<SearchResultType>(
+		`${HOME_URL}/api/articles/search/text?text=${target}&cursorId=${cursorId}&pageSize=5`,
+		{
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				Authorization: `Bearer ${accessToken}`,
+			},
+		},
+	);
+
+	return {
+		articles: data.articles,
+		hasNext: data.hasNext,
+		cursorId: String(data.articles[data.articles.length - 1].id),
+		target: target,
+		searchIndex,
+	};
+};
+
+export const getSearchResult = async ({
+	target,
+	searchIndex,
+	cursorId = '',
+}: {
+	target: string;
+	searchIndex: string;
+	cursorId: string;
+}) => {
+	const accessToken = localStorage.getItem('accessToken');
+	if (searchIndex === '유저') {
+		const data = await getUserSearchResult({ accessToken, target, searchIndex, cursorId });
+		return data;
+	}
+	const data = await getArticleSearchResult({ accessToken, target, searchIndex, cursorId });
+	return data;
 };
