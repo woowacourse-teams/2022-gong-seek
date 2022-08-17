ec87c52eae39fa10c1711d10f4d1aa710fed5ecb
JASUN LEE <liswktjs@gmail.com>
2022-08-17 12:18:26 +0900

    refactor: 로그인 상태일 때에 nav의 아이템 순서 조정

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -49,17 +49,14 @@ const Header = () => {
 			</S.HeaderSection>
 			<S.NavBar>
 				{isLogin ? (
-					<>
-						<S.NavBarItem to="/my-page">마이페이지</S.NavBarItem>
-						<S.LogOutItem onClick={onLogOutClick}>로그아웃</S.LogOutItem>
-					</>
+					<S.LogOutItem onClick={onLogOutClick}>로그아웃</S.LogOutItem>
 				) : (
 					<S.NavBarItem to="/login">로그인</S.NavBarItem>
 				)}
-
 				<S.NavBarItem to="/category">글 쓰러 가기</S.NavBarItem>
 				<S.NavBarItem to="/articles/question">질문 카테고리</S.NavBarItem>
 				<S.NavBarItem to="/articles/discussion">토론 카테고리</S.NavBarItem>
+				{isLogin && <S.NavBarItem to="/my-page">마이페이지</S.NavBarItem>}
 				<S.NavBarItem to="/inquire">문의하기</S.NavBarItem>
 			</S.NavBar>
 		</S.Container>
