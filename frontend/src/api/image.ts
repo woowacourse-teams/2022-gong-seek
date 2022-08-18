6e891aab521653f38b0b39fedbe3669623cd369a
Jungmin Hwang <85891751+hwangstar156@users.noreply.github.com>
2022-08-19 00:56:28 +0900

    toast ui의 imageUrl 길이를 줄인다. (#427)
    * 이미지 변환하는 함수 추가
    
    * feat: 백엔드와 연결하여 이미지 변환이 작동하도록 코드 변환
    
    * feat: 메서드명 알맞게변경

---
+++ frontend/src/api/image.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+import { IMAGE_URL } from '@/constants/url';
+
+export const postImageUrlConverter = async (formData: FormData) => {
+	const accessToken = localStorage.getItem('accessToken');
+
+	const response = await axios.post(`${IMAGE_URL}/api/images/upload`, formData, {
+		headers: {
+			'Content-Type': 'multipart/form-data',
+			Authorization: `Bearer ${accessToken}`,
+		},
+	});
+
+	return response.data.imageUrl;
+};
