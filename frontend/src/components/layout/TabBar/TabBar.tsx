2486e5e08936b74067c5c52b3688649da1e08a5a
Hwang Jung Min <hwangstar156@gmail.com>
2022-08-30 17:04:05 +0900

    feat: 반응형을 고려하여 UserProfile 설정

--- frontend/src/components/layout/TabBar/TabBar.tsx
+++ frontend/src/components/layout/TabBar/TabBar.tsx
@@ -1,12 +1,21 @@
 import { useNavigate } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
+import Loading from '@/components/common/Loading/Loading';
+import { UserProfile } from '@/components/common/UserProfileIcon/UserProfileIcon.styles';
 import * as S from '@/components/layout/TabBar/TabBar.styles';
+import useGetUserInfo from '@/pages/MyPage/hooks/useGetUserInfo';
 import { menuSliderState } from '@/store/menuSliderState';
+import { getUserIsLogin } from '@/store/userState';
 
 const TabBar = () => {
 	const navigate = useNavigate();
+	const isLogin = useRecoilValue(getUserIsLogin);
+
 	const [sliderState, setSliderState] = useRecoilState(menuSliderState);
+	const { data, isLoading } = useGetUserInfo();
+
+	if (isLoading) return <Loading />;
 
 	return (
 		<>
@@ -16,11 +25,16 @@ const TabBar = () => {
 						navigate('/category');
 					}}
 				/>
-				<S.UserCircleLink
-					onClick={() => {
-						navigate('/my-page');
-					}}
-				/>
+				{isLogin ? (
+					<UserProfile src={data?.avatarUrl} onClick={() => navigate('/my-page')} />
+				) : (
+					<S.UserCircleLink
+						onClick={() => {
+							navigate('/my-page');
+						}}
+					/>
+				)}
+
 				<S.MenuLink
 					onClick={() => {
 						setSliderState({ isOpen: true });
