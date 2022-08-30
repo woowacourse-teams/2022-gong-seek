f392f311695fb5d1982b28ea3761ef97652b2335
Hwang Jung Min <hwangstar156@gmail.com>
2022-08-30 16:33:19 +0900

    feat: UserProfileIcon component 추가

---
+++ frontend/src/components/common/UserProfileIcon/UserProfileIcon.tsx
@@ -0,0 +1,34 @@
+import Dropdown from '../Dropdown/Dropdown';
+import { useState } from 'react';
+import { useRecoilState } from 'recoil';
+
+import Loading from '@/components/common/Loading/Loading';
+import * as S from '@/components/common/UserProfileIcon/UserProfileIcon.styles';
+import useGetUserInfo from '@/pages/MyPage/hooks/useGetUserInfo';
+import { dropdownState } from '@/store/dropdownState';
+
+const UserProfileIcon = () => {
+	const { data, isSuccess, isLoading } = useGetUserInfo();
+	const [dropdown, setDropdown] = useRecoilState(dropdownState);
+	if (isLoading) {
+		return <Loading />;
+	}
+
+	return (
+		<S.Container>
+			{isSuccess && (
+				<S.UserProfile
+					src={data?.avatarUrl}
+					onClick={() =>
+						setDropdown((prev) => ({
+							isOpen: !prev.isOpen,
+						}))
+					}
+				/>
+			)}
+			{dropdown.isOpen && <Dropdown onClickCloseDropdown={() => setDropdown({ isOpen: false })} />}
+		</S.Container>
+	);
+};
+
+export default UserProfileIcon;
