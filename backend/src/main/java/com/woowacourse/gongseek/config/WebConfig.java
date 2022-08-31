28cdcbfcfc93c13a5a4afdff8cf5a72673b3f4dd
brorae <jhr3239@gmail.com>
2022-08-31 18:14:05 +0900

    refactor: CORS 설정 추가

--- backend/src/main/java/com/woowacourse/gongseek/config/WebConfig.java
+++ backend/src/main/java/com/woowacourse/gongseek/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://gongseek.site", "http://localhost:3000")
+                .allowedOrigins("https://front.dev.gongseek.site", "https://gongseek.site", "http://localhost:3000")
                 .allowCredentials(true)
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .exposedHeaders("*");
