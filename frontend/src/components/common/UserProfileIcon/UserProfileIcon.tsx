62a9c091f4216f2e3d013610b7a741dd3b5e9f55
Jungmin Hwang <85891751+hwangstar156@users.noreply.github.com>
2022-09-08 11:11:50 +0900

    로그인, 마이페이지를 헤더에 보여주는것에서 드롭다운으로 변경한다. (#455)
    * feat: UserProfileIcon component 추가
    
    * feat: dropdown component 추가
    
    * feat: dropdownState를 이용하여 전역으로 dropdown을 열고 닫을수 있도록 관리
    
    * feat: profile에 대한 header 스타일링 변경및 컴포넌트 변경
    
    * feat: dropdown, userProfile의 스타일링 수정
    
    * feat: 반응형을 고려하여 UserProfile 설정
    
    * refactor: 상대경로 수정
    
    * refactor: 절대 경로 수정
    
    * feat: loading상태일 때는 default 이미지를 보여주도록 수정

---
+++ frontend/src/components/common/UserProfileIcon/UserProfileIcon.tsx
@@ -0,0 +1,37 @@
+import { useRecoilState } from 'recoil';
+
+import gongseek from '@/assets/gongseek.png';
+import Dropdown from '@/components/common/Dropdown/Dropdown';
+import Loading from '@/components/common/Loading/Loading';
+import * as S from '@/components/common/UserProfileIcon/UserProfileIcon.styles';
+import useGetUserInfo from '@/pages/MyPage/hooks/useGetUserInfo';
+import { dropdownState } from '@/store/dropdownState';
+
+const UserProfileIcon = () => {
+	const { data, isSuccess, isLoading } = useGetUserInfo();
+	const [dropdown, setDropdown] = useRecoilState(dropdownState);
+
+	if (isLoading) {
+		return <Loading />;
+	}
+
+	return (
+		<S.Container>
+			{isLoading ? (
+				<S.UserProfile src={gongseek} />
+			) : (
+				<S.UserProfile
+					src={data?.avatarUrl}
+					onClick={() =>
+						setDropdown((prev) => ({
+							isOpen: !prev.isOpen,
+						}))
+					}
+				/>
+			)}
+			{dropdown.isOpen && <Dropdown onClickCloseDropdown={() => setDropdown({ isOpen: false })} />}
+		</S.Container>
+	);
+};
+
+export default UserProfileIcon;
--- frontend/src/components/layout/TabBar/TabBar.tsx
+++ frontend/src/components/layout/TabBar/TabBar.tsx
@@ -1,12 +1,19 @@
 import { useNavigate } from 'react-router-dom';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
+import gongseek from '@/assets/gongseek.png';
+import { UserProfile } from '@/components/common/UserProfileIcon/UserProfileIcon.styles';
 import * as S from '@/components/layout/TabBar/TabBar.styles';
+import useGetUserInfo from '@/pages/MyPage/hooks/useGetUserInfo';
 import { menuSliderState } from '@/store/menuSliderState';
+import { getUserIsLogin } from '@/store/userState';
 
 const TabBar = () => {
 	const navigate = useNavigate();
+	const isLogin = useRecoilValue(getUserIsLogin);
+
 	const [sliderState, setSliderState] = useRecoilState(menuSliderState);
+	const { data, isLoading } = useGetUserInfo();
 
 	return (
 		<>
@@ -16,11 +23,24 @@ const TabBar = () => {
 						navigate('/category');
 					}}
 				/>
-				<S.UserCircleLink
-					onClick={() => {
-						navigate('/my-page');
-					}}
-				/>
+				{isLogin ? (
+					isLoading ? (
+						<UserProfile src={gongseek} />
+					) : (
+						<UserProfile
+							src={data?.avatarUrl}
+							alt={gongseek}
+							onClick={() => navigate('/my-page')}
+						/>
+					)
+				) : (
+					<S.UserCircleLink
+						onClick={() => {
+							navigate('/my-page');
+						}}
+					/>
+				)}
+
 				<S.MenuLink
 					onClick={() => {
 						setSliderState({ isOpen: true });
