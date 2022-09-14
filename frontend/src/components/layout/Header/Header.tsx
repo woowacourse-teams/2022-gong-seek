ab5234367d0f2e47b9534bee1aa820a427d96dda
JASUN LEE <liswktjs@gmail.com>
2022-09-14 14:38:32 +0900

    fix: 헤더 안내 문구 수정

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -45,9 +45,9 @@ const Header = () => {
 			</S.HeaderSection>
 			<S.NavBar>
 				<S.NavBarItem to={URL.CATEGORY_SELECTOR}>글 쓰러 가기</S.NavBarItem>
-				<S.NavBarItem to={URL.CATEGORY_QUESTION}>질문 카테고리</S.NavBarItem>
-				<S.NavBarItem to={URL.CATEGORY_DISCUSSION}>토론 카테고리</S.NavBarItem>
-				<S.NavBarItem to={URL.HASH_TAG_SEARCH}>해시태그로 검색하기</S.NavBarItem>
+				<S.NavBarItem to={URL.CATEGORY_QUESTION}>질문 게시판</S.NavBarItem>
+				<S.NavBarItem to={URL.CATEGORY_DISCUSSION}>토론 게시판</S.NavBarItem>
+				<S.NavBarItem to={URL.HASH_TAG_SEARCH}>해시태그</S.NavBarItem>
 				<S.NavBarItem to={URL.INQUIRE}>문의하기</S.NavBarItem>
 				{isLogin ? (
 					<UserProfileIcon />
