e2c9b56b9aa91794c04fe764f68a1e9c61b51faf
JASUN LEE <liswktjs@gmail.com>
2022-09-15 21:21:37 +0900

    fix: img에 alt문구 추가함

--- frontend/src/pages/NotFound/index.tsx
+++ frontend/src/pages/NotFound/index.tsx
@@ -3,7 +3,7 @@ import * as S from '@/pages/NotFound/index.styles';
 
 const NotFound = () => (
 	<S.Container>
-		<S.LogoImage src={gongseek} />
+		<S.LogoImage src={gongseek} alt="로고 이미지" />
 		<S.ErrorContent>
 			찾을수 없는 페이지입니다.
 			<br /> 경로를 다시 확인해보세요 :D
