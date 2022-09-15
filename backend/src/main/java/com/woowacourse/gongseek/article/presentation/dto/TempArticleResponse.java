36a6febec34e3f5d818976945d48ea8550093fe7
이한울 <dkfghg3@naver.com>
2022-09-15 18:40:26 +0900

    refactor: createAt Json 포멧 설정

--- backend/src/main/java/com/woowacourse/gongseek/article/presentation/dto/TempArticleResponse.java
+++ backend/src/main/java/com/woowacourse/gongseek/article/presentation/dto/TempArticleResponse.java
@@ -1,5 +1,6 @@
 package com.woowacourse.gongseek.article.presentation.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.woowacourse.gongseek.article.domain.TempArticle;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
@@ -13,10 +14,14 @@ import lombok.NoArgsConstructor;
 public class TempArticleResponse {
 
     private long id;
+
     private String title;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime createAt;
 
     public static TempArticleResponse from(TempArticle tempArticle) {
-        return new TempArticleResponse(tempArticle.getId(), tempArticle.getTitle().getValue(), tempArticle.getCreatedAt());
+        return new TempArticleResponse(tempArticle.getId(), tempArticle.getTitle().getValue(),
+                tempArticle.getCreatedAt());
     }
 }
