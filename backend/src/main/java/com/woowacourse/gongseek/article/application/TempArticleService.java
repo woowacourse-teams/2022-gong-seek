dd6e746bda700b2dd9f8b11380a63f035bda8624
이한울 <dkfghg3@naver.com>
2022-09-15 11:19:27 +0900

    refactor: Service Transactional readOnly 어노테이션 추가

--- backend/src/main/java/com/woowacourse/gongseek/article/application/TempArticleService.java
+++ backend/src/main/java/com/woowacourse/gongseek/article/application/TempArticleService.java
@@ -20,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class TempArticleService {
