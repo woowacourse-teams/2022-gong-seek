019dfa7c6e84eba4454c14753fa22fcc9f7d6263
Jungmin Hwang <85891751+hwangstar156@users.noreply.github.com>
2022-09-16 15:24:24 +0900

    투표 글 작성시 에러 메세지 보여주기 (#483)
    * feat: 투표 목록에 대한 예외상황을 메시지로 보여줄수 있도록 기능 추가
    
    * feat: 예외처리시에 다시 input으로 포커스 할수 있도록 수정
    
    * feat: 투표 목록 최대 글자수에 대한 예외처리 추가

--- frontend/src/utils/validateInput.ts
+++ frontend/src/utils/validateInput.ts
@@ -8,5 +8,17 @@ export const validatedHashTagInput = (targetInput: string) =>
 
 export const validatedEditInput = (targetInput: string) => targetInput.length >= 1;
 
+export const validatedVoteItemInputLength = (targetInput: string) =>
+	targetInput.length > 0 && targetInput.length <= 300;
+
+export const validatedDuplicatedVoteItemInput = (targetInput: string, options: string[]) => {
+	const checkArray = options.concat(targetInput);
+	const checkSet = new Set(checkArray);
+
+	return checkSet.size === checkArray.length;
+};
+
+export const validatedVoteItemsQuantity = (options: string[]) => options.length >= 2;
+
 export const validatedCommentInput = (targetInput: string) =>
 	targetInput.length >= 1 && targetInput.length <= 10000;
