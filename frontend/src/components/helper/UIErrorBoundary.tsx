9a76e4a392ca7d72b12736477de6be5a43140b89
hwangjungmin <hwangstar156@gmail.com>
2022-09-18 18:48:22 +0900

    feat: 에러가 날시 UI를 보여주는것으로 처리하는 UIErrorBoundary 구현

---
+++ frontend/src/components/helper/UIErrorBoundary.tsx
@@ -0,0 +1,37 @@
+import CommonErrorBoundary, { ErrorBoundaryProps, ErrorBoundaryState } from './CommonErrorBoundary';
+
+interface UIErrorBoundaryProps {
+	serverErrorFallback: React.ReactNode;
+	NotFoundErrorFallback: React.ReactNode;
+}
+
+class UIErrorBoundary extends CommonErrorBoundary<UIErrorBoundaryProps> {
+	constructor(props: UIErrorBoundaryProps & ErrorBoundaryProps) {
+		super(props);
+	}
+
+	componentDidUpdate(_: never, prevState: ErrorBoundaryState) {
+		if (prevState.error === this.state.error || !this.state.error) {
+			return;
+		}
+
+		if (prevState.error !== null) {
+			this.reset();
+		}
+	}
+
+	render() {
+		const { children, serverErrorFallback, NotFoundErrorFallback } = this.props;
+
+		if (this.state.error) {
+			if (String(this.state.error.errorCode) === '0000') {
+				return serverErrorFallback;
+			}
+			return NotFoundErrorFallback;
+		}
+
+		return children;
+	}
+}
+
+export default UIErrorBoundary;
