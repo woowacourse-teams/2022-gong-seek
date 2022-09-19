27e29477690205680e387dc94bdb8db6b57c2e40
JASUN LEE <liswktjs@gmail.com>
2022-09-19 16:59:48 +0900

    fix: 시간을 변환하는 함수 적용

--- frontend/src/pages/TemporaryArticles/TemporaryArticleItem/TemporaryArticleItem.tsx
+++ frontend/src/pages/TemporaryArticles/TemporaryArticleItem/TemporaryArticleItem.tsx
@@ -1,4 +1,5 @@
 import * as S from '@/pages/TemporaryArticles/TemporaryArticleItem/TemporaryArticleItem.styles';
+import { dateTimeConverter } from '@/utils/converter';
 
 export interface TemporaryArticleItemProps {
 	article: { title: string; createdAt: string };
@@ -8,7 +9,7 @@ export interface TemporaryArticleItemProps {
 const TemporaryArticleItem = ({ article, onClick }: TemporaryArticleItemProps) => (
 	<S.Container onClick={onClick}>
 		<S.Title>{article.title}</S.Title>
-		<S.CreatedAt>{article.createdAt}</S.CreatedAt>
+		<S.CreatedAt>{dateTimeConverter(article.createdAt)}</S.CreatedAt>
 	</S.Container>
 );
 
