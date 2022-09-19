077c251241827e7cdfc43bc66149aa2cba33f041
JASUN LEE <liswktjs@gmail.com>
2022-09-19 15:38:40 +0900

    fix: storybook의 props에 onClick 추가

--- frontend/src/storybook/Article/TemporaryArticleItem.stories.tsx
+++ frontend/src/storybook/Article/TemporaryArticleItem.stories.tsx
@@ -1,6 +1,7 @@
 import TemporaryArticleItem, {
 	TemporaryArticleItemProps,
 } from '@/pages/TemporaryArticles/TemporaryArticleItem/TemporaryArticleItem';
+import { action } from '@storybook/addon-actions';
 import { Meta, Story } from '@storybook/react';
 
 export default {
@@ -24,5 +25,8 @@ Default.args = {
 		title:
 			'글 상세페이지에서의 글 제목이 들어가는 곳, 글 제목이 2줄 이상이 넘어갔을때 어떻게 처리할 것인지 처리하기 위한 예시 문장입니다',
 		createdAt: '2022-08-11T13:34:11',
+	},
+	onClick: () => {
+		action('clicked');
 	},
 };
