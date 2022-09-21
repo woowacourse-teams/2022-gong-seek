9635e23771dea1584be175879b3dacd333038565
hwangjungmin <hwangstar156@gmail.com>
2022-09-21 16:16:45 +0900

    feat: hashtag에서도 반응형이 적용되도록 추가

--- frontend/src/pages/HashTagSearch/HashTagSearchResult/HashTagSearchResult.styles.tsx
+++ frontend/src/pages/HashTagSearch/HashTagSearchResult/HashTagSearchResult.styles.tsx
@@ -32,6 +32,16 @@ export const SearchResult = styled.div`
 
 	gap: ${({ theme }) => theme.size.SIZE_014};
 
+	@media (min-width: ${({ theme }) => theme.breakpoints.DESKTOP_SMALL}) {
+		display: grid;
+		width: 100%;
+		grid-template-columns: 1fr 1fr;
+		place-items: center;
+		margin: 0 auto;
+		gap: ${({ theme }) => theme.size.SIZE_022};
+		margin-top: ${({ theme }) => theme.size.SIZE_040};
+	}
+
 	@media (min-width: ${({ theme }) => theme.breakpoints.DESKTOP_LARGE}) {
 		display: grid;
 		width: 100%;
