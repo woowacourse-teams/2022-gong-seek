0f37f03f39529e6eb768b8e64e6ff17cfb54298d
Jungmin Hwang <85891751+hwangstar156@users.noreply.github.com>
2022-09-22 11:38:23 +0900

    투표에 대한 UI를 수정한다. (#581)
    * feat: 데스크톱 환경에서의 투표 UI 수정
    
    * feat: 모바일일때 Input이 밑으로 항목이 위로 오도록 수정
    
    * feat: 투표 항목은 2개에서 5개까지만 할수 있도록 설정
    
    * feat: 투표 목록 입력에서 많은 글자를 입력시 삭제 버튼이 안보이는 문제 수정
    
    * feat: 한글 입력시 height가 깨져보이는 에러 수정

--- frontend/src/pages/VoteGenerator/AddedOption/AddedOption.tsx
+++ frontend/src/pages/VoteGenerator/AddedOption/AddedOption.tsx
@@ -5,7 +5,7 @@ import styled from '@emotion/styled';
 
 const AddedOption = ({ children, onClick }: { children: React.ReactNode; onClick: () => void }) => (
 	<OptionBox>
-		<div>{children}</div>
+		<Text>{children}</Text>
 		<Trash onClick={onClick} />
 	</OptionBox>
 );
@@ -24,8 +24,12 @@ const OptionBox = styled.div`
 
 	box-shadow: 1px 1px 4px ${({ theme }) => theme.boxShadows.primary};
 	padding: 0 ${({ theme }) => theme.size.SIZE_012};
+`;
 
-	overflow-x: scroll;
+const Text = styled.div`
+	width: 90%;
+	overflow-x: auto;
+	white-space: nowrap;
 `;
 
 const Trash = styled(AiFillDelete)`
