bce46cbf8d011608ce55e91ea81b5c125426ca79
JASUN LEE <liswktjs@gmail.com>
2022-09-22 13:16:42 +0900

    type: mutate관련 타입 지정

--- frontend/src/components/helper/types/ErrorBoundary.type.ts
+++ frontend/src/components/helper/types/ErrorBoundary.type.ts
@@ -1,6 +1,9 @@
+import { AxiosError, AxiosResponse } from 'axios';
+import { UseMutateFunction } from 'react-query';
 import { NavigateFunction } from 'react-router-dom';
 
 import CustomError from '@/components/helper/CustomError';
+import { ErrorMessage } from '@/constants/ErrorMessage';
 
 export interface ErrorBoundaryProps {
 	children?: React.ReactNode;
@@ -13,6 +16,18 @@ export interface ErrorBoundaryState {
 export interface LogicErrorBoundaryProps extends ErrorBoundaryProps {
 	showSnackBar?: (message: string) => void;
 	navigate?: NavigateFunction;
+	mutateDeleteRefreshToken?: UseMutateFunction<
+		AxiosResponse<null, any>,
+		AxiosError<
+			{
+				errorCode: keyof typeof ErrorMessage;
+				message: string;
+			},
+			any
+		>,
+		void,
+		unknown
+	>;
 }
 
 export interface FallbackErrorBoundaryProps {
