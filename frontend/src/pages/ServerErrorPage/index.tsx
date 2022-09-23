df1480eec390ba2158c6b0447562ab4429c60897
JASUN LEE <liswktjs@gmail.com>
2022-09-23 12:58:52 +0900

    fix: 서버 에러 페이지에서 홈으로 가는 버튼 제작
    서버 에러 페이지에서 홈으로 가는 버튼을 제작하여 유저가 홈으로 이동할 수 있도록 함  reload될 수 있도록 navigate가 아닌 replace 사용

--- frontend/src/pages/ServerErrorPage/index.tsx
+++ frontend/src/pages/ServerErrorPage/index.tsx
@@ -1,4 +1,5 @@
 import gongseek from '@/assets/gongseek.png';
+import { URL } from '@/constants/url';
 import * as S from '@/pages/ServerErrorPage/index.styles';
 
 const ServerErrorPage = () => (
@@ -6,6 +7,7 @@ const ServerErrorPage = () => (
 		<S.LogoImg src={gongseek} alt="로고 이미지" />
 		<S.Message>일시적 서버 장애가 발생하였습니다 </S.Message>
 		<S.Message>잠시 후 다시 시도해주세요</S.Message>
+		<S.HomeButton onClick={() => window.location.replace(URL.HOME)}>홈으로 가기</S.HomeButton>
 	</S.Container>
 );
 
