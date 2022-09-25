f1711a07ad41682870096ad4413b852aeea97207
jurlring <78091011+jurlring@users.noreply.github.com>
2022-09-25 19:14:45 +0900

    refactor: JPQL에 @Param을 추가

--- backend/src/main/java/com/woowacourse/gongseek/comment/domain/repository/CommentRepository.java
+++ backend/src/main/java/com/woowacourse/gongseek/comment/domain/repository/CommentRepository.java
@@ -4,11 +4,12 @@ import com.woowacourse.gongseek.comment.domain.Comment;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("select c from Comment c join fetch c.member where c.article.id = :articleId")
-    List<Comment> findAllByArticleIdWithMember(Long articleId);
+    List<Comment> findAllByArticleIdWithMember(@Param("articleId") Long articleId);
 
     List<Comment> findAllByMemberId(Long memberId);
 
