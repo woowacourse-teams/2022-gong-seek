b54546adbf2b85954bc1db5e30149bf76ac1e026
Gyuchool <balance0591@naver.com>
2022-09-26 21:25:02 +0900

    style: 사용하지 않는 import문 제거

--- backend/src/main/java/com/woowacourse/gongseek/config/RedisConfig.java
+++ backend/src/main/java/com/woowacourse/gongseek/config/RedisConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
