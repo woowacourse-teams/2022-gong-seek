1673445bf3ed126b95466251107ec04f90c48acd
JASUN LEE <60773373+liswktjs@users.noreply.github.com>
2022-09-28 09:59:06 +0900

    로그아웃을 구현한다 (#613)
    * fix: refreshToken 삭제 요청을 보내는 비동기 통신 로직과 훅 구현
    
    * feat: 로그아웃 하는 곳에서 로그아웃 요청 보냄
    
    * type: mutate관련 타입 지정
    
    * feat: 에러 바운더리에서 refreshToken 삭제하는 요청 보내기 위해서 props로 넘김
    
    * feat: 에러바운더리에서 refreshToken 삭제요청 연결
    
    * fix: 함수가 실행되기 전에 페이지가 전환되어서 navigate로 교체

--- frontend/src/components/helper/types/ErrorBoundary.type.ts
+++ frontend/src/components/helper/types/ErrorBoundary.type.ts
@@ -1,6 +1,9 @@
+import { AxiosError, AxiosResponse } from 'axios';
+import { UseMutateFunction } from 'react-query';
 import { NavigateFunction } from 'react-router-dom';
 
 import CustomError from '@/components/helper/CustomError';
+import { ErrorMessage } from '@/constants/ErrorMessage';
 
 export interface ErrorBoundaryState {
 	error: CustomError | null;
@@ -9,6 +12,18 @@ export interface ErrorBoundaryState {
 export interface LogicErrorBoundaryProps {
 	showSnackBar?: (message: string) => void;
 	navigate?: NavigateFunction;
+	mutateDeleteRefreshToken?: UseMutateFunction<
+		AxiosResponse<null, any>,
+		AxiosError<
+			{
+				errorCode: keyof typeof ErrorMessage;
+				message: string;
+			},
+			any
+		>,
+		void,
+		unknown
+	>;
 }
 
 export interface FallbackErrorBoundaryProps {
--- frontend/src/utils/withHooksHOC.tsx
+++ frontend/src/utils/withHooksHOC.tsx
@@ -1,13 +1,22 @@
 import { useNavigate } from 'react-router-dom';
 
 import useSnackBar from '@/hooks/common/useSnackBar';
+import useDeleteRefreshToken from '@/hooks/login/useDeleteRefreshToken';
 
 function WithHooksHOC<F>(Component: React.ComponentType<F>) {
 	return function Hoc(props: F) {
 		const { showSnackBar } = useSnackBar();
 		const navigate = useNavigate();
+		const { mutate: mutateDeleteRefreshToken } = useDeleteRefreshToken();
 
-		return <Component {...props} showSnackBar={showSnackBar} navigate={navigate} />;
+		return (
+			<Component
+				{...props}
+				showSnackBar={showSnackBar}
+				navigate={navigate}
+				mutateDeleteRefreshToken={mutateDeleteRefreshToken}
+			/>
+		);
 	};
 }
 
