471ec679b87c2a77a6b65d9db55052491439a95b
JASUN LEE <liswktjs@gmail.com>
2022-09-29 16:42:56 +0900

    refactor: Mypage에서 선택된 카테고리 별로 컴포넌트 보여주기

--- frontend/src/pages/MyPage/index.tsx
+++ frontend/src/pages/MyPage/index.tsx
@@ -1,13 +1,17 @@
+import React, { Suspense, useState } from 'react';
+
 import Loading from '@/components/common/Loading/Loading';
-import { URL } from '@/constants/url';
-import useGetUserArticles from '@/hooks/user/useGetUserArticles';
-import useGetUserComments from '@/hooks/user/useGetUserComments';
 import useGetUserInfo from '@/hooks/user/useGetUserInfo';
-import UserArticleItem from '@/pages/MyPage/UserArticleItem/UserArticleItem';
-import UserCommentBox from '@/pages/MyPage/UserCommentBox/UserCommentBox';
-import UserItemBox from '@/pages/MyPage/UserItemBox/UserItemBox';
+import CategoryTab from '@/pages/MyPage/CategoryTab/CategoryTab';
 import UserProfile from '@/pages/MyPage/UserProfile/UserProfile';
 import * as S from '@/pages/MyPage/index.styles';
+import { CategoryType } from '@/types/myPage';
+
+const ArticleBox = React.lazy(() => import('@/pages/MyPage/UserArticleBox/UserArticleBox'));
+const CommentBox = React.lazy(() => import('@/pages/MyPage/UserCommentBox/UserCommentBox'));
+const TemporaryArticleList = React.lazy(
+	() => import('@/pages/TemporaryArticles/TemporaryArticleList/TemporaryArticleList'),
+);
 
 const MyPage = () => {
 	const {
@@ -16,60 +20,28 @@ const MyPage = () => {
 		isLoading: isInfoLoading,
 		isIdle: isInfoIdle,
 	} = useGetUserInfo();
-	const {
-		data: articles,
-		isSuccess: isArticlesSuccess,
-		isLoading: isArticlesLoading,
-		isIdle: isArticlesIdle,
-	} = useGetUserArticles();
-	const {
-		data: comments,
-		isSuccess: isCommentsSuccess,
-		isLoading: isCommentsLoading,
-		isIdle: isCommentsIdle,
-	} = useGetUserComments();
 
-	if (
-		isInfoLoading ||
-		isArticlesLoading ||
-		isCommentsLoading ||
-		isInfoIdle ||
-		isArticlesIdle ||
-		isCommentsIdle
-	) {
+	const [category, setCategory] = useState<CategoryType>('article');
+
+	if (isInfoLoading || isInfoIdle) {
 		return <Loading />;
 	}
 
 	return (
 		<S.Container>
 			<S.Title>마이 페이지</S.Title>
-
-			{isInfoSuccess && isArticlesSuccess && isCommentsSuccess ? (
-				<S.ContentContainer>
-					{info && <UserProfile name={info.name} avatarUrl={info.avatarUrl} />}
-					<S.LinkTemporaryArticle to={URL.TEMP_ARTICLE_LIST}>
-						임시작성글 보러가기
-					</S.LinkTemporaryArticle>
-					<UserItemBox subTitle="내가 작성한 글">
-						{articles ? (
-							articles.articles.map((article) => (
-								<UserArticleItem key={article.id} article={article} />
-							))
-						) : (
-							<div>작성하신 글이 없습니다</div>
-						)}
-					</UserItemBox>
-
-					<UserItemBox subTitle="내가 작성한 댓글">
-						{comments ? (
-							comments.comments.map((comment) => (
-								<UserCommentBox key={comment.id} comment={comment} />
-							))
-						) : (
-							<div>작성하신 댓글이 없습니다</div>
-						)}
-					</UserItemBox>
-				</S.ContentContainer>
+			{isInfoSuccess && info ? (
+				<>
+					<UserProfile name={info.name} avatarUrl={info.avatarUrl} />
+					<CategoryTab category={category} setCategory={setCategory} />
+					<Suspense fallback={<Loading />}>
+						<S.ContentContainer>
+							{category === 'article' && <ArticleBox />}
+							{category === 'comment' && <CommentBox />}
+							{category === 'tempArticle' && <TemporaryArticleList />}
+						</S.ContentContainer>
+					</Suspense>
+				</>
 			) : (
 				<div>정보를 가져오는데 실패하였습니다</div>
 			)}
