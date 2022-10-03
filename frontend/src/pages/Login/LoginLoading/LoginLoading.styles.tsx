3ee4dd21d8b48bffe4a862bb244715280b0e296d
JASUN LEE <liswktjs@gmail.com>
2022-10-03 20:26:59 +0900

    feat: 로그인 중 보여줄 컴포넌트 구현

---
+++ frontend/src/pages/Login/LoginLoading/LoginLoading.styles.tsx
@@ -0,0 +1,30 @@
+import { keyframes } from '@emotion/react';
+import styled from '@emotion/styled';
+
+const logoRotateAnimation = keyframes`
+    100%{
+        transform: rotate(360deg);
+    }
+`;
+
+export const Container = styled.section`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+
+	width: 100%;
+	height: 100%;
+`;
+
+export const LogoImg = styled.img`
+	width: ${({ theme }) => theme.size.SIZE_160};
+	height: ${({ theme }) => theme.size.SIZE_160};
+
+	animation: ${logoRotateAnimation} 2s infinite linear;
+`;
+
+export const Message = styled.div`
+	margin-top: ${({ theme }) => theme.size.SIZE_040};
+	size: ${({ theme }) => theme.size.SIZE_018};
+`;
