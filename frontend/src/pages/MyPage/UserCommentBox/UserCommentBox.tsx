4fe5bcb21a588d09503e764c4cbbc6fc3be56977
JASUN LEE <60773373+liswktjs@users.noreply.github.com>
2022-10-04 12:01:24 +0900

    임시저장 게시글 제목이 길면 삭제버튼이 보이지 않는 에러를 수정한다 (#666)
    * style: Theme size추가
    
    * style: 스타일링 수정
    
    width값 조정 통해서 삭제버튼이 가려지지 않도록 함
    
    * feat: 임시저장글 정보에 카테고리 추가
    
    임시저장글 정보에 카테고리 정보를 추가하고 스타일링 수정
    
    * feat: 카테고리를 한글 이름으로 바꾸는 유틸 함수 생성
    
    * refactor: 한글 카테고리 이름이 사용되는 곳에서 converter 함수 적용

--- frontend/src/pages/MyPage/UserCommentBox/UserCommentBox.tsx
+++ frontend/src/pages/MyPage/UserCommentBox/UserCommentBox.tsx
@@ -2,16 +2,17 @@ import { useNavigate } from 'react-router-dom';
 
 import * as S from '@/pages/MyPage/UserCommentBox/UserCommentBox.styles';
 import { UserComment } from '@/types/commentResponse';
-import { dateTimeConverter } from '@/utils/converter';
+import { categoryNameConverter, dateTimeConverter } from '@/utils/converter';
 
 const UserCommentBox = ({ comment }: { comment: UserComment }) => {
 	const { id, content, createdAt, updatedAt, articleId, category, articleTitle } = comment;
 	const navigate = useNavigate();
-	const 한글_카테고리 = category === 'question' ? '질문' : '토론';
 	return (
 		<S.Container onClick={() => navigate(`/articles/${category}/${articleId}`)}>
 			<S.ArticleBox>
-				<S.ArticleCategory isQuestion={category === 'question'}>{한글_카테고리}</S.ArticleCategory>
+				<S.ArticleCategory isQuestion={category === 'question'}>
+					{categoryNameConverter(category)}
+				</S.ArticleCategory>
 				<S.ArticleTitle>{articleTitle}</S.ArticleTitle>
 			</S.ArticleBox>
 
--- frontend/src/pages/TemporaryArticles/TemporaryArticleList/TemporaryArticleList.styles.tsx
+++ frontend/src/pages/TemporaryArticles/TemporaryArticleList/TemporaryArticleList.styles.tsx
@@ -19,9 +19,14 @@ export const ArticleListBox = styled.div`
 	display: flex;
 	flex-direction: column;
 	gap: ${({ theme }) => theme.size.SIZE_020};
+
+	width: 100%;
 `;
 
 export const ArticleItemBox = styled.div`
 	display: flex;
 	align-items: center;
+
+	width: 100%;
+	justify-content: space-between;
 `;
