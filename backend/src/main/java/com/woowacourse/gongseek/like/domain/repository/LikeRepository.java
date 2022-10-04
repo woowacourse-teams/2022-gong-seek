3360b7b6da06b07a2cf91d03de347ca9451cc5f4
윤주리 <78091011+jurlring@users.noreply.github.com>
2022-10-04 18:33:07 +0900

    Like 삭제 수정 및 redis server 설정 추가 (#697)
    * refactor: like 삭제할 때, 바로 삭제하도록 수정
    
    * refactor: redis server maxMemory 설정 추가
    
    * refactor: entity를 통해서 delete하도록 수정
    
    Co-authored-by: Gyuchool <balance0591@naver.com>

--- backend/src/main/java/com/woowacourse/gongseek/like/domain/repository/LikeRepository.java
+++ backend/src/main/java/com/woowacourse/gongseek/like/domain/repository/LikeRepository.java
@@ -1,13 +1,15 @@
 package com.woowacourse.gongseek.like.domain.repository;
 
+import com.woowacourse.gongseek.article.domain.Article;
 import com.woowacourse.gongseek.like.domain.Like;
-import java.util.Optional;
+import com.woowacourse.gongseek.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
-    Optional<Like> findByArticleIdAndMemberId(Long articleId, Long memberId);
+
+    void deleteByArticleAndMember(Article article, Member member);
 
     boolean existsByArticleIdAndMemberId(Long articleId, Long memberId);
 
