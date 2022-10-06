ff052832ad8ad40317a0e34e3921d92bd6ee7adf
JASUN LEE <liswktjs@gmail.com>
2022-10-06 10:19:23 +0900

    refactor: HashTag검색결과 보여주는 페이지에서 EmptyMessage와 ResponsiveInfiniteCardList 컴포넌트 적용

--- frontend/src/pages/HashTagSearch/HashTagSearchResult/HashTagSearchResult.tsx
+++ frontend/src/pages/HashTagSearch/HashTagSearchResult/HashTagSearchResult.tsx
@@ -1,10 +1,15 @@
+import React, { Suspense } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import ArticleItem from '@/components/common/ArticleItem/ArticleItem';
-import InfiniteScrollObserver from '@/components/common/InfiniteScrollObserver/InfiniteScrollObserver';
 import Loading from '@/components/common/Loading/Loading';
 import useGetArticleByHashTag from '@/hooks/hashTag/useGetArticleByHashTag';
 import * as S from '@/pages/HashTagSearch/HashTagSearchResult/HashTagSearchResult.styles';
+import { EmptyMessage } from '@/pages/Search/index.styles';
+
+const ResponsiveInfiniteCardList = React.lazy(
+	() => import('@/components/common/ResponsiveInfiniteCardList/ResponsiveInfiniteCardList'),
+);
 
 export interface HashTagSearchResultProps {
 	hashTags: string[];
@@ -22,26 +27,28 @@ const HashTagSearchResult = ({ hashTags }: HashTagSearchResultProps) => {
 	return (
 		<S.Container>
 			<S.Title>검색 결과</S.Title>
-			{data && data.pages[0].articles.length >= 1 ? (
-				<InfiniteScrollObserver
-					hasNext={data.pages[data.pages.length - 1].hasNext}
-					fetchNextPage={fetchNextPage}
-				>
-					<S.SearchResult>
-						{data.pages.map(({ articles }) =>
-							articles.map((article) => (
-								<ArticleItem
-									key={article.id}
-									article={article}
-									onClick={() => navigate(`/articles/${article.category}/${article.id}`)}
-								/>
-							)),
-						)}
-					</S.SearchResult>
-				</InfiniteScrollObserver>
-			) : (
-				<S.EmptyMsg>검색 결과가 존재하지 않습니다</S.EmptyMsg>
-			)}
+			<Suspense fallback={<Loading />}>
+				{data && data.pages[0].articles.length >= 1 ? (
+					<ResponsiveInfiniteCardList
+						hasNext={data.pages[data.pages.length - 1].hasNext}
+						fetchNextPage={fetchNextPage}
+					>
+						<>
+							{data.pages.map(({ articles }) =>
+								articles.map((article) => (
+									<ArticleItem
+										key={article.id}
+										article={article}
+										onClick={() => navigate(`/articles/${article.category}/${article.id}`)}
+									/>
+								)),
+							)}
+						</>
+					</ResponsiveInfiniteCardList>
+				) : (
+					<EmptyMessage>검색결과가 존재하지 않습니다</EmptyMessage>
+				)}
+			</Suspense>
 		</S.Container>
 	);
 };
