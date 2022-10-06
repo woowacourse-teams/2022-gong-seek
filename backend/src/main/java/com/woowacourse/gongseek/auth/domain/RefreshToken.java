4de8c2421e54e062cb25373eaaba6b8ec47d5afd
Gyuchool <balance0591@naver.com>
2022-10-07 00:38:55 +0900

    refactor: embedded 설정 제거

--- backend/src/main/java/com/woowacourse/gongseek/auth/domain/RefreshToken.java
+++ backend/src/main/java/com/woowacourse/gongseek/auth/domain/RefreshToken.java
@@ -1,21 +1,18 @@
 package com.woowacourse.gongseek.auth.domain;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
 
-@NoArgsConstructor
 @Getter
 @RedisHash(value = "refreshToken", timeToLive = 1_209_600L)
-public class RefreshToken implements Serializable {
+public class RefreshToken {
 
     @Id
     @GeneratedValue(generator = "uuid4")
@@ -24,9 +21,9 @@ public class RefreshToken implements Serializable {
     private UUID id;
 
     @Indexed
-    private Long memberId;
+    private final Long memberId;
 
-    private LocalDateTime expiryDate;
+    private final LocalDateTime expiryDate;
 
     private boolean issue;
 
