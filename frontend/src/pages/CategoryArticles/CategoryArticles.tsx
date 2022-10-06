0c2b2e13f6d71e204ea0adc7e6b69033e2cf2fc6
JASUN LEE <liswktjs@gmail.com>
2022-10-06 10:12:18 +0900

    refactor: CategorySelector에 ResponsiveInfiniteCardList 적용

--- frontend/src/pages/CategoryArticles/CategoryArticles.tsx
+++ frontend/src/pages/CategoryArticles/CategoryArticles.tsx
@@ -1,14 +1,18 @@
+import React, { Suspense } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import ArticleItem from '@/components/common/ArticleItem/ArticleItem';
 import EmptyMessage from '@/components/common/EmptyMessage/EmptyMessage';
-import InfiniteScrollObserver from '@/components/common/InfiniteScrollObserver/InfiniteScrollObserver';
 import Loading from '@/components/common/Loading/Loading';
 import SortDropdown from '@/components/common/SortDropdown/SortDropDown';
 import { URL } from '@/constants/url';
 import useGetCategoryArticles from '@/hooks/article/useGetCategoryArticles';
 import * as S from '@/pages/CategoryArticles/CategoryArticles.styles';
 import { categoryNameConverter } from '@/utils/converter';
+
+const ResponsiveInfiniteCardList = React.lazy(
+	() => import('@/components/common/ResponsiveInfiniteCardList/ResponsiveInfiniteCardList'),
+);
 
 const CategoryArticles = () => {
 	const navigate = useNavigate();
@@ -42,28 +46,30 @@ const CategoryArticles = () => {
 					setSortIndex={setSortIndex}
 				/>
 			</S.TitleBox>
-			{data?.pages.length ? (
-				<InfiniteScrollObserver
-					hasNext={data?.pages[data.pages.length - 1].hasNext}
-					fetchNextPage={fetchNextPage}
-				>
-					<S.ArticleItemList>
-						{data.pages.map(({ articles }) =>
-							articles.map((item) => (
-								<ArticleItem
-									key={item.id}
-									article={item}
-									onClick={() => {
-										navigate(`/articles/${category}/${item.id}`);
-									}}
-								/>
-							)),
-						)}
-					</S.ArticleItemList>
-				</InfiniteScrollObserver>
-			) : (
-				<EmptyMessage>게시글이 존재하지 않습니다</EmptyMessage>
-			)}
+			<Suspense fallback={<Loading />}>
+				{data?.pages.length ? (
+					<ResponsiveInfiniteCardList
+						hasNext={data.pages[data.pages.length - 1].hasNext}
+						fetchNextPage={fetchNextPage}
+					>
+						<>
+							{data.pages.map(({ articles }) =>
+								articles.map((item) => (
+									<ArticleItem
+										key={item.id}
+										article={item}
+										onClick={() => {
+											navigate(`/articles/${category}/${item.id}`);
+										}}
+									/>
+								)),
+							)}
+						</>
+					</ResponsiveInfiniteCardList>
+				) : (
+					<EmptyMessage>게시글이 존재하지 않습니다</EmptyMessage>
+				)}
+			</Suspense>
 		</S.Container>
 	);
 };
