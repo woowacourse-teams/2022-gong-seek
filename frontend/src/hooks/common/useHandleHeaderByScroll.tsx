7d60488aab323222726536e5e2fcf52f2835a55e
Jungmin Hwang <85891751+hwangstar156@users.noreply.github.com>
2022-10-07 11:33:03 +0900

    모바일에서 글 작성시 입력하는 곳으로 포커스가 오도록 수정한다. (#680)
    * feat: useInterval 훅 제작
    
    useEffect내에서 setInterval을 이용하기 위해 만든 훅
    
    * feat: 스크롤을 위로 올렸을때와 아래로 내렸을때를 감지하는 함수 생성
    
    * feat: active상태를 받을때에 대한 header animation 설정
    
    * refactor: 스크롤 여부 판단하는 함수들 유틸로 분리
    
    * feat: useEffect의 cleanUp으로 언마운트될때 쓰지않을 함수 정리
    
    * refactor: useScroll로 scroll 관리하는 로직 분리
    
    * refactor: 절대경로 및 안쓰는 함수 제거
    
    * feat: 검색 페이지 헤더에서 접히지 않는 문제 해결
    
    * refactor: 헤더 스크롤 관리하는 로직 훅으로 분리
    
    * refactor: 오타 수정
    
    * refactor: 스크롤 관리하는 로직 헤더 컴포넌트내로 이동
    
    * refactor: useHeaderViewByScroll -> useActiveHeaderByScroll로 함수 이름 변경
    
    * refactor: 함수 네이밍 변경

---
+++ frontend/src/hooks/common/useHandleHeaderByScroll.tsx
@@ -0,0 +1,48 @@
+import { useRef, useState } from 'react';
+
+import { isScrollDown, isScrollUp, isMinDeltaScroll } from '@/utils/scrollObserver';
+
+const useHandleHeaderByScroll = () => {
+	const [isActiveHeader, setIsActiveHeader] = useState(true);
+	const headerElement = useRef<HTMLDivElement>(null);
+	const lastScrollTop = useRef(0);
+	const minDeltaScroll = useRef(10);
+
+	const handleHeaderViewByScroll = () => {
+		const currentScroll = document.documentElement.scrollTop;
+		if (!headerElement.current) {
+			return;
+		}
+
+		if (
+			isMinDeltaScroll({
+				minDeltaScroll: minDeltaScroll.current,
+				currentScroll,
+				lastScrollTop: lastScrollTop.current,
+			})
+		) {
+			return;
+		}
+
+		if (
+			isScrollDown({
+				currentScroll,
+				lastScrollTop: lastScrollTop.current,
+				headerHeight: headerElement.current.offsetHeight,
+			})
+		) {
+			setIsActiveHeader(false);
+			return;
+		}
+
+		if (isScrollUp({ currentScroll })) {
+			setIsActiveHeader(true);
+		}
+
+		lastScrollTop.current = currentScroll;
+	};
+
+	return { isActiveHeader, handleHeaderViewByScroll, headerElement };
+};
+
+export default useHandleHeaderByScroll;
