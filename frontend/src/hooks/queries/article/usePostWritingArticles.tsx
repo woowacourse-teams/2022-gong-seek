af7c17185143c0299468053877a693b684e96fa3
hwangjungmin <hwangstar156@gmail.com>
2022-10-09 17:11:15 +0900

    refactor: usePostWritingArticles client state와 server state의 분리

--- frontend/src/hooks/queries/article/usePostWritingArticles.tsx
+++ frontend/src/hooks/queries/article/usePostWritingArticles.tsx
@@ -1,5 +1,4 @@
 import { AxiosError, AxiosResponse } from 'axios';
-import { useEffect, useRef, useState } from 'react';
 import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 
@@ -7,17 +6,11 @@ import { postWritingArticle } from '@/api/article';
 import { ErrorMessage } from '@/constants/ErrorMessage';
 import { CATEGORY } from '@/constants/categoryType';
 import useThrowCustomError from '@/hooks/common/useThrowCustomError';
-import { validatedTitleInput } from '@/utils/validateInput';
-import { Editor } from '@toast-ui/react-editor';
 
-const usePostWritingArticles = ({
-	category,
-	isAnonymous,
-}: {
-	category?: string;
-	isAnonymous: boolean;
-}) => {
-	const { data, mutate, isError, isLoading, isSuccess, error } = useMutation<
+const usePostWritingArticles = ({ categoryOption }: { categoryOption: string }) => {
+	const navigate = useNavigate();
+
+	const { mutate, isError, isLoading, error } = useMutation<
 		AxiosResponse<{ id: string }>,
 		AxiosError<{ errorCode: keyof typeof ErrorMessage; message: string }>,
 		{
@@ -28,72 +21,27 @@ const usePostWritingArticles = ({
 			isAnonymous: boolean;
 			tempArticleId: number | '';
 		}
-	>(postWritingArticle, { retry: 1 });
-
-	const content = useRef<Editor | null>(null);
-	const [title, setTitle] = useState('');
-	const [categoryOption, setCategoryOption] = useState<string>(category ? category : '');
-	const [isValidTitleInput, setIsValidTitleInput] = useState(true);
-	const [hashTags, setHashTags] = useState<string[]>([]);
-	const titleInputRef = useRef<HTMLInputElement>(null);
-	const navigate = useNavigate();
+	>(postWritingArticle, {
+		retry: 1,
+		onSuccess: (data) => {
+			if (categoryOption === CATEGORY.discussion) {
+				if (confirm('글 등록이 완료되었습니다. 투표를 등록하시겠습니까?')) {
+					navigate(`/votes/${data.data.id}`);
+					return;
+				}
+				navigate(`/articles/${categoryOption}/${data.data.id}`);
+			}
+			if (categoryOption === CATEGORY.question) {
+				navigate(`/articles/${categoryOption}/${data.data.id}`);
+			}
+		},
+	});
 
 	useThrowCustomError(isError, error);
 
-	if (typeof category === 'undefined') {
-		throw new Error('카테고리가 존재하지 않습니다.');
-	}
-
-	useEffect(() => {
-		if (isSuccess && categoryOption === CATEGORY.discussion) {
-			if (confirm('글 등록이 완료되었습니다. 투표를 등록하시겠습니까?')) {
-				navigate(`/votes/${data.data.id}`);
-				return;
-			}
-			navigate(`/articles/${categoryOption}/${data.data.id}`);
-		}
-		if (isSuccess && categoryOption === CATEGORY.question) {
-			navigate(`/articles/${categoryOption}/${data.data.id}`);
-		}
-	}, [isSuccess]);
-
-	const handleSubmitButtonClick = (categoryOption: string, tempArticleId: number | '') => {
-		if (content.current === null) {
-			return;
-		}
-
-		if (!validatedTitleInput(title)) {
-			setIsValidTitleInput(false);
-
-			if (titleInputRef.current !== null) {
-				titleInputRef.current.focus();
-			}
-
-			return;
-		}
-		setIsValidTitleInput(true);
-		mutate({
-			title: title,
-			category: categoryOption,
-			content: content.current.getInstance().getMarkdown(),
-			tag: hashTags,
-			isAnonymous,
-			tempArticleId,
-		});
-	};
-
 	return {
-		handleSubmitButtonClick,
-		content,
 		isLoading,
-		title,
-		titleInputRef,
-		isValidTitleInput,
-		setTitle,
-		hashTags,
-		setHashTags,
-		categoryOption,
-		setCategoryOption,
+		mutate,
 	};
 };
 
