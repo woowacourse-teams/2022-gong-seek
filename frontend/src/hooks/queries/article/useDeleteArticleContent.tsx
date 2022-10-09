e5ba10bc1e5eaed31ebbb2db99ebd100aba604c8
hwangjungmin <hwangstar156@gmail.com>
2022-10-09 14:20:01 +0900

    feat: useDeleteArticleContent onSuccess로 로직 분리

--- frontend/src/hooks/queries/article/useDeleteArticleContent.tsx
+++ frontend/src/hooks/queries/article/useDeleteArticleContent.tsx
@@ -1,5 +1,4 @@
 import { AxiosError } from 'axios';
-import { useEffect } from 'react';
 import { useMutation } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 
@@ -11,21 +10,21 @@ import useThrowCustomError from '@/hooks/common/useThrowCustomError';
 
 const useDeleteArticleContent = () => {
 	const { showSnackBar } = useSnackBar();
-	const { isSuccess, isError, error, mutate } = useMutation<
+	const navigate = useNavigate();
+
+	const { isError, error, mutate } = useMutation<
 		unknown,
 		AxiosError<{ errorCode: keyof typeof ErrorMessage; message: string }>,
 		string
-	>(deleteArticle, { retry: 1 });
-	const navigate = useNavigate();
-
-	useThrowCustomError(isError, error);
-
-	useEffect(() => {
-		if (isSuccess) {
+	>(deleteArticle, {
+		retry: 1,
+		onSuccess: () => {
 			showSnackBar('게시글이 삭제 되었습니다');
 			navigate(URL.HOME);
-		}
-	}, [isSuccess]);
+		},
+	});
+
+	useThrowCustomError(isError, error);
 
 	const handleDeleteArticle = (articleId: string) => {
 		if (window.confirm('게시글을 삭제하시겠습니까?')) {
