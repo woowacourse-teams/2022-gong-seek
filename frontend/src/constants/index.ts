7ddd41a243c80d25262d2053b9c2439e5a76024b
hwangjungmin <hwangstar156@gmail.com>
2022-10-17 02:58:31 +0900

    feat: resize 될때마다 스크롤 이동시켜야될 너비를 변경할 수 있도록 구현
    debounce를 이용하여 마지막 resize만 적용한다.

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -7,3 +7,5 @@ export const CAROUSEL_ITEM_WIDTH = 335;
 export const DEBOUNCE_SCROLL_TIME = 100;
 
 export const CAROUSEL_AUTO_PLAY_TIME = 3000;
+
+export const DEBOUNCE_RESIZE_TIME = 300;
--- frontend/src/hooks/common/useCarousel.tsx
+++ frontend/src/hooks/common/useCarousel.tsx
@@ -1,14 +1,25 @@
+import useDebounce from './useDebounce';
 import { useState, useCallback, useEffect, useRef } from 'react';
 
-import { CAROUSEL_ITEM_WIDTH, DEBOUNCE_SCROLL_TIME } from '@/constants';
+import { DEBOUNCE_RESIZE_TIME, DEBOUNCE_SCROLL_TIME } from '@/constants';
+
+const MIN_CAROUSEL_INDEX = 0;
+const MAX_CAROUSEL_INDEX = 9;
 
 const useCarousel = () => {
 	const [carouselElementRef, setCarouselElementRef] = useState<null | HTMLDivElement>(null);
 	const [currentIndex, setCurrentIndex] = useState(0);
 	const [isScroll, setIsScroll] = useState(false);
 	const timerId = useRef<number | null>();
+	const carouselItemWidth = useRef<number>(0);
 
-	const handleScrollEnd = () => {
+	const setCarouselItemWidth = useDebounce(() => {
+		if (carouselElementRef) {
+			carouselItemWidth.current = carouselElementRef.scrollWidth / 15;
+		}
+	}, DEBOUNCE_RESIZE_TIME);
+
+	const handleScrollEnd = useCallback(() => {
 		setIsScroll(true);
 		if (timerId.current !== null) {
 			clearTimeout(timerId.current);
@@ -16,30 +27,33 @@ const useCarousel = () => {
 		timerId.current = window.setTimeout(function () {
 			setIsScroll(false);
 		}, DEBOUNCE_SCROLL_TIME);
-	};
+	}, [timerId.current]);
 
 	useEffect(() => {
 		if (carouselElementRef) {
+			carouselItemWidth.current = carouselElementRef.scrollWidth / 15;
 			carouselElementRef.addEventListener('scroll', handleScrollEnd);
+			window.addEventListener('resize', setCarouselItemWidth);
 
 			carouselElementRef.scrollTo({
-				left: CAROUSEL_ITEM_WIDTH,
+				left: carouselItemWidth.current,
 				behavior: 'auto',
 			});
 		}
 
 		return () => {
 			carouselElementRef?.removeEventListener('scroll', handleScrollEnd);
+			window.removeEventListener('resize', setCarouselItemWidth);
 		};
 	}, [carouselElementRef]);
 
 	const handleLeftSlideEvent = () => {
-		if (currentIndex === 0 || isScroll) {
+		if (currentIndex === MIN_CAROUSEL_INDEX || isScroll) {
 			return;
 		}
 
 		carouselElementRef?.scrollBy({
-			left: -CAROUSEL_ITEM_WIDTH,
+			left: -carouselItemWidth.current,
 			behavior: 'smooth',
 		});
 		setCurrentIndex((prev) => prev - 1);
@@ -50,17 +64,17 @@ const useCarousel = () => {
 			return;
 		}
 
-		if (currentIndex === 9) {
-			setCurrentIndex(0);
+		if (currentIndex === MAX_CAROUSEL_INDEX) {
+			setCurrentIndex(MIN_CAROUSEL_INDEX);
 			carouselElementRef?.scrollTo({
-				left: CAROUSEL_ITEM_WIDTH,
+				left: carouselItemWidth.current,
 				behavior: 'smooth',
 			});
 			return;
 		}
 
 		carouselElementRef?.scrollBy({
-			left: CAROUSEL_ITEM_WIDTH,
+			left: carouselItemWidth.current,
 			behavior: 'smooth',
 		});
 
