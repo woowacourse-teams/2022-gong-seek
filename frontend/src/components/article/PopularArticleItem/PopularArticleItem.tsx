8ef0499d75851412fc9ef9c5d0fe305ac46c9036
hwangjungmin <hwangstar156@gmail.com>
2022-10-16 23:10:40 +0900

    feat: 일정시간후에 다음으로 자동으로 넘어가도록 구현

--- frontend/src/components/article/PopularArticleItem/PopularArticleItem.tsx
+++ frontend/src/components/article/PopularArticleItem/PopularArticleItem.tsx
@@ -1,5 +1,8 @@
+import { useEffect, useRef } from 'react';
+
 import Card from '@/components/@common/Card/Card';
 import * as S from '@/components/article/ArticleItem/ArticleItem.styles';
+import * as PopularS from '@/components/article/PopularArticle/PopularArticle.styles';
 import { theme } from '@/styles/Theme';
 import { PopularArticleItemCardStyle } from '@/styles/cardStyle';
 import { CommonArticleType } from '@/types/articleResponse';
@@ -10,18 +13,31 @@ const PopularArticleItem = ({
 	article,
 	isActive,
 	onClick,
+	rightSlide,
 }: {
 	article: CommonArticleType;
 	isActive: boolean;
-	onClick: () => void;
+	onClick?: () => void;
+	rightSlide?: () => void;
 }) => {
 	const { title, author, commentCount, views, createdAt, tag } = article;
+	const timerId = useRef<number | undefined>(undefined);
+	useEffect(() => {
+		if (isActive && rightSlide) {
+			timerId.current = window.setTimeout(rightSlide, 5000);
+		}
+
+		return () => {
+			window.clearTimeout(timerId.current);
+		};
+	}, [isActive, rightSlide]);
+
 	return (
 		<>
 			<Card {...PopularArticleItemCardStyle} isActive={isActive} onClick={onClick}>
-				<S.ProfileBox
+				<div
 					css={css`
-						background: linear-gradient(to bottom, #5e0080 7%, #3399ff 96%);
+						background: #ba55d3;
 						opacity: 0.8;
 						box-shadow: 0, 0, 0, inset 0 0 4px #5e0080;
 						padding: ${theme.size.SIZE_012};
@@ -30,18 +46,26 @@ const PopularArticleItem = ({
 						margin-bottom: 1rem;
 					`}
 				>
-					<S.UserProfile
-						src={convertGithubAvatarUrlForResize(author.avatarUrl)}
-						css={css`
-							border: 2px solid transparent;
-							background-image: linear-gradient(to bottom, #ffffcc 7%, #ff9900 96%),
-								linear-gradient(to bottom, #ff3300 7%, #66ffcc 96%);
-							background-origin: border-box;
-							background-clip: content-box, border-box;
-						`}
-					/>
-					<div>{author.name}</div>
-				</S.ProfileBox>
+					<S.ProfileBox>
+						<S.UserProfile
+							src={convertGithubAvatarUrlForResize(author.avatarUrl)}
+							css={css`
+								border: 2px solid transparent;
+								background-image: linear-gradient(to bottom, #ffffcc 7%, #ff9900 96%),
+									linear-gradient(to bottom, #ff3300 7%, #66ffcc 96%);
+								background-origin: border-box;
+								background-clip: content-box, border-box;
+							`}
+						/>
+						<div
+							css={css`
+								color: white;
+							`}
+						>
+							{author.name}
+						</div>
+					</S.ProfileBox>
+				</div>
 				<div
 					css={css`
 						padding: ${theme.size.SIZE_014};
@@ -57,18 +81,26 @@ const PopularArticleItem = ({
 					<S.ArticleInfoBox>
 						<S.ArticleTimeStamp>{dateTimeConverter(createdAt)}</S.ArticleTimeStamp>
 						<S.ArticleInfoSubBox>
-							<S.CommentCount>댓글 수 {commentCount}</S.CommentCount>
-							<S.Views>조회 수 {views}</S.Views>
+							<S.CommentCount>
+								<PopularS.CommentIcon />
+								{commentCount}
+							</S.CommentCount>
+							<S.Views>
+								<PopularS.ViewIcon /> {views}
+							</S.Views>
 						</S.ArticleInfoSubBox>
 					</S.ArticleInfoBox>
-					<S.HashTagListBox>
+
+					<S.HashTagListBox
+						css={css`
+							margin: 1rem 0;
+							height: inherit;
+						`}
+					>
 						{tag &&
 							tag.length >= 1 &&
 							tag.map((item) => <S.HashTagItem key={item}>#{item}</S.HashTagItem>)}
 					</S.HashTagListBox>
-					<S.FooterBox>
-						<S.RightFooterBox></S.RightFooterBox>
-					</S.FooterBox>
 				</div>
 			</Card>
 		</>
