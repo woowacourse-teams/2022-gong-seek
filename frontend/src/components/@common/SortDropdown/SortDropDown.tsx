4a2bc0688fb6d20cf16bff5949ad7c39fc259f7a
hwangjungmin <hwangstar156@gmail.com>
2022-10-18 03:34:22 +0900

    feat: SortDropdown 접근성 추가

--- frontend/src/components/@common/SortDropdown/SortDropDown.tsx
+++ frontend/src/components/@common/SortDropdown/SortDropDown.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import * as S from '@/components/@common/SortDropdown/SortDropdown.styles';
+import { SrOnlyContainer } from '@/components/user/UserProfileIcon/UserProfileIcon.styles';
 
 export interface SortPropDownProps {
 	sortList: string[];
@@ -10,6 +11,7 @@ export interface SortPropDownProps {
 
 const SortDropdown = ({ sortList, sortIndex, setSortIndex }: SortPropDownProps) => {
 	const [onDropdown, setOnDropdown] = useState(false);
+	const [isLog, setIsLog] = useState(false);
 
 	const handleClickSortBox = () => {
 		setOnDropdown((prevOnDropdown) => !prevOnDropdown);
@@ -20,22 +22,46 @@ const SortDropdown = ({ sortList, sortIndex, setSortIndex }: SortPropDownProps) 
 		setOnDropdown(false);
 	};
 
+	useEffect(() => {
+		if (isLog) {
+			setIsLog(false);
+		}
+	}, [isLog]);
+
+	const handleSortDropdownKeydown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+		if (e.key === 'Escape') {
+			setIsLog(true);
+			setOnDropdown(false);
+		}
+	};
+
 	return (
-		<S.Container>
-			<S.SortBox onClick={handleClickSortBox}>
+		<S.Container onKeyDown={handleSortDropdownKeydown}>
+			<S.SortBox
+				onClick={handleClickSortBox}
+				tabIndex={0}
+				aria-label={`게시물 정렬 드롭다운 선택된 상태 ${sortIndex}`}
+			>
 				<div>{sortIndex}</div>
 				<S.ArrowDown />
 			</S.SortBox>
 
 			{onDropdown && (
-				<S.DropdownBox>
+				<S.DropdownBox role="tablist">
 					{sortList.map((sort, idx) => (
-						<S.DropdownItem key={idx} idx={idx} onClick={() => handleClickDropdownItem(sort)}>
+						<S.DropdownItem
+							key={idx}
+							idx={idx}
+							onClick={() => handleClickDropdownItem(sort)}
+							role="tab"
+							tabIndex={0}
+						>
 							{sort}
 						</S.DropdownItem>
 					))}
 				</S.DropdownBox>
 			)}
+			{isLog && <SrOnlyContainer role="alert">닫힘</SrOnlyContainer>}
 		</S.Container>
 	);
 };
