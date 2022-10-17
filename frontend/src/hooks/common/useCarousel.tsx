70cc7f4d8dfd5121730d41166ad6841f14c7fc4e
hwangjungmin <hwangstar156@gmail.com>
2022-10-17 21:23:26 +0900

    fix: window가 포커스 안되있을때 state는 변하지만 스크롤은 변하지 않는 문제 해결

--- frontend/src/hooks/common/useCarousel.tsx
+++ frontend/src/hooks/common/useCarousel.tsx
@@ -1,4 +1,5 @@
 import useDebounce from './useDebounce';
+import usePrevState from './usePrevState';
 import { useState, useCallback, useEffect, useRef } from 'react';
 
 import { DEBOUNCE_RESIZE_TIME, DEBOUNCE_SCROLL_TIME } from '@/constants';
@@ -9,9 +10,29 @@ const MAX_CAROUSEL_INDEX = 9;
 const useCarousel = () => {
 	const [carouselElementRef, setCarouselElementRef] = useState<null | HTMLDivElement>(null);
 	const [currentIndex, setCurrentIndex] = useState(0);
+	const [scrollable, setScrollable] = useState(true);
 	const [isScroll, setIsScroll] = useState(false);
 	const timerId = useRef<number | null>();
 	const carouselItemWidth = useRef<number>(0);
+	const prevCurrentIndex = usePrevState(currentIndex);
+
+	useEffect(() => {
+		const handleBlurWindow = () => {
+			setScrollable(false);
+		};
+
+		const handleFocusWindow = () => {
+			setScrollable(true);
+		};
+
+		window.addEventListener('blur', handleBlurWindow);
+		window.addEventListener('focus', handleFocusWindow);
+
+		return () => {
+			window.removeEventListener('blur', handleBlurWindow);
+			window.removeEventListener('focus', handleFocusWindow);
+		};
+	}, []);
 
 	const setCarouselItemWidth = useDebounce(() => {
 		if (carouselElementRef) {
@@ -47,16 +68,27 @@ const useCarousel = () => {
 		};
 	}, [carouselElementRef]);
 
+	useEffect(() => {
+		if (prevCurrentIndex === MAX_CAROUSEL_INDEX) {
+			return;
+		}
+
+		const isRightSlide = prevCurrentIndex < currentIndex;
+
+		carouselElementRef?.scrollBy({
+			left: carouselItemWidth.current * (isRightSlide ? 1 : -1),
+			behavior: 'smooth',
+		});
+	}, [currentIndex]);
+
 	const handleLeftSlideEvent = () => {
 		if (currentIndex === MIN_CAROUSEL_INDEX || isScroll) {
 			return;
 		}
 
-		carouselElementRef?.scrollBy({
-			left: -carouselItemWidth.current,
-			behavior: 'smooth',
-		});
-		setCurrentIndex((prev) => prev - 1);
+		if (scrollable) {
+			setCurrentIndex((prev) => prev - 1);
+		}
 	};
 
 	const handleRightSlideEvent = () => {
@@ -66,6 +98,7 @@ const useCarousel = () => {
 
 		if (currentIndex === MAX_CAROUSEL_INDEX) {
 			setCurrentIndex(MIN_CAROUSEL_INDEX);
+
 			carouselElementRef?.scrollTo({
 				left: carouselItemWidth.current,
 				behavior: 'smooth',
@@ -73,12 +106,9 @@ const useCarousel = () => {
 			return;
 		}
 
-		carouselElementRef?.scrollBy({
-			left: carouselItemWidth.current,
-			behavior: 'smooth',
-		});
-
-		setCurrentIndex((prev) => prev + 1);
+		if (scrollable) {
+			setCurrentIndex((prev) => prev + 1);
+		}
 	};
 
 	const handleCarouselElementRef = useCallback((node: HTMLDivElement) => {
