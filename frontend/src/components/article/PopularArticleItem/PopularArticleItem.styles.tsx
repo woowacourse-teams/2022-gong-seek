c1566251c9dc01f79a2e3c97f58a1325124c4635
hwangjungmin <hwangstar156@gmail.com>
2022-10-18 12:55:17 +0900

    refactor: inline css props 제거

--- frontend/src/components/article/PopularArticleItem/PopularArticleItem.styles.tsx
+++ frontend/src/components/article/PopularArticleItem/PopularArticleItem.styles.tsx
@@ -1,5 +1,12 @@
 import { AiOutlineEye, AiOutlineMessage, AiFillHeart } from 'react-icons/ai';
 
+import {
+	UserProfile,
+	ArticleItemTitle,
+	HashTagListBox,
+} from '@/components/article/ArticleItem/ArticleItem.styles';
+import { Category } from '@/types/articleResponse';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 export const CategoryBox = styled.div<{ categoryType: 'question' | 'discussion' }>`
@@ -36,4 +43,41 @@ export const IconContainer = styled.div`
 	gap: ${({ theme }) => theme.size.SIZE_006};
 	font-size: ${({ theme }) => theme.size.SIZE_010};
 	font-weight: 400;
+`;
+
+export const AuthorNameText = styled.div`
+	color: white;
+`;
+
+export const PopularArticleUserProfile = styled(UserProfile)`
+	border: 2px solid transparent;
+	background-image: linear-gradient(to bottom, #ffffcc 7%, #ff9900 96%),
+		linear-gradient(to bottom, #ff3300 7%, #66ffcc 96%);
+	background-origin: border-box;
+	background-clip: content-box, border-box;
+`;
+
+export const PopularArticleHeader = styled.div<{ category: Category }>`
+	${({ theme, category }) => css`
+		background: ${category === 'question' ? theme.colors.RED_500 : theme.colors.BLUE_500};
+		opacity: 0.8;
+		box-shadow: 0, 0, 0, inset 0 0 4px #5e0080;
+		padding: ${theme.size.SIZE_012};
+		border-top-left-radius: ${theme.size.SIZE_010};
+		border-top-right-radius: ${theme.size.SIZE_010};
+		margin-bottom: 1rem;
+	`}
+`;
+
+export const PopularArticleContent = styled.div`
+	padding: ${({ theme }) => theme.size.SIZE_014};
+`;
+
+export const PopularArticleHashTagListBox = styled(HashTagListBox)`
+	margin: 1rem 0;
+	height: inherit;
+`;
+
+export const PopularArticleItemTitle = styled(ArticleItemTitle)`
+	height: inherit;
 `;
--- frontend/src/components/article/PopularArticleItem/PopularArticleItem.tsx
+++ frontend/src/components/article/PopularArticleItem/PopularArticleItem.tsx
@@ -4,11 +4,9 @@ import Card from '@/components/@common/Card/Card';
 import * as S from '@/components/article/ArticleItem/ArticleItem.styles';
 import * as PopularS from '@/components/article/PopularArticleItem/PopularArticleItem.styles';
 import { CAROUSEL_AUTO_PLAY_TIME } from '@/constants/index';
-import { theme } from '@/styles/Theme';
 import { PopularArticleItemCardStyle } from '@/styles/cardStyle';
 import { CommonArticleType } from '@/types/articleResponse';
 import { convertGithubAvatarUrlForResize, dateTimeConverter } from '@/utils/converter';
-import { css } from '@emotion/react';
 
 const PopularArticleItem = ({
 	article,
@@ -38,51 +36,20 @@ const PopularArticleItem = ({
 	return (
 		<>
 			<Card {...PopularArticleItemCardStyle} isActive={isActive} onClick={onClick} role="tab">
-				<div
-					css={css`
-						background: ${category === 'question' ? theme.colors.RED_500 : theme.colors.BLUE_500};
-						opacity: 0.8;
-						box-shadow: 0, 0, 0, inset 0 0 4px #5e0080;
-						padding: ${theme.size.SIZE_012};
-						border-top-left-radius: ${theme.size.SIZE_010};
-						border-top-right-radius: ${theme.size.SIZE_010};
-						margin-bottom: 1rem;
-					`}
-				>
+				<PopularS.PopularArticleHeader category={category}>
 					<S.ProfileBox>
-						<S.UserProfile
+						<PopularS.PopularArticleUserProfile
 							src={convertGithubAvatarUrlForResize(author.avatarUrl)}
 							alt="프로필 이미지"
-							css={css`
-								border: 2px solid transparent;
-								background-image: linear-gradient(to bottom, #ffffcc 7%, #ff9900 96%),
-									linear-gradient(to bottom, #ff3300 7%, #66ffcc 96%);
-								background-origin: border-box;
-								background-clip: content-box, border-box;
-							`}
 						/>
-						<div
-							css={css`
-								color: white;
-							`}
-						>
-							{author.name}
-						</div>
+						<PopularS.AuthorNameText>{author.name}</PopularS.AuthorNameText>
 						<PopularS.CategoryBox categoryType={category}>{한글_카테고리}</PopularS.CategoryBox>
 					</S.ProfileBox>
-				</div>
-				<div
-					css={css`
-						padding: ${theme.size.SIZE_014};
-					`}
-				>
-					<S.ArticleItemTitle
-						css={css`
-							height: inherit;
-						`}
-					>
+				</PopularS.PopularArticleHeader>
+				<PopularS.PopularArticleContent>
+					<PopularS.PopularArticleItemTitle>
 						<div>{title}</div>
-					</S.ArticleItemTitle>
+					</PopularS.PopularArticleItemTitle>
 					<S.ArticleInfoBox>
 						<S.ArticleTimeStamp>{dateTimeConverter(createdAt)}</S.ArticleTimeStamp>
 						<S.ArticleInfoSubBox>
@@ -101,12 +68,7 @@ const PopularArticleItem = ({
 						</S.ArticleInfoSubBox>
 					</S.ArticleInfoBox>
 
-					<S.HashTagListBox
-						css={css`
-							margin: 1rem 0;
-							height: inherit;
-						`}
-					>
+					<PopularS.PopularArticleHashTagListBox>
 						{tag &&
 							tag.length >= 1 &&
 							tag.map((item) => (
@@ -115,8 +77,8 @@ const PopularArticleItem = ({
 									{item}
 								</S.HashTagItem>
 							))}
-					</S.HashTagListBox>
-				</div>
+					</PopularS.PopularArticleHashTagListBox>
+				</PopularS.PopularArticleContent>
 			</Card>
 		</>
 	);
