f0aa46ba269f5103abda481d2b315cbf24cc15e2
brorae <jhr3239@gmail.com>
2022-10-20 21:43:12 +0900

    회원이름 길이 제한 255자로 설정 (#829)
    * refactor: 회원이름 길이 제한 255자로 설정
    
    * refactor: dto 검증 제거

---
+++ backend/src/main/java/com/woowacourse/gongseek/member/exception/NameTooLongException.java
@@ -0,0 +1,7 @@
+package com.woowacourse.gongseek.member.exception;
+
+import com.woowacourse.gongseek.common.exception.BadRequestException;
+
+public class NameTooLongException extends BadRequestException {
+
+}
