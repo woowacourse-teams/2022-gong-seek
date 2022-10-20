992228c4f972d1205ab181d456728acd32cfd814
Jungmin Hwang <85891751+hwangstar156@users.noreply.github.com>
2022-10-20 17:28:31 +0900

    image converter 요청 보내는 url을 변경한다.  (#801)
    * feat: imageUrl 변경하는 함수
    
    * fix: 이미지 서버 주소 변경에 따른 수정 진행
    
    Co-authored-by: JASUN LEE <liswktjs@gmail.com>
    Co-authored-by: JASUN LEE <60773373+liswktjs@users.noreply.github.com>

--- frontend/src/constants/apiUrl.ts
+++ frontend/src/constants/apiUrl.ts
@@ -1,2 +1,2 @@
-export const HOME_URL = process.env.STORYBOOK ? process.env.STORYBOOK_URL : process.env.HOME_URL;
-export const IMAGE_URL = process.env.IMAGE_URL;
+export const HOME_URL = process.env.HOME_URL;
+
