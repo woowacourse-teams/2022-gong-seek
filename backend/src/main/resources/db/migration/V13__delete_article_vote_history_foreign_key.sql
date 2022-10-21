c59310b2d2cac6d7dd4bf8ce21a6dd4b5a6eb736
Gyuchool <balance0591@naver.com>
2022-10-21 10:41:26 +0900

    chore: flyway에서 외래키 제거 추가

---
+++ backend/src/main/resources/db/migration/V13__delete_article_vote_history_foreign_key.sql
@@ -0,0 +1,3 @@
+alter table comment drop foreign key FK5yx0uphgjc6ik6hb82kkw501y;
+alter table likes drop foreign key FK1hlv6urq91y6fqfg6bds5gvp9;
+alter table vote_history drop foreign key FKa7aj8mtky83ty4x7qkykwdkvl;
