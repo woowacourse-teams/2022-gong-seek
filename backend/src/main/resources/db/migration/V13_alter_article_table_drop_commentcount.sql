1873e740ed2a11deb76d445b92ee7dba27a080e1
brorae <jhr3239@gmail.com>
2022-10-31 19:26:02 +0900

    chore: article version drop 추가

--- backend/src/main/resources/db/migration/V13_alter_article_table_drop_commentcount.sql
+++ backend/src/main/resources/db/migration/V13_alter_article_table_drop_commentcount.sql
@@ -1,4 +1,5 @@
 alter table article drop comment_count;
+alter table article drop version;
 alter table vote_item drop version;
 alter table vote_history drop index idx_vote_vote_history;
 alter table comment drop index idx_comment_article;
