cf43924f648b210ba1352ed75cb82018a40e9de8
Hanul Lee <46413629+hanull@users.noreply.github.com>
2022-11-12 10:44:52 +0900

    refactor: 불필요한 이미지 스토리지 디렉토리 삭제

--- image-storage/src/main/java/com/gongseek/imagestorage/exception/FileNameEmptyException.java
+++
@@ -1,8 +0,0 @@
-package com.gongseek.imagestorage.exception;
-
-public class FileNameEmptyException extends ImageFileIOException {
-
-    public FileNameEmptyException() {
-        super("이미지 파일명은 공백일 수 없습니다.");
-    }
-}
