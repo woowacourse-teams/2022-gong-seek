cb198d2a9bdf2d9164c68875665024b032ab20d6
jurlring <dbswnfl2@naver.com>
2022-11-23 15:46:19 +0900

    refactor: 추천 생성, 삭제시 비회원 권한 확인 로직 제거

--- backend/src/main/java/com/woowacourse/gongseek/like/domain/repository/LikeRepository.java
+++ backend/src/main/java/com/woowacourse/gongseek/like/domain/repository/LikeRepository.java
@@ -1,11 +1,15 @@
 package com.woowacourse.gongseek.like.domain.repository;
 
+import com.woowacourse.gongseek.article.domain.Article;
 import com.woowacourse.gongseek.like.domain.Like;
+import com.woowacourse.gongseek.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
 
     void deleteByArticleIdAndMemberId(Long articleId, Long memberId);
+
+    void deleteByArticleAndMember(Article article, Member member);
 
     boolean existsByArticleIdAndMemberId(Long articleId, Long memberId);
 
